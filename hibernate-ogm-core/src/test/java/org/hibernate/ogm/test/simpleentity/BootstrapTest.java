dab73c8d81fc32648578cfdbc352e6c13cb2fa20
Emmanuel Bernard <emmanuel@hibernate.org>
2010-08-17 14:32:46 +0200

    Improve test to add insertion and subsequent read

--- hibernate-ogm-core/src/test/java/org/hibernate/ogm/test/simpleentity/BootstrapTest.java
+++ hibernate-ogm-core/src/test/java/org/hibernate/ogm/test/simpleentity/BootstrapTest.java
@@ -1,6 +1,7 @@
 package org.hibernate.ogm.test.simpleentity;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 /**
  * @author Emmanuel Bernard
@@ -9,6 +10,16 @@ public class BootstrapTest extends OgmTestCase {
 
 	public void testBootstrap() throws Exception {
 		final Session session = openSession();
+		Hypothesis hyp = new Hypothesis();
+		hyp.setId( "1234567890" );
+		hyp.setDescription( "P != NP" );
+		final Transaction transaction = session.beginTransaction();
+		session.persist( hyp );
+		session.flush();
+		session.clear();
+		final Hypothesis loadedHyp = (Hypothesis) session.get( Hypothesis.class, hyp.getId() );
+		assertEquals( hyp.getDescription(), loadedHyp.getDescription() );
+		transaction.rollback();
 		session.close();
 	}
 
