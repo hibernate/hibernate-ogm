73931a92a8377afe26bad147c521cfa116bda873
Emmanuel Bernard <emmanuel@hibernate.org>
2011-04-07 23:08:23 +0200

    OGM-36 Make grid keys serializable

--- hibernate-ogm-core/src/main/java/org/hibernate/ogm/grid/AssociationKey.java
+++ hibernate-ogm-core/src/main/java/org/hibernate/ogm/grid/AssociationKey.java
@@ -31,6 +31,8 @@ import java.util.Arrays;
 public class AssociationKey implements Serializable {
 	private final String table;
 	private final String[] columns;
+	//column value types do have to be serializable so AssociationKey is serializable
+	//should it be a Serializable[] type? It seems to be more pain than anything else
 	private final Object[] columnValues;
 
 	public AssociationKey(String table, String[] columns, Object[] columnValues) {
