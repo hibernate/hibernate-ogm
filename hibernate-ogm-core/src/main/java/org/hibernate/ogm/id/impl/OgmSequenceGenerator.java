a1b1a36d14541d6ca6f4be7bd76fd1a8de58217b
Nabeel Ali Memon <nabeelalimemon@gmail.com>
2011-06-18 21:29:45 +0400

    OGM-33 JPA Sequence identifier generator for grid.

---
+++ hibernate-ogm-core/src/main/java/org/hibernate/ogm/id/impl/OgmSequenceGenerator.java
@@ -0,0 +1,111 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2010-2011, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+
+package org.hibernate.ogm.id.impl;
+
+import java.io.Serializable;
+import java.util.Map;
+import java.util.Properties;
+
+import org.hibernate.HibernateException;
+import org.hibernate.MappingException;
+import org.hibernate.dialect.Dialect;
+import org.hibernate.engine.SessionImplementor;
+import org.hibernate.id.Configurable;
+import org.hibernate.id.PersistentIdentifierGenerator;
+import org.hibernate.id.enhanced.SequenceStyleGenerator;
+import org.hibernate.type.Type;
+
+/**
+ * <p>A JPA sequence-based identifier generator.</p>
+ *
+ * Configuration parameters:
+ * <table>
+ * <tr>
+ * <td><b>NAME</b></td>
+ * <td><b>DESCRIPTION</b></td>
+ * </tr>
+ * <tr>
+ * <td>{@link org.hibernate.id.enhanced.SequenceStyleGenerator#SEQUENCE_PARAM}</td>
+ * <td>The name of the sequence to use store/retrieve sequence values</td>
+ * </tr>
+ * </table>
+ *
+ * @author Nabeel Ali Memon <nabeel@nabeelalimemon.com>
+ */
+public class OgmSequenceGenerator implements PersistentIdentifierGenerator, Configurable {
+	private final OgmTableGenerator tableGenerator;
+
+	public OgmSequenceGenerator() {
+		tableGenerator = new OgmTableGenerator();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void configure(Type type, Properties params, Dialect dialect) throws MappingException {
+		Properties newParams = new Properties( params );
+		for ( Map.Entry<Object, Object> param : params.entrySet() ) {
+			newParams.put( param.getKey(), param.getValue() );
+		}
+		newParams.setProperty(
+				OgmTableGenerator.SEGMENT_VALUE_PARAM,
+				params.getProperty( SequenceStyleGenerator.SEQUENCE_PARAM )
+		);
+		tableGenerator.configure( type, newParams, dialect );
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String[] sqlCreateStrings(Dialect dialect) throws HibernateException {
+		return new String[] { };
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String[] sqlDropStrings(Dialect dialect) throws HibernateException {
+		return new String[] { };
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Object generatorKey() {
+		return tableGenerator.getTableName();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Serializable generate(final SessionImplementor session, Object object) throws HibernateException {
+		return tableGenerator.generate( session, object );
+	}
+}
---
+++ hibernate-ogm-core/src/test/java/org/hibernate/ogm/test/id/SequenceIdGeneratorTest.java
@@ -0,0 +1,98 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.ogm.test.id;
+
+import javax.persistence.EntityManager;
+
+import org.junit.Test;
+
+import org.hibernate.ogm.test.jpa.util.JpaTestCase;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * @author Nabeel Ali Memon <nabeel@nabeelalimemon.com>
+ */
+public class SequenceIdGeneratorTest extends JpaTestCase {
+	@Test
+	public void testSequenceIdGenerationInJTA() throws Exception {
+		getTransactionManager().begin();
+		final EntityManager em = getFactory().createEntityManager();
+		Song firstSong = new Song();
+		firstSong.setSinger( "Charlotte Church" );
+		firstSong.setTitle( "Ave Maria" );
+		em.persist( firstSong );
+
+		Song secondSong = new Song();
+		secondSong.setSinger( "Charlotte Church" );
+		secondSong.setTitle( "Flower Duet" );
+		em.persist( secondSong );
+
+		Actor firstActor = new Actor();
+		firstActor.setName( "Russell Crowe" );
+		firstActor.setBestMovieTitle( "Gladiator" );
+		em.persist( firstActor );
+
+		Actor secondActor = new Actor();
+		secondActor.setName( "Johnny Depp" );
+		secondActor.setBestMovieTitle( "Pirates of the Caribbean" );
+		em.persist( secondActor );
+		getTransactionManager().commit();
+
+		em.clear();
+
+		getTransactionManager().begin();
+		firstSong = em.find( Song.class, firstSong.getId() );
+		assertThat( firstSong ).isNotNull();
+		assertThat( firstSong.getId() ).isEqualTo( Song.INITIAL_VALUE );
+		assertThat( firstSong.getTitle() ).isEqualTo( "Ave Maria" );
+		em.remove( firstSong );
+
+		secondSong = em.find( Song.class, secondSong.getId() );
+		assertThat( secondSong ).isNotNull();
+		assertThat( secondSong.getId() ).isEqualTo( Song.INITIAL_VALUE + 1 );
+		assertThat( secondSong.getTitle() ).isEqualTo( "Flower Duet" );
+		em.remove( secondSong );
+
+		firstActor = em.find( Actor.class, firstActor.getId() );
+		assertThat( firstActor ).isNotNull();
+		assertThat( firstActor.getId() ).isEqualTo( Actor.INITIAL_VALUE );
+		assertThat( firstActor.getName() ).isEqualTo( "Russell Crowe" );
+		em.remove( firstActor );
+
+		secondActor = em.find( Actor.class, secondActor.getId() );
+		assertThat( secondActor ).isNotNull();
+		assertThat( secondActor.getId() ).isEqualTo( Actor.INITIAL_VALUE + 1 );
+		assertThat( secondActor.getName() ).isEqualTo( "Johnny Depp" );
+		em.remove( secondActor );
+		getTransactionManager().commit();
+
+		em.close();
+	}
+
+	@Override
+	public Class<?>[] getEntities() {
+		return new Class<?>[] {
+				Song.class,
+				Actor.class
+		};
+	}
+}
