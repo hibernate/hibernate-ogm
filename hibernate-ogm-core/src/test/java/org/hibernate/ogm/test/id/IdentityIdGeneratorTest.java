2c0c633846e694e1d75ec55546bf0d078a42573d
Nabeel Ali Memon <nabeelalimemon@gmail.com>
2011-06-19 15:33:46 +0400

    OGM-35 JPA Identity identifier generator for grid.

---
+++ hibernate-ogm-core/src/test/java/org/hibernate/ogm/test/id/IdentityIdGeneratorTest.java
@@ -0,0 +1,74 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.ogm.test.id;
+
+import javax.persistence.EntityManager;
+
+import org.junit.Test;
+
+import org.hibernate.ogm.test.jpa.util.JpaTestCase;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * @author Nabeel Ali Memon <nabeel@nabeelalimemon.com>
+ */
+public class IdentityIdGeneratorTest extends JpaTestCase {
+	@Test
+	public void testIdentityGenerator() throws Exception {
+		getTransactionManager().begin();
+		final EntityManager em = getFactory().createEntityManager();
+		Animal jungleKing = new Animal();
+		jungleKing.setName( "Lion" );
+		jungleKing.setSpecies( "Mammal" );
+		em.persist( jungleKing );
+
+		Animal fish = new Animal();
+		fish.setName( "Shark" );
+		fish.setSpecies( "Tiger Shark" );
+		em.persist( fish );
+		getTransactionManager().commit();
+
+		em.clear();
+
+		getTransactionManager().begin();
+		Animal animal = em.find( Animal.class, jungleKing.getId() );
+		assertThat( animal ).isNotNull();
+		assertThat( animal.getId() ).isEqualTo( 1 );
+		assertThat( animal.getName() ).isEqualTo( "Lion" );
+		em.remove( animal );
+
+		animal = em.find( Animal.class, fish.getId() );
+		assertThat( animal ).isNotNull();
+		assertThat( animal.getId() ).isEqualTo( 2 );
+		assertThat( animal.getName() ).isEqualTo( "Shark" );
+		em.remove( animal );
+		getTransactionManager().commit();
+		em.close();
+	}
+
+	@Override
+	public Class<?>[] getEntities() {
+		return new Class<?>[] {
+				Animal.class
+		};
+	}
+}
