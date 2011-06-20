d63665edd7e9f3b65ac7039cb9d51aed975d652d
Nabeel Ali Memon <nabeelalimemon@gmail.com>
2011-06-20 12:37:57 +0400

    OGM-34 JPA Auto identifier generator for grid.

---
+++ hibernate-ogm-core/src/test/java/org/hibernate/ogm/test/id/AutoIdGeneratorTest.java
@@ -0,0 +1,72 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.ogm.test.id;
+
+import javax.persistence.EntityManager;
+
+import org.junit.Test;
+
+import org.hibernate.ogm.test.jpa.util.JpaTestCase;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * Test case for JPA Auto identifier generator.
+ *
+ * @author Nabeel Ali Memon <nabeel@nabeelalimemon.com>
+ */
+public class AutoIdGeneratorTest extends JpaTestCase {
+
+	@Test
+	public void testAutoIdentifierGenerator() throws Exception {
+		getTransactionManager().begin();
+		final EntityManager em = getFactory().createEntityManager();
+		DistributedRevisionControl git = new DistributedRevisionControl();
+		git.setName( "Git" );
+		em.persist( git );
+
+		DistributedRevisionControl bzr = new DistributedRevisionControl();
+		bzr.setName( "Bazaar" );
+		em.persist( bzr );
+		getTransactionManager().commit();
+
+		em.clear();
+		getTransactionManager().begin();
+		DistributedRevisionControl dvcs = em.find( DistributedRevisionControl.class, git.getId() );
+		assertThat( dvcs ).isNotNull();
+		assertThat( dvcs.getId() ).isEqualTo( 1 );
+		em.remove( dvcs );
+
+		dvcs = em.find( DistributedRevisionControl.class, bzr.getId() );
+		assertThat( dvcs ).isNotNull();
+		assertThat( dvcs.getId() ).isEqualTo( 2 );
+
+		getTransactionManager().commit();
+		em.close();
+	}
+
+	@Override
+	public Class<?>[] getEntities() {
+		return new Class<?>[] {
+				DistributedRevisionControl.class
+		};
+	}
+}
