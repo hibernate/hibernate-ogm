e0300d54bf82762408f0ea1bf98f089f54d2368c
Guillaume Scheibel <guillaume.scheibel@gmail.com>
2012-03-06 16:33:22 +0100

    add CRUD tests on Embedded objects

--- hibernate-ogm-mongodb/src/test/java/org/hibernate/ogm/test/associations/embedded/CrudTest.java
+++ hibernate-ogm-mongodb/src/test/java/org/hibernate/ogm/test/associations/embedded/CrudTest.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
 import org.hibernate.ogm.test.Utils;
+
 import org.hibernate.ogm.test.associations.embedded.entities.EmbeddedObject;
 import org.hibernate.ogm.test.associations.embedded.entities.EmbeddedSecondLvl;
 import org.hibernate.ogm.test.associations.embedded.entities.Root;
@@ -24,18 +25,18 @@ public class CrudTest {
 		int embIntValue = 10;
 		String secondValue = "secondLevel";
 
-		String id = this.createDefaultObject(rootValue, embValue, embIntValue, secondValue);
+		String id = this.createDefaultObject( rootValue, embValue, embIntValue, secondValue );
 
-		Root checkRoot = em.find(Root.class, id);
-		assertNotNull(checkRoot);
-		assertEquals(checkRoot.getRootValue(), rootValue);
-		assertNotNull(checkRoot.getEmbeddedObject());
+		Root checkRoot = em.find( Root.class, id );
+		assertNotNull( checkRoot );
+		assertEquals( checkRoot.getRootValue(), rootValue );
+		assertNotNull( checkRoot.getEmbeddedObject() );
 		EmbeddedObject checkEmb = checkRoot.getEmbeddedObject();
-		assertEquals(checkEmb.getEmbValue(), embValue);
-		assertEquals(checkEmb.getIntValue(), embIntValue);
-		assertNotNull(checkEmb.getSecondLvl());
+		assertEquals( checkEmb.getEmbValue(), embValue );
+		assertEquals( checkEmb.getIntValue(), embIntValue );
+		assertNotNull( checkEmb.getSecondLvl() );
 		EmbeddedSecondLvl secondCheck = checkEmb.getSecondLvl();
-		assertEquals(secondCheck.getSecondLvlValue(), secondValue);
+		assertEquals( secondCheck.getSecondLvlValue(), secondValue );
 
 		em.close();
 
@@ -43,13 +44,13 @@ public class CrudTest {
 
 	private String createDefaultObject(String rootValue, String embValue, int embIntValue, String secondValue) {
 		EntityManager em = Utils.getEM();
-		EmbeddedSecondLvl esl = new EmbeddedSecondLvl(secondValue);
-		EmbeddedObject emb = new EmbeddedObject(embValue, embIntValue, esl);
-		Root root = new Root(rootValue, emb);
+		EmbeddedSecondLvl esl = new EmbeddedSecondLvl( secondValue );
+		EmbeddedObject emb = new EmbeddedObject( embValue, embIntValue, esl );
+		Root root = new Root( rootValue, emb );
 
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
-		em.persist(root);
+		em.persist( root );
 		em.flush();
 		String id = root.getId();
 		tx.commit();
@@ -62,79 +63,80 @@ public class CrudTest {
 		EntityManager em = Utils.getEM();
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
-		String id = this.createDefaultObject("rootValue", "embValue", 100, "second");
-		em.remove(em.find(Root.class, id));
+		String id = this.createDefaultObject( "rootValue", "embValue", 100, "second" );
+		em.remove( em.find( Root.class, id ) );
 		em.flush();
 		tx.commit();
-		assertNull(em.find(Root.class, id));
+		assertNull( em.find( Root.class, id ) );
 		em.close();
 	}
 
 	@Test
 	public void updateFirstLevelTest() {
 		String beforeUpdateValue = "rootValue";
-		String id = this.createDefaultObject(beforeUpdateValue, "embValue", 100, "second");
+		String id = this.createDefaultObject( beforeUpdateValue, "embValue", 100, "second" );
 		EntityManager em = Utils.getEM();
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
-		Root root = em.find(Root.class, id);
-		assertEquals(root.getRootValue(), beforeUpdateValue);
+		Root root = em.find( Root.class, id );
+		assertEquals( root.getRootValue(), beforeUpdateValue );
 
 		String newRootValue = "new Value";
-		root.setRootValue(newRootValue);
-		em.merge(root);
+		root.setRootValue( newRootValue );
+		em.merge( root );
 		em.flush();
 		tx.commit();
 
-		Root checkRoot = em.find(Root.class, id);
-		assertEquals(checkRoot.getRootValue(), newRootValue);
+		Root checkRoot = em.find( Root.class, id );
+		assertEquals( checkRoot.getRootValue(), newRootValue );
 		em.close();
 	}
-	
+
 	@Test
-	public void updateSecondLevelTest(){
+	public void updateSecondLevelTest() {
 		String beforeUpdateValue = "embValue";
 		int beforeIntValue = 100;
-		String id = this.createDefaultObject("rootValue", beforeUpdateValue, beforeIntValue, "second");
+		String id = this.createDefaultObject( "rootValue", beforeUpdateValue, beforeIntValue, "second" );
+
 		EntityManager em = Utils.getEM();
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
-		Root root = em.find(Root.class, id);
-		assertEquals(root.getEmbeddedObject().getEmbValue(), beforeUpdateValue);
-		assertEquals(root.getEmbeddedObject().getIntValue(), beforeIntValue);
+		Root root = em.find( Root.class, id );
+		assertEquals( root.getEmbeddedObject().getEmbValue(), beforeUpdateValue );
+		assertEquals( root.getEmbeddedObject().getIntValue(), beforeIntValue );
 
 		String newSecondValue = "new Value";
 		int newIntValue = 200;
-		root.getEmbeddedObject().setEmbValue(newSecondValue);
-		root.getEmbeddedObject().setIntValue(newIntValue);
-		em.merge(root);
+		root.getEmbeddedObject().setEmbValue( newSecondValue );
+		root.getEmbeddedObject().setIntValue( newIntValue );
+		em.merge( root );
 		em.flush();
 		tx.commit();
 
-		Root checkRoot = em.find(Root.class, id);
-		assertEquals(checkRoot.getEmbeddedObject().getEmbValue(), newSecondValue);
-		assertEquals(checkRoot.getEmbeddedObject().getIntValue(), newIntValue);
+		Root checkRoot = em.find( Root.class, id );
+		assertEquals( checkRoot.getEmbeddedObject().getEmbValue(), newSecondValue );
+		assertEquals( checkRoot.getEmbeddedObject().getIntValue(), newIntValue );
 		em.close();
 	}
-	
-	
+
 	@Test
-	public void updateThirdLevelTest(){
+	public void updateThirdLevelTest() {
 		String beforeUpdateValue = "thirdBefore";
-		String id = this.createDefaultObject("rootValue", "secondLevel", 10, beforeUpdateValue);
+		String id = this.createDefaultObject( "rootValue", "secondLevel", 10, beforeUpdateValue );
+
 		EntityManager em = Utils.getEM();
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
-		Root root = em.find(Root.class, id);
-		
+		Root root = em.find( Root.class, id );
+
 		String newThirdValue = "new Value";
-		root.getEmbeddedObject().getSecondLvl().setSecondLvlValue(newThirdValue);
-		em.merge(root);
+		root.getEmbeddedObject().getSecondLvl().setSecondLvlValue( newThirdValue );
+		em.merge( root );
 		em.flush();
 		tx.commit();
 
-		Root checkRoot = em.find(Root.class, id);
-		assertEquals(checkRoot.getEmbeddedObject().getSecondLvl().getSecondLvlValue(),newThirdValue );
+		Root checkRoot = em.find( Root.class, id );
+		assertEquals( checkRoot.getEmbeddedObject().getSecondLvl().getSecondLvlValue(), newThirdValue );
 		em.close();
 	}
 }
