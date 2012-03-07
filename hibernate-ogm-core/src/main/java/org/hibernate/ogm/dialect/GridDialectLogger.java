ef72fb3f9b552820f84b0c77c05c144b8df79853
Sebastien Lorber <lorber.sebastien@gmail.com>
2012-03-07 22:56:47 +0100

    OGM-73 Put logger dialect decorator to log operations

---
+++ hibernate-ogm-core/src/main/java/org/hibernate/ogm/dialect/GridDialectLogger.java
@@ -0,0 +1,129 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.ogm.dialect;
+
+import org.hibernate.LockMode;
+import org.hibernate.dialect.lock.LockingStrategy;
+import org.hibernate.id.IntegralDataTypeHolder;
+import org.hibernate.ogm.datastore.spi.Association;
+import org.hibernate.ogm.datastore.spi.Tuple;
+import org.hibernate.ogm.grid.AssociationKey;
+import org.hibernate.ogm.grid.EntityKey;
+import org.hibernate.ogm.grid.RowKey;
+import org.hibernate.ogm.util.impl.Log;
+import org.hibernate.ogm.util.impl.LoggerFactory;
+import org.hibernate.persister.entity.Lockable;
+
+/**
+ * A wrapper dialect that logs the calls performed on the real dialect.
+ * It is only used when this class's logger level is set to Trace
+ *
+ * @author Sebastien Lorber (<i>lorber.sebastien@gmail.com</i>)
+ * @see org.hibernate.ogm.dialect.impl.GridDialectFactoryImpl#buildGridDialect(java.util.Map, org.hibernate.service.ServiceRegistry)
+ */
+public class GridDialectLogger implements GridDialect {
+
+	private static final Log log = LoggerFactory.make();
+
+	private final GridDialect gridDialect; // the real wrapped grid dialect
+
+	public GridDialectLogger(GridDialect gridDialect) {
+		if (gridDialect == null) {
+			throw new IllegalArgumentException("GridDialect should never be null");
+		}
+		this.gridDialect = gridDialect;
+	}
+
+	/**
+	 * Returns true if this grid dialect logger should wrap the real grid dialect
+	 *
+	 * @return boolean
+	 */
+	public static boolean isActive() {
+		return log.isTraceEnabled();
+	}
+
+	@Override
+	public LockingStrategy getLockingStrategy(Lockable lockable, LockMode lockMode) {
+		return gridDialect.getLockingStrategy(lockable, lockMode);
+	}
+
+	@Override
+	public Tuple getTuple(EntityKey key) {
+		log.trace("Reading Tuple from store with key: " + key);
+		return gridDialect.getTuple(key);
+	}
+
+	@Override
+	public Tuple createTuple(EntityKey key) {
+		log.trace("Creating Tuple in store (or not!) with key: " + key);
+		return gridDialect.createTuple(key);
+	}
+
+	@Override
+	public void updateTuple(Tuple tuple, EntityKey key) {
+		log.trace("Updating Tuple from store with key: " + key);
+		gridDialect.updateTuple(tuple, key);
+	}
+
+	@Override
+	public void removeTuple(EntityKey key) {
+		log.trace("Removing Tuple from store with key: " + key);
+		gridDialect.removeTuple(key);
+	}
+
+	@Override
+	public Association getAssociation(AssociationKey key) {
+		log.trace("Reading association from store with key: " + key);
+		return gridDialect.getAssociation(key);
+	}
+
+	@Override
+	public Association createAssociation(AssociationKey key) {
+		log.trace("Creating association in store (or not!) with key: " + key);
+		return gridDialect.createAssociation(key);
+	}
+
+	@Override
+	public void updateAssociation(Association association, AssociationKey key) {
+		log.trace("Updating association from store with key: " + key);
+		gridDialect.updateAssociation(association, key);
+	}
+
+	@Override
+	public void removeAssociation(AssociationKey key) {
+		log.trace("Removing association from store with key: " + key);
+		gridDialect.removeAssociation(key);
+	}
+
+	@Override
+	public Tuple createTupleAssociation(AssociationKey associationKey, RowKey rowKey) {
+		log.trace("Creating Tuple association from store with associationKey: " + associationKey + " and rowkey" + rowKey);
+		return gridDialect.createTupleAssociation(associationKey, rowKey);
+	}
+
+	@Override
+	public void nextValue(RowKey key, IntegralDataTypeHolder value, int increment, int initialValue) {
+		log.trace("Reading next value from key: " + key);
+		gridDialect.nextValue(key, value, increment, initialValue);
+	}
+
+}
