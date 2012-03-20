5e9915049632e3a261280f65ace6891d8d50049e
Sanne Grinovero <sanne@hibernate.org>
2012-03-20 13:28:51 +0000

    OGM-132 MongoDB module requires Search dependencies for tests

---
+++ hibernate-ogm-mongodb/src/main/java/org/hibernate/ogm/datastore/mongodb/Environment.java
@@ -0,0 +1,57 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010-2012 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.ogm.datastore.mongodb;
+
+/**
+ * Configuration options of the MongoDB GridDialect
+ * 
+ * @author Sanne Grinovero <sanne@hibernate.org> (C) 2012 Red Hat Inc.
+ */
+public interface Environment {
+
+	/**
+	 * The MongoDB Database name to connect to.
+	 */
+	public static final String MONGODB_DATABASE = "hibernate.ogm.mongodb.database";
+
+	/**
+	 * The port of the MongoDB instance.
+	 */
+	public static final String MONGODB_PORT = "hibernate.ogm.mongodb.port";
+
+	/**
+	 * The hostname of the MongoDB instance.
+	 */
+	public static final String MONGODB_HOST = "hibernate.ogm.mongodb.host";
+
+	/**
+	 * The default host used to connect to MongoDB: if the {@link MONGODB_HOST}
+	 * property is not set, we'll attempt to connect to localhost.
+	 */
+	public static final String MONGODB_DEFAULT_HOST = "127.0.0.1";
+
+	/**
+	* The default port used to connect to MongoDB: if the {@link MONGODB_PORT}
+	* property is not set, we'll try this port.
+	*/
+	public static final int MONGODB_DEFAULT_PORT = 27017;
+
+}
---
+++ hibernate-ogm-mongodb/src/main/java/org/hibernate/ogm/logging/mongodb/impl/Log.java
@@ -0,0 +1,66 @@
+/* 
+ * Hibernate, Relational Persistence for Idiomatic Java
+ * 
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.ogm.logging.mongodb.impl;
+
+import static org.jboss.logging.Logger.Level.INFO;
+
+import org.hibernate.HibernateException;
+import org.hibernate.ogm.datastore.mongodb.Environment;
+import org.jboss.logging.Cause;
+import org.jboss.logging.LogMessage;
+import org.jboss.logging.Message;
+import org.jboss.logging.MessageLogger;
+
+/**
+ * @author Sanne Grinovero <sanne@hibernate.org> (C) 2012 Red Hat Inc.
+ */
+@MessageLogger(projectCode = "OGM")
+public interface Log extends org.hibernate.ogm.util.impl.Log {
+
+	@LogMessage(level = INFO)
+	@Message(id = 1201, value = "Connecting to MongoDB at %1$s:%2$d")
+	void connectingToMongo(String host, int port);
+
+	@LogMessage(level = INFO)
+	@Message(id = 1202, value = "Closing connection to MongoDB")
+	void disconnectingFromMongo();
+
+	@Message(id = 1203, value = "Unable to find or initialize a connection to the MongoDB server")
+	HibernateException unableToInitializeMongoDB(@Cause RuntimeException e);
+
+	@Message(id = 1204, value = "The value set for the configuration property '" + Environment.MONGODB_PORT + "' must be a number between 1 and 65535")
+	HibernateException mongoPortIllegalValue();
+
+	@Message(id = 1205, value = "Could not resolve MongoDB hostname [%s]")
+	HibernateException mongoOnUnknownHost(String hostname);
+
+	@LogMessage(level = INFO)
+	@Message(id = 1206, value = "Mongo database named [%s] is not defined. Creating it!")
+	void creatingDatabase(String dbName);
+
+	@LogMessage(level = INFO)
+	@Message(id = 1207, value = "Connecting to Mongo database named [%s].")
+	void connectingToMongoDatabase(String dbName);
+
+	@Message(id = 1208, value = "The configuration property '" + Environment.MONGODB_DATABASE + "' was not set. Can't connect to MongoDB.")
+	HibernateException mongoDbNameMissing();
+
+}
