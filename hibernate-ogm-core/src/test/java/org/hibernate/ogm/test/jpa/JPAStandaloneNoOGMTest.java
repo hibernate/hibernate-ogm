470a470948b3705f6c952c995025e4c16638d9ed
Sanne Grinovero <sanne@hibernate.org>
2012-04-24 15:48:18 +0100

    OGM-161 Avoid the need to set the hibernate.dialect in configuration properties

--- hibernate-ogm-core/src/test/java/org/hibernate/ogm/test/jpa/JPAStandaloneNoOGMTest.java
+++ hibernate-ogm-core/src/test/java/org/hibernate/ogm/test/jpa/JPAStandaloneNoOGMTest.java
@@ -20,70 +20,33 @@
  */
 package org.hibernate.ogm.test.jpa;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.hibernate.ogm.test.utils.PackagingRule;
 import org.junit.Rule;
 import org.junit.Test;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.transaction.Status;
-import javax.transaction.TransactionManager;
-
-import static org.fest.assertions.Assertions.assertThat;
-import static org.hibernate.ogm.test.utils.jpa.JpaTestCase.extractJBossTransactionManager;
+import org.junit.rules.ExpectedException;
 
 /**
  * @author Emmanuel Bernard <emmanuel@hibernate.org>
+ * @author Sanne Grinovero <sanne@hibernate.org>
  */
 public class JPAStandaloneNoOGMTest {
 
 	@Rule
 	public PackagingRule packaging = new PackagingRule( "persistencexml/jpajtastandalone-noogm.xml", Poem.class );
 
+	@Rule
+	public ExpectedException error = ExpectedException.none();
+
 	@Test
 	public void testJTAStandaloneNoOgm() throws Exception {
-		EntityManagerFactory emf = null;
-		boolean emfBuilt = false;
-		emf = Persistence.createEntityManagerFactory( "jpajtastandalone-noogm" );
-
-		TransactionManager transactionManager = extractJBossTransactionManager( emf );
-		try {
-
-
-			transactionManager.begin();
-			final EntityManager em = emf.createEntityManager();
-			Poem poem = new Poem();
-			poem.setName( "L'albatros" );
-			em.persist( poem );
-			transactionManager.commit();
-
-			em.clear();
-
-			transactionManager.begin();
-			poem = em.find( Poem.class, poem.getId() );
-			assertThat( poem ).isNotNull();
-			assertThat( poem.getName() ).isEqualTo( "L'albatros" );
-			em.remove( poem );
-			transactionManager.commit();
-
-			em.close();
-
-			emfBuilt = true;
-		}
-		catch (Exception e) {
-			// e.printStackTrace(  );
-		}
-		finally {
-			if ( transactionManager.getStatus() == Status.STATUS_ACTIVE ) {
-				transactionManager.rollback();
-			}
-		}
-		if ( emfBuilt ) {
-			emf.close();
-		}
-		assertThat( emfBuilt ).as( "Should fail as we don't have a RDBMS" ).isFalse();
+		// Failure is expected as we didn't configure a JDBC connection nor a Dialect
+		// (and this would fail only if effectively loading Hibernate ORM without OGM superpowers)
+		error.expect( javax.persistence.PersistenceException.class );
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory( "jpajtastandalone-noogm" );
+		emf.close(); // should not be reached, but cleanup in case the test fails.
 	}
-
 
 }
