1acb2a858e68de72cdbdae72424acbb45f46dddd
Emmanuel Bernard <emmanuel@hibernate.org>
2012-05-03 19:45:09 +0200

    OGM-152 Fix style for CalendarTimeZoneDateTimeTypeDescriptorTest

--- hibernate-ogm-core/src/test/java/org/hibernate/ogm/test/type/descriptor/CalendarTimeZoneDateTimeTypeDescriptorTest.java
+++ hibernate-ogm-core/src/test/java/org/hibernate/ogm/test/type/descriptor/CalendarTimeZoneDateTimeTypeDescriptorTest.java
@@ -35,15 +35,15 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
-@RunWith(value = Parameterized.class)
+@RunWith( value = Parameterized.class )
 public class CalendarTimeZoneDateTimeTypeDescriptorTest {
 
 	private Calendar one;
-	
+
 	private Calendar another;
-	
+
 	private boolean exceptedEquality;
-	
+
 	public CalendarTimeZoneDateTimeTypeDescriptorTest(Calendar one, Calendar another, boolean exceptedEquality) {
 		this.one = one;
 		this.another = another;
@@ -52,31 +52,30 @@ public class CalendarTimeZoneDateTimeTypeDescriptorTest {
 
 	@Parameters
 	public static Collection<Object[]> data() {
-       Calendar past = new GregorianCalendar();
-       past.set(Calendar.DAY_OF_MONTH, 28);
-       past.set(Calendar.MONTH, 12);
-       past.set(Calendar.YEAR, 1976);
-       past.setTimeZone(TimeZone.getTimeZone( "Africa/Casablanca" ));
+		Calendar past = new GregorianCalendar();
+		past.set( Calendar.DAY_OF_MONTH, 28 );
+		past.set( Calendar.MONTH, 12 );
+		past.set( Calendar.YEAR, 1976 );
+		past.setTimeZone( TimeZone.getTimeZone( "Africa/Casablanca" ) );
 
-       Calendar pastGMT = (GregorianCalendar) past.clone();
-       pastGMT.setTimeZone(TimeZone.getDefault());
-       
-	   Object[][] data = new Object[][] { 
-			   { null, null, true }, 
-			   { GregorianCalendar.getInstance(), null, false }, 
-			   {  null, GregorianCalendar.getInstance(), false }, 
-			   { past, past, true }, 
-			   { past, new GregorianCalendar(), false }, 
-			   { past, pastGMT, false }
-			  };
-	   return Arrays.asList(data);
+		Calendar pastGMT = (GregorianCalendar) past.clone();
+		pastGMT.setTimeZone( TimeZone.getDefault() );
+
+		Object[][] data = new Object[][]{
+				{ null, null, true },
+				{ GregorianCalendar.getInstance(), null, false },
+				{ null, GregorianCalendar.getInstance(), false },
+				{ past, past, true },
+				{ past, new GregorianCalendar(), false },
+				{ past, pastGMT, false }
+		};
+		return Arrays.asList( data );
 	}
-	 
+
 	@Test
 	public void testCalendarTimeZoneDateTimeObjects() {
 		CalendarTimeZoneDateTimeTypeDescriptor calendarTimeZoneDateTimeTypeDescriptor = new CalendarTimeZoneDateTimeTypeDescriptor();
-		
-		assertThat(calendarTimeZoneDateTimeTypeDescriptor.areEqual(one, another)).isEqualTo(exceptedEquality);
+		assertThat( calendarTimeZoneDateTimeTypeDescriptor.areEqual( one, another ) ).isEqualTo( exceptedEquality );
 	}
 
 }
