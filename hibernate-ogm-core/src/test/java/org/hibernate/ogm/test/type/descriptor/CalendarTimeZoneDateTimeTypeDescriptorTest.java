61f3e36c254b166373152c1f23e1fed4affccf9b
Oliver Carr <ocarr@redhat.com>
2012-05-03 13:37:29 +0200

    OGM-152 Improve CalendarTimeZoneDateTimeTypeDescriptorTest

--- hibernate-ogm-core/src/test/java/org/hibernate/ogm/test/type/descriptor/CalendarTimeZoneDateTimeTypeDescriptorTest.java
+++ hibernate-ogm-core/src/test/java/org/hibernate/ogm/test/type/descriptor/CalendarTimeZoneDateTimeTypeDescriptorTest.java
@@ -1,3 +1,23 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
 package org.hibernate.ogm.test.type.descriptor;
 
 
@@ -36,7 +56,7 @@ public class CalendarTimeZoneDateTimeTypeDescriptorTest {
        past.set(Calendar.DAY_OF_MONTH, 28);
        past.set(Calendar.MONTH, 12);
        past.set(Calendar.YEAR, 1976);
-//       past.setTimeZone(TimeZone.getTimeZone("gmt +9"));  // check
+       past.setTimeZone(TimeZone.getTimeZone( "Africa/Casablanca" ));
 
        Calendar pastGMT = (GregorianCalendar) past.clone();
        pastGMT.setTimeZone(TimeZone.getDefault());
@@ -58,5 +78,5 @@ public class CalendarTimeZoneDateTimeTypeDescriptorTest {
 		
 		assertThat(calendarTimeZoneDateTimeTypeDescriptor.areEqual(one, another)).isEqualTo(exceptedEquality);
 	}
-	
+
 }
