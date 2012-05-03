1c1cae58df25f1c4e23e5558ca64cb852d854f0f
Emmanuel Bernard <emmanuel@hibernate.org>
2012-05-03 19:28:24 +0200

    OGM-148 Fix style for BuiltInTypeTest

--- hibernate-ogm-core/src/test/java/org/hibernate/ogm/test/type/BuiltInTypeTest.java
+++ hibernate-ogm-core/src/test/java/org/hibernate/ogm/test/type/BuiltInTypeTest.java
@@ -20,7 +20,13 @@
  */
 package org.hibernate.ogm.test.type;
 
-import static org.hibernate.ogm.test.utils.TestHelper.extractEntityTuple;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.ogm.grid.EntityKey;
+import org.hibernate.ogm.test.simpleentity.OgmTestCase;
+import org.hibernate.ogm.util.impl.Log;
+import org.hibernate.ogm.util.impl.LoggerFactory;
+import org.junit.Test;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
@@ -33,13 +39,7 @@ import java.util.Map;
 import java.util.Random;
 import java.util.UUID;
 
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.hibernate.ogm.grid.EntityKey;
-import org.hibernate.ogm.test.simpleentity.OgmTestCase;
-import org.hibernate.ogm.util.impl.Log;
-import org.hibernate.ogm.util.impl.LoggerFactory;
-import org.junit.Test;
+import static org.hibernate.ogm.test.utils.TestHelper.extractEntityTuple;
 
 /**
  * @author Emmanuel Bernard <emmanuel@hibernate.org>
@@ -51,25 +51,26 @@ public class BuiltInTypeTest extends OgmTestCase {
 	private static final Log log = LoggerFactory.make();
 
 	private static final Random RANDOM = new Random();
-	
-	private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS Z");;
-	
+
+	private static final DateFormat DATE_FORMAT = new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss:SSS Z" );
+	;
+
 	@Test
 	public void testTypesSupport() throws Exception {
 		final Session session = openSession();
 
 		Transaction transaction = session.beginTransaction();
 		Bookmark b = new Bookmark();
-		b.setId("42");
+		b.setId( "42" );
 		b.setDescription( "Hibernate Site" );
-		b.setUrl(new URL("http://www.hibernate.org/"));
-		BigDecimal weight = new BigDecimal("21.77");
-		b.setSiteWeight( weight);
-		BigInteger visitCount= new BigInteger( "444");
-		b.setVisitCount(visitCount);
-		b.setFavourite(true);
-		Byte displayMask= new Byte((byte) '8');
-		b.setDisplayMask(displayMask);
+		b.setUrl( new URL( "http://www.hibernate.org/" ) );
+		BigDecimal weight = new BigDecimal( "21.77" );
+		b.setSiteWeight( weight );
+		BigInteger visitCount = new BigInteger( "444" );
+		b.setVisitCount( visitCount );
+		b.setFavourite( true );
+		Byte displayMask = new Byte( (byte) '8' );
+		b.setDisplayMask( displayMask );
 		Date now = new Date( System.currentTimeMillis() );
 		b.setCreationDate( now );
 		b.setDestructionDate( now );
@@ -85,14 +86,14 @@ public class BuiltInTypeTest extends OgmTestCase {
 		blob[3] = '4';
 		blob[4] = '5';
 		b.setBlob( blob );
-		UUID serialNumber= UUID.randomUUID();
-		b.setSerialNumber(serialNumber);
+		UUID serialNumber = UUID.randomUUID();
+		b.setSerialNumber( serialNumber );
 		final Long userId = RANDOM.nextLong();
-		log.info("User ID created: " + userId);
-		b.setUserId(userId);
-		final Integer stockCount = Integer.valueOf(RANDOM.nextInt());
-		b.setStockCount(stockCount);
-		
+		log.info( "User ID created: " + userId );
+		b.setUserId( userId );
+		final Integer stockCount = Integer.valueOf( RANDOM.nextInt() );
+		b.setStockCount( stockCount );
+
 		session.persist( b );
 		transaction.commit();
 
@@ -100,38 +101,38 @@ public class BuiltInTypeTest extends OgmTestCase {
 
 		transaction = session.beginTransaction();
 		b = (Bookmark) session.get( Bookmark.class, b.getId() );
-		assertEquals("http://www.hibernate.org/",b.getUrl().toString());
-		assertEquals(weight,b.getSiteWeight());
-		assertEquals(visitCount,b.getVisitCount());
-		assertEquals(new Boolean(true),b.isFavourite());
-		assertEquals(displayMask,b.getDisplayMask());
-		assertEquals("serial number incorrect", serialNumber,b.getSerialNumber());
-		assertEquals("user id incorrect",userId,b.getUserId());
-		assertEquals("stock count incorrect", stockCount, b.getStockCount());
-		
-		assertEquals("Creation Date Incorrect", now,b.getCreationDate());
+		assertEquals( "http://www.hibernate.org/", b.getUrl().toString() );
+		assertEquals( weight, b.getSiteWeight() );
+		assertEquals( visitCount, b.getVisitCount() );
+		assertEquals( new Boolean( true ), b.isFavourite() );
+		assertEquals( displayMask, b.getDisplayMask() );
+		assertEquals( "serial number incorrect", serialNumber, b.getSerialNumber() );
+		assertEquals( "user id incorrect", userId, b.getUserId() );
+		assertEquals( "stock count incorrect", stockCount, b.getStockCount() );
 
-		assertEquals("Timezone Info Not Correct", iCal.getTimeZone(),b.getDestructionCalendar().getTimeZone());
+		assertEquals( "Creation Date Incorrect", now, b.getCreationDate() );
 
-		assertEquals("Date info String Not Correct iCal",
-				iCal.getTime().toGMTString(),b.getDestructionCalendar().getTime().toGMTString());
+		assertEquals( "Timezone Info Not Correct", iCal.getTimeZone(), b.getDestructionCalendar().getTimeZone() );
 
-	// This test can break in ehcache dialect.
-		assertEquals("Timezone Info Not Correct", iCal.getTimeZone(),b.getDestructionCalendar().getTimeZone());
+		assertEquals( "Date info String Not Correct iCal",
+				iCal.getTime().toGMTString(), b.getDestructionCalendar().getTime().toGMTString() );
 
-		assertEquals("Date info Not Correct iCal: " 
-				+ DATE_FORMAT.format(iCal.getTime()) 
+		// This test can break in ehcache dialect.
+		assertEquals( "Timezone Info Not Correct", iCal.getTimeZone(), b.getDestructionCalendar().getTimeZone() );
+
+		assertEquals( "Date info Not Correct iCal: "
+				+ DATE_FORMAT.format( iCal.getTime() )
 				+ " dest millis: " + b.getDestructionCalendar().getTimeInMillis()
 				+ " iCal millis: " + iCal.getTimeInMillis(),
-				iCal.getTime(),b.getDestructionCalendar().getTime());
-		
-		assertEquals("Byte array incorrect length", blob.length,b.getBlob().length);
-		assertEquals(blob[0],b.getBlob()[0]);
-		assertEquals('1',b.getBlob()[0]);
-		assertEquals('2',b.getBlob()[1]);
-		assertEquals('3',b.getBlob()[2]);
-		assertEquals('4',b.getBlob()[3]);
-		assertEquals('5',b.getBlob()[4]);
+				iCal.getTime(), b.getDestructionCalendar().getTime() );
+
+		assertEquals( "Byte array incorrect length", blob.length, b.getBlob().length );
+		assertEquals( blob[0], b.getBlob()[0] );
+		assertEquals( '1', b.getBlob()[0] );
+		assertEquals( '2', b.getBlob()[1] );
+		assertEquals( '3', b.getBlob()[2] );
+		assertEquals( '4', b.getBlob()[3] );
+		assertEquals( '5', b.getBlob()[4] );
 
 		session.delete( b );
 		transaction.commit();
@@ -154,27 +155,27 @@ public class BuiltInTypeTest extends OgmTestCase {
 		UUID serialNumber = UUID.randomUUID();
 		b.setSerialNumber( serialNumber );
 		final Long userId = RANDOM.nextLong();
-		b.setUserId(userId);
-		final Integer stockCount = Integer.valueOf(RANDOM.nextInt());
-		b.setStockCount(stockCount);
+		b.setUserId( userId );
+		final Integer stockCount = Integer.valueOf( RANDOM.nextInt() );
+		b.setStockCount( stockCount );
 
 		session.persist( b );
 		transaction.commit();
 		session.clear();
 
 		transaction = session.beginTransaction();
-		b = ( Bookmark ) session.get( Bookmark.class, b.getId() );
+		b = (Bookmark) session.get( Bookmark.class, b.getId() );
 
 		//Check directly in the cache the values stored
-		EntityKey key = new EntityKey( "Bookmark", new String[] {"id"}, new Object[] {"42"} );
-		Map<String, String> entity = ( Map<String, String> ) extractEntityTuple( sessions, key );
+		EntityKey key = new EntityKey( "Bookmark", new String[]{ "id" }, new Object[]{ "42" } );
+		Map<String, String> entity = (Map<String, String>) extractEntityTuple( sessions, key );
 
-		assertEquals("Entity visits count incorrect", entity.get( "visits_count" ), "444" );
-		assertEquals("Entity serial number incorrect", entity.get( "serialNumber" ), serialNumber.toString() );
-		assertEquals("Entity URL incorrect", entity.get( "url" ), "http://www.hibernate.org/" );
-		assertEquals("Entity site weight incorrect", entity.get( "site_weight" ), "21.77" );
-		assertEquals("Entity user id incorrect", entity.get( "userId" ), userId );
-		assertEquals("Entity stock count incorrect", entity.get( "stockCount" ), stockCount );
+		assertEquals( "Entity visits count incorrect", entity.get( "visits_count" ), "444" );
+		assertEquals( "Entity serial number incorrect", entity.get( "serialNumber" ), serialNumber.toString() );
+		assertEquals( "Entity URL incorrect", entity.get( "url" ), "http://www.hibernate.org/" );
+		assertEquals( "Entity site weight incorrect", entity.get( "site_weight" ), "21.77" );
+		assertEquals( "Entity user id incorrect", entity.get( "userId" ), userId );
+		assertEquals( "Entity stock count incorrect", entity.get( "stockCount" ), stockCount );
 
 		session.delete( b );
 		transaction.commit();
@@ -183,7 +184,7 @@ public class BuiltInTypeTest extends OgmTestCase {
 
 	@Override
 	protected Class<?>[] getAnnotatedClasses() {
-		return new Class<?>[] {
+		return new Class<?>[]{
 				Bookmark.class
 		};
 	}
