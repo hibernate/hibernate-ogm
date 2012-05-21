0285be348041798ebeba64c54de2bb9a492cac82
Guillaume Scheibel <guillaume.scheibel@gmail.com>
2012-05-22 01:01:25 +0200

    OGM-174 update test case by adding @OneToMany relation
    update copyright

--- hibernate-ogm-core/src/test/java/org/hibernate/ogm/test/id/News.java
+++ hibernate-ogm-core/src/test/java/org/hibernate/ogm/test/id/News.java
@@ -2,7 +2,7 @@
  * Hibernate, Relational Persistence for Idiomatic Java
  *
  * JBoss, Home of Professional Open Source
- * Copyright 2010-2012 Red Hat Inc. and/or its affiliates and other contributors
+ * Copyright 2012 Red Hat Inc. and/or its affiliates and other contributors
  * as indicated by the @authors tag. All rights reserved.
  * See the copyright.txt in the distribution for a
  * full listing of individual contributors.
@@ -20,8 +20,13 @@
  */
 package org.hibernate.ogm.test.id;
 
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.OneToMany;
 
 /**
  * @author Guillaume Scheibel<guillaume.scheibel@gmail.com>
@@ -29,33 +34,64 @@ import javax.persistence.Entity;
 @Entity
 public class News {
 
+	@EmbeddedId
+	private NewsID newsId;
+	private String content;
+
+	@OneToMany(cascade =  CascadeType.ALL )
+	@JoinColumns({@JoinColumn(name="news_topic_fk", nullable = false) ,
+				  @JoinColumn(name="news_author_fk", nullable = false)})
+	private List<Label> labels;
+
 	public News() {
 		super();
 	}
 
-	public News(NewsID newsId, String content) {
+	public News(NewsID newsId, String content, List<Label> labels) {
 		this.newsId = newsId;
 		this.content = content;
+		this.labels = labels;
 	}
 
-	@EmbeddedId
-	private NewsID newsId;
 
-	public NewsID getNewsId() {
-		return newsId;
+	public NewsID getNewsId() { return newsId; }
+	public void setNewsId(NewsID newsId) { this.newsId = newsId; }
+
+	public String getContent() { return content; }
+	public void setContent(String content) { this.content = content; }
+
+	public List<Label> getLabels() { return labels;	}
+	public void setLabels(List<Label> labels) {	this.labels = labels; }
+
+	@Override
+	public boolean equals(Object o) {
+		if ( this == o ) {
+			return true;
+		}
+		if ( o == null || getClass() != o.getClass() ) {
+			return false;
+		}
+
+		News news = (News) o;
+
+		if ( content != null ? !content.equals( news.content ) : news.content != null ) {
+			return false;
+		}
+		if ( labels != null ? !labels.equals( news.labels ) : news.labels != null ) {
+			return false;
+		}
+		if ( newsId != null ? !newsId.equals( news.newsId ) : news.newsId != null ) {
+			return false;
+		}
+
+		return true;
 	}
 
-	public void setNewsId(NewsID newsId) {
-		this.newsId = newsId;
-	}
-
-	private String content;
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
+	@Override
+	public int hashCode() {
+		int result = newsId != null ? newsId.hashCode() : 0;
+		result = 31 * result + ( content != null ? content.hashCode() : 0 );
+		result = 31 * result + ( labels != null ? labels.hashCode() : 0 );
+		return result;
 	}
 }
