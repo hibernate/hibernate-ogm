31aced7407fb81c5243b6752fbec202542f06dbe
Guillaume Scheibel <guillaume.scheibel@gmail.com>
2012-07-19 19:05:27 +0200

    OGM-173 Add a new context information used to only load useful columns

---
+++ hibernate-ogm-core/src/main/java/org/hibernate/ogm/datastore/spi/AssociationContext.java
@@ -0,0 +1,54 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.ogm.datastore.spi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * It represents all information used to load an association
+ * when the relation between 2 objects is not embedded (IN_ENTITY mode).
+ * Currently, the required information are: _id, rows and columns.
+ * @author Guillaume Scheibel<guillaume.scheibel@gmail.com>
+ */
+public class AssociationContext {
+	private final List<String> selectableColumns;
+
+	public AssociationContext() {
+		this.selectableColumns = new ArrayList<String>();
+		this.selectableColumns.add( "rows" );
+		this.selectableColumns.add( "columns" );
+	}
+
+	public List<String> getSelectableColumns() {
+		return selectableColumns;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder builder = new StringBuilder( "Association Context {" );
+		for ( String columnName : this.getSelectableColumns() ) {
+			builder.append( columnName );
+		}
+		builder.append( "}" );
+		return builder.toString();
+	}
+}
