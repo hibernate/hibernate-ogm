e0dff84bf6a980edf481015697faa2175861e5d1
Emmanuel Bernard <emmanuel@hibernate.org>
2012-07-31 16:29:29 +0200

    OGM-135 Update getting started code sample to make it work again
    - No longer use ORM's platform code as it requires to wire a registry
    - Add necessary dependencies that are no transitively inherited
    - Make JBoss Transaction not store logs

--- hibernate-ogm-documentation/examples/gettingstarted/src/main/java/org/hibernate/ogm/examples/gettingstarted/DogBreedRunner.java
+++ hibernate-ogm-documentation/examples/gettingstarted/src/main/java/org/hibernate/ogm/examples/gettingstarted/DogBreedRunner.java
@@ -1,59 +1,75 @@
 package org.hibernate.ogm.examples.gettingstarted;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.transaction.TransactionManager;
-
 import org.hibernate.ogm.examples.gettingstarted.domain.Breed;
 import org.hibernate.ogm.examples.gettingstarted.domain.Dog;
 import org.hibernate.ogm.util.impl.Log;
 import org.hibernate.ogm.util.impl.LoggerFactory;
-import org.hibernate.service.jta.platform.internal.JBossStandAloneJtaPlatform;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.transaction.TransactionManager;
+import java.lang.reflect.InvocationTargetException;
 
 public class DogBreedRunner {
 
+	private static final String JBOSS_TM_CLASS_NAME = "com.arjuna.ats.jta.TransactionManager";
 	private static final Log logger = LoggerFactory.make();
 
 	public static void main(String[] args) {
-		
-		//accessing JBoss's Transaction can be done differently but this one works nicely
-		TransactionManager tm = new JBossStandAloneJtaPlatform().getTransactionManager();
+
+		TransactionManager tm = getTransactionManager();
 
 		//build the EntityManagerFactory as you would build in in Hibernate Core
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("org.hibernate.ogm.tutorial.jpa");
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory( "org.hibernate.ogm.tutorial.jpa" );
 
 		//Persist entities the way you are used to in plain JPA
 		try {
 			tm.begin();
+			logger.infof( "About to store dog and breed" );
 			EntityManager em = emf.createEntityManager();
 			Breed collie = new Breed();
-			collie.setName("Collie");
-			em.persist(collie);
+			collie.setName( "Collie" );
+			em.persist( collie );
 			Dog dina = new Dog();
-			dina.setName("Dina");
-			dina.setBreed(collie);
-			em.persist(dina);
+			dina.setName( "Dina" );
+			dina.setBreed( collie );
+			em.persist( dina );
 			Long dinaId = dina.getId();
 			em.flush();
 			em.close();
 			tm.commit();
 
 			//Retrieve your entities the way you are used to in plain JPA
+			logger.infof( "About to retrieve dog and breed" );
 			tm.begin();
 			em = emf.createEntityManager();
-			dina = em.find(Dog.class, dinaId);
-			logger.debug(dina.getName());
-			logger.debug(dina.getBreed().getName());
+			dina = em.find( Dog.class, dinaId );
+			logger.infof( "Found dog %s of breed %s", dina.getName(), dina.getBreed().getName() );
 			em.flush();
 			em.close();
 			tm.commit();
 
 			emf.close();
-		} catch (Exception e) {
+		} catch ( Exception e ) {
 			e.printStackTrace();
 		}
 
 	}
 
+	public static TransactionManager getTransactionManager() {
+		try {
+			Class<?> tmClass = DogBreedRunner.class.getClassLoader().loadClass( JBOSS_TM_CLASS_NAME );
+			return (TransactionManager) tmClass.getMethod( "transactionManager" ).invoke( null );
+		} catch ( ClassNotFoundException e ) {
+			e.printStackTrace();
+		} catch ( InvocationTargetException e ) {
+			e.printStackTrace();
+		} catch ( NoSuchMethodException e ) {
+			e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+		} catch ( IllegalAccessException e ) {
+			e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+		}
+		return null;
+	}
 }
