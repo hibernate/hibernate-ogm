437cd320819c62970fa18763b5e50b04c2b0f294
Guillaume Scheibel <guillaume.scheibel@gmail.com>
2012-08-21 18:21:40 +0200

    OGM-201 Create a list of all available shorcuts used to target a datastore provider

---
+++ hibernate-ogm-core/src/main/java/org/hibernate/ogm/datastore/impl/AvailableDatastoreProvider.java
@@ -0,0 +1,37 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.ogm.datastore.impl;
+
+/**
+ * This enumeration describes all available datastore providers by providing some shortcuts.
+ * It's used for the Datastore Provider initialization to find the provider to instantiate.
+ * @author Guillaume Scheibel <guillaume.scheibel@gmail.com>
+ */
+public enum AvailableDatastoreProvider {
+	MAPBASED(  "org.hibernate.ogm.datastore.mapbased.impl.MapBasedDatastoreProvider" ),
+	INFINISPAN( "org.hibernate.ogm.datastore.infinispan.impl.InfinispanDatastoreProvider" ),
+	EHCACHE( "org.hibernate.ogm.datastore.ehcache.impl.EhcacheDatastoreProvider" ),
+	MONGODB( "org.hibernate.ogm.datastore.mongodb.impl.MongoDBDatastoreProvider" );
+
+	private String value;
+	private AvailableDatastoreProvider(String value) { this.value = value; }
+	public String getValue(){ return this.value; }
+}
