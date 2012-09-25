e0fce7dc958086c0537569213772bc91c9c7d10d
Emmanuel Bernard <emmanuel@hibernate.org>
2012-09-25 18:26:24 +0200

    OGM-236 Make texts explicitly chose a association storage strategy

--- hibernate-ogm-mongodb/src/test/java/org/hibernate/ogm/test/mongodb/associations/OneToOneCollectionTest.java
+++ hibernate-ogm-mongodb/src/test/java/org/hibernate/ogm/test/mongodb/associations/OneToOneCollectionTest.java
@@ -18,51 +18,23 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA  02110-1301, USA.
  */
-package org.hibernate.ogm.test.mongodb.loading;
+package org.hibernate.ogm.test.mongodb.associations;
 
 import org.hibernate.cfg.Configuration;
 import org.hibernate.ogm.datastore.mongodb.AssociationStorage;
 import org.hibernate.ogm.datastore.mongodb.Environment;
-import org.hibernate.ogm.datastore.mongodb.impl.MongoDBDatastoreProvider;
-import org.hibernate.ogm.datastore.spi.DatastoreProvider;
-
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
+import org.hibernate.ogm.test.associations.onetoone.OneToOneTest;
 
 /**
- * @author Guillaume Scheibel <guillaume.scheibel@gmail.com>
+ * @author Emmanuel Bernard <emmanuel@hibernate.org>
  */
-public class LoadSelectedColumnsGlobalTest extends LoadSelectedColumnsCollectionTest {
-
+public class OneToOneCollectionTest extends OneToOneTest {
 	@Override
 	protected void configure(Configuration cfg) {
 		super.configure( cfg );
 		cfg.setProperty(
 				Environment.MONGODB_ASSOCIATIONS_STORE,
-				AssociationStorage.GLOBAL_COLLECTION.toString().toLowerCase()
+				AssociationStorage.COLLECTION.toString().toLowerCase()
 		);
-	}
-
-	/**
-	 * To be sure the datastoreProvider retrieves only the columns we want,
-	 * an extra column is manually added to the association document
-	 */
-	@Override
-	protected void addExtraColumn(){
-		MongoDBDatastoreProvider provider = (MongoDBDatastoreProvider) super.getService( DatastoreProvider.class );
-		DB database = provider.getDatabase();
-		DBCollection collection = database.getCollection( "Associations" );
-
-		final BasicDBObject idObject = new BasicDBObject( 2 );
-		idObject.append( "Project_id", "projectID" );
-		idObject.append( "table", "Project_Module" );
-
-		BasicDBObject query = new BasicDBObject( 1 );
-		query.put( "_id", idObject );
-
-		BasicDBObject updater = new BasicDBObject( 1 );
-		updater.put( "$push", new BasicDBObject( "extraColumn", 1 ) );
-		collection.update( query, updater );
 	}
 }
