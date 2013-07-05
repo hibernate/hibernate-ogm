8ac564849cba911e7a1e937f26a472d6f7b08ed4
Emmanuel Bernard <emmanuel@hibernate.org>
2013-07-05 14:54:59 +0200

    OGM-297 Make MongoDBModuleMemberRegistrationIT test use MONGODB_HOSTNAME and MONGODB_PORT

--- hibernate-ogm-integrationtest/hibernate-ogm-integrationtest-mongodb/src/test/java/org/hibernate/ogm/test/integration/jbossas7/MongoDBModuleMemberRegistrationIT.java
+++ hibernate-ogm-integrationtest/hibernate-ogm-integrationtest-mongodb/src/test/java/org/hibernate/ogm/test/integration/jbossas7/MongoDBModuleMemberRegistrationIT.java
@@ -25,8 +25,9 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.descriptor.api.Descriptors;
 import org.jboss.shrinkwrap.descriptor.api.persistence20.PersistenceDescriptor;
+import org.jboss.shrinkwrap.descriptor.api.persistence20.PersistenceUnit;
+import org.jboss.shrinkwrap.descriptor.api.persistence20.Properties;
 import org.junit.runner.RunWith;
-
 import org.hibernate.ogm.test.integration.jbossas7.model.Member;
 import org.hibernate.ogm.test.integration.jbossas7.util.ModuleMemberRegistrationDeployment;
 
@@ -37,6 +38,7 @@ import org.hibernate.ogm.test.integration.jbossas7.util.ModuleMemberRegistration
  */
 @RunWith(Arquillian.class)
 public class MongoDBModuleMemberRegistrationIT extends ModuleMemberRegistrationScenario {
+
 	@Deployment
 	public static Archive<?> createTestArchive() {
 		return new ModuleMemberRegistrationDeployment
@@ -47,15 +49,29 @@ public class MongoDBModuleMemberRegistrationIT extends ModuleMemberRegistrationS
 	}
 
 	private static PersistenceDescriptor persistenceXml() {
-		return Descriptors.create( PersistenceDescriptor.class )
+		String host = System.getenv( "MONGODB_HOSTNAME" );
+		String port = System.getenv( "MONGODB_PORT" );
+
+		Properties<PersistenceUnit<PersistenceDescriptor>> propertiesContext = Descriptors.create( PersistenceDescriptor.class )
 				.version( "2.0" )
 				.createPersistenceUnit()
 				.name( "primary" )
 				.provider( "org.hibernate.ogm.jpa.HibernateOgmPersistence" )
 				.clazz( Member.class.getName() )
-				.getOrCreateProperties()
+				.getOrCreateProperties();
+		if ( isNotNull( host ) ) {
+			propertiesContext.createProperty().name( "hibernate.ogm.mongodb.host" ).value( host );
+		}
+		if ( isNotNull( port ) ) {
+			propertiesContext.createProperty().name( "hibernate.ogm.mongodb.port" ).value( port );
+		}
+		return propertiesContext
 					.createProperty().name( "hibernate.ogm.datastore.provider" ).value( "mongodb" ).up()
 					.createProperty().name( "hibernate.ogm.mongodb.database" ).value( "ogm_test_database" ).up()
 				.up().up();
+	}
+
+	private static boolean isNotNull(String mongoHostName) {
+		return mongoHostName != null && mongoHostName.length() > 0 && ! "null".equals( mongoHostName );
 	}
 }
