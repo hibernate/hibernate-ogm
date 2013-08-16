28da0f4a0effb9bedce0312f0b2e942db55f6de3
Davide D'Alto <davide@hibernate.org>
2013-08-16 11:27:23 +0100

    OGM-208 Add option examples that can be used for testing

---
+++ hibernate-ogm-core/src/test/java/org/hibernate/ogm/test/options/examples/annotations/EmbedExample.java
@@ -0,0 +1,54 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.ogm.test.options.examples.annotations;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import org.hibernate.ogm.options.spi.AnnotationConverter;
+import org.hibernate.ogm.options.spi.MappingOption;
+import org.hibernate.ogm.options.spi.Option;
+import org.hibernate.ogm.test.options.examples.EmbedExampleOption;
+import org.hibernate.ogm.test.options.examples.annotations.EmbedExample.EmbedExampleOptionConverter;
+
+/**
+ * @author Davide D'Alto <davide@hibernate.org>
+ */
+@Target({ TYPE, FIELD })
+@Retention(RUNTIME)
+@MappingOption(EmbedExampleOptionConverter.class)
+public @interface EmbedExample {
+
+	String value();
+
+	static class EmbedExampleOptionConverter implements AnnotationConverter<EmbedExample> {
+
+		@Override
+		public Option<?, ?> convert(EmbedExample annotation) {
+			return new EmbedExampleOption( annotation.value() );
+		}
+
+	}
+}
