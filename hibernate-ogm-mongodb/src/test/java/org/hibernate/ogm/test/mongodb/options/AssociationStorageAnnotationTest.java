a7bde052f445c1e98921c871f906d2550a47cba8
Davide D'Alto <davide@hibernate.org>
2013-08-30 12:57:39 +0100

    OGM-208 Add unit tests for MongoDB mapping

---
+++ hibernate-ogm-mongodb/src/test/java/org/hibernate/ogm/test/mongodb/options/AssociationStorageAnnotationTest.java
@@ -0,0 +1,61 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.ogm.test.mongodb.options;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.hibernate.ogm.test.utils.OptionContainerHelper.retrieveOptionsFor;
+
+import org.hibernate.ogm.datastore.mongodb.AssociationStorageType;
+import org.hibernate.ogm.options.mongodb.AssociationStorage;
+import org.hibernate.ogm.options.mongodb.AssociationStorageOption;
+import org.hibernate.ogm.options.mongodb.mapping.impl.MongoDBMappingServiceFactory;
+import org.hibernate.ogm.options.mongodb.mapping.spi.MongoDBGlobalContext;
+import org.hibernate.ogm.options.navigation.impl.MappingContext;
+import org.junit.Test;
+
+/**
+ * Test the {@link AssociationStorage} annotation used to set the {@link AssociationStorageType} in MongoDB.
+ *
+ * @author Davide D'Alto <davide@hibernate.org>
+ */
+public class AssociationStorageAnnotationTest {
+
+	@Test
+	public void testAssociationStorageMappingOption() throws Exception {
+		MongoDBMappingServiceFactory factory = new MongoDBMappingServiceFactory();
+		MappingContext context = new MappingContext();
+		MongoDBGlobalContext mapping = factory.createMapping( context );
+		mapping
+			.entity( EntityAnnotated.class );
+
+		assertThat( retrieveOptionsFor( context, EntityAnnotated.class, "content" ) )
+			.hasSize( 1 )
+			.contains( new AssociationStorageOption( AssociationStorageType.IN_ENTITY ) );
+	}
+
+	private static final class EntityAnnotated {
+
+		@AssociationStorage(AssociationStorageType.IN_ENTITY)
+		public String content;
+
+	}
+
+}
