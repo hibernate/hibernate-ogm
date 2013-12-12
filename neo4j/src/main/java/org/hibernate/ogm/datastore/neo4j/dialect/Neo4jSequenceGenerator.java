c7ceba12d8fba264f01d059abe38cfa3a23ac816
Davide D'Alto <davide@hibernate.org>
2013-12-12 11:44:12 +0000

    OGM-367 Move Neo4SequenceGenerator and Neo4jTypeConverter in the dialect package

--- neo4j/src/main/java/org/hibernate/ogm/datastore/neo4j/dialect/Neo4jSequenceGenerator.java
+++ neo4j/src/main/java/org/hibernate/ogm/datastore/neo4j/dialect/Neo4jSequenceGenerator.java
@@ -18,7 +18,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA  02110-1301, USA.
  */
-package org.hibernate.ogm.datastore.neo4j.impl;
+package org.hibernate.ogm.datastore.neo4j.dialect;
 
 import java.util.Map;
 
