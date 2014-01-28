e60a5cfb0da75aec96bcded41126a65b80c4a9cb
Gunnar Morling <gunnar.morling@googlemail.com>
2014-01-28 10:24:15 +0100

    OGM-408 Incorporating misc. review feedback
    * Making OGM_ON an internal property
    * Making QUERY_PARSER_SERVICE an internal property
    * Avoiding repeated retrieval of CL service from registry

--- core/src/main/java/org/hibernate/ogm/cfg/impl/InternalProperties.java
+++ core/src/main/java/org/hibernate/ogm/cfg/impl/InternalProperties.java
@@ -2,7 +2,7 @@
  * Hibernate, Relational Persistence for Idiomatic Java
  *
  * JBoss, Home of Professional Open Source
- * Copyright 2013 Red Hat Inc. and/or its affiliates and other contributors
+ * Copyright 2013-2014 Red Hat Inc. and/or its affiliates and other contributors
  * as indicated by the @authors tag. All rights reserved.
  * See the copyright.txt in the distribution for a
  * full listing of individual contributors.
@@ -27,11 +27,21 @@ package org.hibernate.ogm.cfg.impl;
  */
 public class InternalProperties {
 
+	public static final String OGM_ON = "hibernate.ogm._activate";
+
 	/**
 	 * Name of the configuration option for passing in set up
 	 * {@link org.hibernate.ogm.options.spi.OptionsService.OptionsServiceContext}s.
 	 */
 	public static final String OGM_OPTION_CONTEXT = "hibernate.ogm.options.context";
+
+	/**
+	 * Name of the configuration option for specifying the {@link org.hibernate.ogm.service.impl.QueryParserService} to
+	 * be used. Accepts a fully-qualified class name. If not specified, the parser type returned by
+	 * {@link org.hibernate.ogm.datastore.spi.DatastoreProvider#getDefaultQueryParserServiceType()}
+	 * will be used.
+	 */
+	public static final String QUERY_PARSER_SERVICE = "hibernate.ogm.query.parser";
 
 	private InternalProperties() {
 	}
