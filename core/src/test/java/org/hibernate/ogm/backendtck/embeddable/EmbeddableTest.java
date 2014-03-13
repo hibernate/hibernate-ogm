bcae3fb9e848d3f74920994e13606d04f6327351
Gunnar Morling <gunnar.morling@googlemail.com>
2014-03-13 20:08:18 +0100

    OGM-251 Using only actual property names as column names for element collections of embeddedable types

--- core/src/test/java/org/hibernate/ogm/backendtck/embeddable/EmbeddableTest.java
+++ core/src/test/java/org/hibernate/ogm/backendtck/embeddable/EmbeddableTest.java
@@ -2,7 +2,7 @@
  * Hibernate, Relational Persistence for Idiomatic Java
  *
  * JBoss, Home of Professional Open Source
- * Copyright 2010-2011 Red Hat Inc. and/or its affiliates and other contributors
+ * Copyright 2010-2014 Red Hat Inc. and/or its affiliates and other contributors
  * as indicated by the @authors tag. All rights reserved.
  * See the copyright.txt in the distribution for a
  * full listing of individual contributors.
@@ -28,7 +28,10 @@ import org.hibernate.ogm.utils.OgmTestCase;
 import org.junit.Test;
 
 /**
+ * Tests for {@code @Embeddable} types and {@code @ElementCollection}s there-of.
+ *
  * @author Emmanuel Bernard
+ * @Gunnar Morling
  */
 public class EmbeddableTest extends OgmTestCase {
 
@@ -84,8 +87,76 @@ public class EmbeddableTest extends OgmTestCase {
 		session.close();
 	}
 
+	@Test
+	public void testElementCollectionOfEmbeddable() throws Exception {
+		final Session session = openSession();
+
+		Transaction transaction = session.beginTransaction();
+
+		Address address = new Address();
+		address.setCity( "Paris" );
+		address.setCountry( "France" );
+		address.setStreet1( "1 avenue des Champs Elysees" );
+		address.setZipCode( "75007" );
+
+		Address anotherAddress = new Address();
+		anotherAddress.setCity( "Rome" );
+		anotherAddress.setCountry( "Italy" );
+		anotherAddress.setStreet1( "Piazza del Colosseo, 1" );
+		anotherAddress.setZipCode( "00184" );
+
+		MultiAddressAccount account = new MultiAddressAccount();
+		account.setLogin( "gunnar" );
+		account.setPassword( "highly secret" );
+		account.getAddresses().add( address );
+		account.getAddresses().add( anotherAddress );
+
+		session.persist( account );
+		transaction.commit();
+
+		session.clear();
+
+		transaction = session.beginTransaction();
+		MultiAddressAccount loadedAccount = (MultiAddressAccount) session.get( MultiAddressAccount.class, account.getLogin() );
+		assertThat( loadedAccount ).as( "Cannot load persisted object" ).isNotNull();
+		assertThat( loadedAccount.getAddresses() ).onProperty( "city" ).contains( "Paris", "Rome" );
+		assertThat( loadedAccount.getAddresses() ).onProperty( "zipCode" ).contains( "75007", "00184" );
+		assertThat( loadedAccount.getAddresses() ).onProperty( "country" ).contains( "France", "Italy" );
+
+		Address loadedAddress1 = loadedAccount.getAddresses().get( 0 );
+		Address loadedAddress2 = loadedAccount.getAddresses().get( 1 );
+
+		transaction.commit();
+
+		session.clear();
+
+		transaction = session.beginTransaction();
+		loadedAddress1.setCountry( "USA" );
+		loadedAddress2.setCountry( "Germany" );
+
+		session.merge( loadedAccount );
+		transaction.commit();
+
+		session.clear();
+
+		transaction = session.beginTransaction();
+		MultiAddressAccount secondLoadedAccount = (MultiAddressAccount) session.get( MultiAddressAccount.class, account.getLogin() );
+		assertThat( secondLoadedAccount.getAddresses() ).onProperty( "city" ).contains( "Paris", "Rome" );
+		assertThat( secondLoadedAccount.getAddresses() ).onProperty( "country" ).contains( "USA", "Germany" );
+		session.delete( secondLoadedAccount );
+		transaction.commit();
+
+		session.clear();
+
+		transaction = session.beginTransaction();
+		assertThat( session.get( MultiAddressAccount.class, account.getLogin() ) ).isNull();
+		transaction.commit();
+
+		session.close();
+	}
+
 	@Override
 	protected Class<?>[] getAnnotatedClasses() {
-		return new Class<?>[] { Account.class };
+		return new Class<?>[] { Account.class, MultiAddressAccount.class };
 	}
 }
