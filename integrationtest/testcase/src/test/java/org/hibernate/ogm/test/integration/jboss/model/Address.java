bbab0ab52bede5a38ad4cf5a21e6bdaaf0462d8e
Davide D'Alto <davide@hibernate.org>
2014-03-20 11:03:13 +0000

    OGM-482 Rename wildfly folder to jboss in integration test project

--- integrationtest/testcase/src/test/java/org/hibernate/ogm/test/integration/jboss/model/Address.java
+++ integrationtest/testcase/src/test/java/org/hibernate/ogm/test/integration/jboss/model/Address.java
@@ -18,7 +18,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA  02110-1301, USA.
  */
-package org.hibernate.ogm.test.integration.wildfly.model;
+package org.hibernate.ogm.test.integration.wildlfy.model;
 
 import javax.persistence.Embeddable;
 
