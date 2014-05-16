348584e190387a77188b7876277ac17d3798f2ec
Davide D'Alto <davide@hibernate.org>
2014-05-16 12:17:18 +0100

    OGM-367 Add tests in CorrectNumberOfElementsTest
      Add assertions to verify the relationship connect the right nodes and
      that the elements have the expected properties and labels.

--- neo4j/src/test/java/org/hibernate/ogm/datastore/neo4j/test/CorrectNumberOfElementsTest.java
+++ neo4j/src/test/java/org/hibernate/ogm/datastore/neo4j/test/CorrectNumberOfElementsTest.java
@@ -32,13 +32,14 @@ import org.hibernate.ogm.backendtck.id.DistributedRevisionControl;
 import org.hibernate.ogm.backendtck.id.Label;
 import org.hibernate.ogm.backendtck.id.News;
 import org.hibernate.ogm.backendtck.id.NewsID;
+import org.hibernate.ogm.datastore.neo4j.dialect.impl.Neo4jSequenceGenerator;
 import org.hibernate.ogm.utils.jpa.JpaTestCase;
 import org.junit.After;
 import org.junit.Test;
 
 /**
- * These tests only purpose is to check the the right number of nodes and relationships is created in the different
- * scenarios.
+ * These tests check that the expected number of nodes and relationship is created on the db. They also checks for the
+ * existence of the expected attributes and labels.
  *
  * @author Davide D'Alto
  */
@@ -56,6 +57,8 @@ public class CorrectNumberOfElementsTest extends JpaTestCase {
 
 		assertNumberOfNodes( 1 );
 		assertRelationships( 0 );
+
+		assertExpectedMapping( "(n:JUG:ENTITY {jug_id: '" + jug.getId() + "', name: '" + jug.getName() + "' })" );
 	}
 
 	@Test
@@ -71,6 +74,10 @@ public class CorrectNumberOfElementsTest extends JpaTestCase {
 		// 1 node for the sequence + 1 node for the entity
 		assertNumberOfNodes( 2 );
 		assertRelationships( 0 );
+
+		assertExpectedMapping( "(:DistributedRevisionControl:ENTITY {id: " + git.getId() + ", name: '" + git.getName() + "' })" );
+		assertExpectedMapping( "(:DistributedRevisionControl:hibernate_sequences {" + Neo4jSequenceGenerator.SEQUENCE_NAME_PROPERTY
+				+ ": 'DistributedRevisionControl', DistributedRevisionControl: 2 })" );
 	}
 
 	@Test
@@ -97,6 +104,18 @@ public class CorrectNumberOfElementsTest extends JpaTestCase {
 
 		assertNumberOfNodes( 3 );
 		assertRelationships( 4 );
+
+		String ownerNode = "(owner:AccountOwner:ENTITY {id: '" + owner.getId() + "', SSN: '" + owner.getSSN() + "' })";
+		String barcklaysNode = "(barclays:BankAccount:ENTITY {id: '" + barclays.getId() + "', accountNumber: '" + barclays.getAccountNumber() + "' })";
+		String sogeNode = "(soge:BankAccount:ENTITY {id: '" + soge.getId() + "', accountNumber: '" + soge.getAccountNumber() + "' })";
+
+		// Bidirectional relationship with Barcklays
+		assertExpectedMapping( ownerNode + "- [:bankAccounts] -> " + barcklaysNode );
+		assertExpectedMapping( barcklaysNode + " - [:owners] -> " + ownerNode );
+
+		// Bidirectional relationship with Sorge
+		assertExpectedMapping( ownerNode + " - [:bankAccounts] -> " + sogeNode );
+		assertExpectedMapping( sogeNode + " - [:owners] -> " + ownerNode );
 	}
 
 	@Test
@@ -123,6 +142,10 @@ public class CorrectNumberOfElementsTest extends JpaTestCase {
 
 		assertNumberOfNodes( 3 );
 		assertRelationships( 0 );
+
+		assertExpectedMapping( "(n:JUG:ENTITY {jug_id: '" + jug.getId() + "', name: '" + jug.getName() + "' })" );
+		assertExpectedMapping( "(n:Member:ENTITY {member_id: '" + emmanuel.getId() + "', name: '" + emmanuel.getName() + "', memberOf_jug_id: '" + jug.getId() + "' })" );
+		assertExpectedMapping( "(n:Member:ENTITY {member_id: '" + jerome.getId() + "' , name: '" + jerome.getName() + "', memberOf_jug_id: '" + jug.getId() + "' })" );
 	}
 
 	@Test
@@ -130,9 +153,9 @@ public class CorrectNumberOfElementsTest extends JpaTestCase {
 		getTransactionManager().begin();
 		final EntityManager em = getFactory().createEntityManager();
 
-		SnowFlake sf = new SnowFlake();
-		sf.setDescription( "Snowflake 1" );
-		em.persist( sf );
+		SnowFlake sf1 = new SnowFlake();
+		sf1.setDescription( "Snowflake 1" );
+		em.persist( sf1 );
 
 		SnowFlake sf2 = new SnowFlake();
 		sf2.setDescription( "Snowflake 2" );
@@ -140,7 +163,7 @@ public class CorrectNumberOfElementsTest extends JpaTestCase {
 
 		Cloud cloud = new Cloud();
 		cloud.setLength( 23 );
-		cloud.getProducedSnowFlakes().add( sf );
+		cloud.getProducedSnowFlakes().add( sf1 );
 		cloud.getProducedSnowFlakes().add( sf2 );
 
 		em.persist( cloud );
@@ -148,6 +171,13 @@ public class CorrectNumberOfElementsTest extends JpaTestCase {
 
 		assertNumberOfNodes( 3 );
 		assertRelationships( 2 );
+
+		String cloudNode = "(:Cloud:ENTITY {id: '" + cloud.getId() + "', length: 23 })";
+		String sf1Node = "(sf1:SnowFlake:ENTITY {id: '" + sf1.getId() + "', description: '" + sf1.getDescription() + "' })";
+		String sf2Node = "(sf2:SnowFlake:ENTITY {id: '" + sf2.getId() + "', description: '" + sf2.getDescription() + "' })";
+
+		assertExpectedMapping( cloudNode + " - [:producedSnowFlakes] -> " + sf1Node );
+		assertExpectedMapping( cloudNode + " - [:producedSnowFlakes] -> " + sf2Node );
 	}
 
 	@Test
@@ -163,11 +193,11 @@ public class CorrectNumberOfElementsTest extends JpaTestCase {
 		child2.setName( "Christophe" );
 		em.persist( child2 );
 
-		Father father = new Father();
-		father.getOrderedChildren().add( child1 );
-		father.getOrderedChildren().add( child2 );
+		Father father1 = new Father();
+		father1.getOrderedChildren().add( child1 );
+		father1.getOrderedChildren().add( child2 );
 
-		em.persist( father );
+		em.persist( father1 );
 
 		Child child3 = new Child();
 		child3.setName( "Caroline" );
@@ -186,44 +216,20 @@ public class CorrectNumberOfElementsTest extends JpaTestCase {
 
 		assertNumberOfNodes( 6 );
 		assertRelationships( 4 );
-	}
 
-	private void assertRelationships(int rel) throws Exception {
-		assertThat( numberOfRelationships() ).as( "Unexpected number of relationships" ).isEqualTo( rel );
-	}
+		String father1Node = "(:Father:ENTITY {id: '" + father1.getId() + "' })";
+		String child1Node = "(:Child:ENTITY {id: '" + child1.getId() + "', name: '" + child1.getName() + "' })";
+		String child2Node = "(:Child:ENTITY {id: '" + child2.getId() + "', name: '" + child2.getName() + "' })";
 
-	private void assertNumberOfNodes(int nodes) throws Exception {
-		assertThat( numberOfNodes() ).as( "Unexpected number of nodes" ).isEqualTo( nodes );
-	}
+		assertExpectedMapping( father1Node + " - [:orderedChildren {birthorder: 0}] -> " + child1Node );
+		assertExpectedMapping( father1Node + " - [:orderedChildren {birthorder: 1}] -> " + child2Node );
 
-	@After
-	public void deleteAll() throws Exception {
-		executeQuery( "MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n, r" );
-	}
+		String father2Node = "(:Father:ENTITY {id: '" + father2.getId() + "' })";
+		String child3Node = "(:Child:ENTITY {id: '" + child3.getId() + "', name: '" + child3.getName() + "' })";
+		String child4Node = "(:Child:ENTITY {id: '" + child4.getId() + "', name: '" + child4.getName() + "' })";
 
-	private Long numberOfNodes() throws Exception {
-		return executeQuery( "MATCH (n) RETURN COUNT(*)" );
-	}
-
-	private Long numberOfRelationships() throws Exception {
-		return executeQuery( "MATCH (n) - [r] -> () RETURN COUNT(r)" );
-	}
-
-	private Long executeQuery(String queryString) throws Exception {
-		getTransactionManager().begin();
-		final EntityManager em = getFactory().createEntityManager();
-		@SuppressWarnings("unchecked")
-		List<Object> results = em.createNativeQuery( queryString ).getResultList();
-		Long uniqueResult = null;
-		if ( !results.isEmpty() ) {
-			uniqueResult = (Long) ( (Object[]) results.get( 0 ) )[0];
-		}
-		commitOrRollback( true );
-		em.close();
-		if ( uniqueResult == null ) {
-			return null;
-		}
-		return uniqueResult;
+		assertExpectedMapping( father2Node + " - [:orderedChildren {birthorder: 0}] -> " + child3Node );
+		assertExpectedMapping( father2Node + " - [:orderedChildren {birthorder: 1}] -> " + child4Node );
 	}
 
 	@Test
@@ -272,6 +278,15 @@ public class CorrectNumberOfElementsTest extends JpaTestCase {
 
 		assertNumberOfNodes( 1 );
 		assertRelationships( 0 );
+
+		assertExpectedMapping( "(n:Account:ENTITY {"
+				+ "  `login`: '" + account.getLogin() + "'"
+				+ ", `password`: '" + account.getPassword() + "'"
+				+ ", `homeAddress.street1`: '" + account.getHomeAddress().getStreet1() + "'"
+				+ ", `homeAddress.city`: '" + account.getHomeAddress().getCity() + "'"
+				+ ", `homeAddress.country`: '" + account.getHomeAddress().getCountry() + "'"
+				+ ", `postal_code`: '" + account.getHomeAddress().getZipCode() + "'"
+				+ " })" );
 	}
 
 	@Test
@@ -303,6 +318,118 @@ public class CorrectNumberOfElementsTest extends JpaTestCase {
 
 		assertNumberOfNodes( 3 );
 		assertRelationships( 2 );
+
+		String accountNode = "(:MultiAddressAccount:ENTITY {"
+				+ "  `login`: '" + account.getLogin() + "'"
+				+ ", `password`: '" + account.getPassword() + "'"
+				+ " })";
+
+		String addressNode = "(:MultiAddressAccount_addresses {"
+				+ "  street1: '" + address.getStreet1() + "'"
+				+ ", city: '" + address.getCity() + "'"
+				+ ", country: '" + address.getCountry() + "'"
+				+ ", postal_code: '" + address.getZipCode() + "'"
+				+ " })";
+
+		String anotherAddressNode = "(:MultiAddressAccount_addresses {"
+				+ "  street1: '" + anotherAddress.getStreet1() + "'"
+				+ ", city: '" + anotherAddress.getCity() + "'"
+				+ ", country: '" + anotherAddress.getCountry() + "'"
+				+ ", postal_code: '" + anotherAddress.getZipCode() + "'"
+				+ " })";
+
+		assertExpectedMapping( accountNode + " - [:addresses] -> " + addressNode );
+		assertExpectedMapping( accountNode + " - [:addresses] -> " + anotherAddressNode );
+	}
+
+	@Test
+	public void testBidirectionalManyToOneRegular() throws Exception {
+		getTransactionManager().begin();
+		EntityManager em = getFactory().createEntityManager();
+
+		SalesForce force = new SalesForce();
+		force.setCorporation( "Red Hat" );
+		em.persist( force );
+
+		SalesGuy eric = new SalesGuy();
+		eric.setName( "Eric" );
+		eric.setSalesForce( force );
+		force.getSalesGuys().add( eric );
+		em.persist( eric );
+
+		SalesGuy simon = new SalesGuy();
+		simon.setName( "Simon" );
+		simon.setSalesForce( force );
+		force.getSalesGuys().add( simon );
+		em.persist( simon );
+
+		commitOrRollback( true );
+		em.close();
+
+		assertNumberOfNodes( 3 );
+		assertRelationships( 2 );
+
+		String forceNode = "(:SalesForce:ENTITY {"
+				+ "  id: '" + force.getId() + "'"
+				+ ", corporation: '" + force.getCorporation() + "'"
+				+ " })";
+
+		String ericNode = "(:SalesGuy:ENTITY {"
+				+ "  id: '" + eric.getId() + "'"
+				+ ", name: '" + eric.getName() + "'"
+				+ ", salesForce_id: '" + force.getId() + "'"
+				+ " })";
+
+		String simonNode = "(:SalesGuy:ENTITY {"
+				+ "  id: '" + simon.getId() + "'"
+				+ ", name: '" + simon.getName() + "'"
+				+ ", salesForce_id: '" + force.getId() + "'"
+				+ " })";
+
+		assertExpectedMapping( forceNode + " - [:salesGuys] -> " + ericNode );
+		assertExpectedMapping( forceNode + " - [:salesGuys] -> " + simonNode );
+	}
+
+	private void assertRelationships(int rel) throws Exception {
+		assertThat( numberOfRelationships() ).as( "Unexpected number of relationships" ).isEqualTo( rel );
+	}
+
+	private void assertNumberOfNodes(int nodes) throws Exception {
+		assertThat( numberOfNodes() ).as( "Unexpected number of nodes" ).isEqualTo( nodes );
+	}
+
+	private void assertExpectedMapping(String element) throws Exception {
+		assertThat( executeQuery( "MATCH " + element + " RETURN 1" ) ).as( "Not found in the db: " + element ).isNotNull();
+	}
+
+	@After
+	public void deleteAll() throws Exception {
+		executeQuery( "MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n, r" );
+	}
+
+	private Long numberOfNodes() throws Exception {
+		return executeQuery( "MATCH (n) RETURN COUNT(*)" );
+	}
+
+	private Long numberOfRelationships() throws Exception {
+		return executeQuery( "MATCH (n) - [r] -> () RETURN COUNT(r)" );
+	}
+
+	private Long executeQuery(String queryString) throws Exception {
+		getTransactionManager().begin();
+		final EntityManager em = getFactory().createEntityManager();
+		@SuppressWarnings("unchecked")
+		List<Object> results = em.createNativeQuery( queryString ).getResultList();
+		Long uniqueResult = null;
+		if ( !results.isEmpty() ) {
+			uniqueResult = (Long) ( (Object[]) results.get( 0 ) )[0];
+		}
+		commitOrRollback( true );
+		em.close();
+		if ( uniqueResult == null ) {
+			return null;
+		}
+		return uniqueResult;
 	}
 
 	private List<Label> labels(String... names) {
