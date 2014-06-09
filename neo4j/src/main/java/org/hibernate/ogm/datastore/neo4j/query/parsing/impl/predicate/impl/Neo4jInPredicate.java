7576854476e61459f524b0baa32319aac755e02f
Davide D'Alto <davide@hibernate.org>
2014-06-09 17:00:33 +0100

    OMG-489 Create new classes for the Neo4j query parser

---
+++ neo4j/src/main/java/org/hibernate/ogm/datastore/neo4j/query/parsing/impl/predicate/impl/Neo4jInPredicate.java
@@ -0,0 +1,60 @@
+/*
+ * Hibernate OGM, Domain model persistence for NoSQL datastores
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.ogm.datastore.neo4j.query.parsing.impl.predicate.impl;
+
+import static org.neo4j.cypherdsl.CypherQuery.any;
+import static org.neo4j.cypherdsl.CypherQuery.collection;
+import static org.neo4j.cypherdsl.CypherQuery.has;
+import static org.neo4j.cypherdsl.CypherQuery.identifier;
+import static org.neo4j.cypherdsl.CypherQuery.none;
+import static org.neo4j.cypherdsl.CypherQuery.not;
+
+import java.util.List;
+
+import org.hibernate.hql.ast.spi.predicate.InPredicate;
+import org.hibernate.hql.ast.spi.predicate.NegatablePredicate;
+import org.neo4j.cypherdsl.Property;
+import org.neo4j.cypherdsl.expression.BooleanExpression;
+
+/**
+ * @author Davide D'Alto <davide@hibernate.org>
+ */
+public class Neo4jInPredicate extends InPredicate<BooleanExpression> implements NegatablePredicate<BooleanExpression> {
+
+	private static final String X = "_x_";
+	private final String alias;
+
+	public Neo4jInPredicate(String alias, String columnName, List<Object> values) {
+		super( columnName, values );
+		this.alias = alias;
+	}
+
+	/**
+	 * ANY( x IN values WHERE n.propertyName = x)
+	 */
+	@Override
+	public BooleanExpression getQuery() {
+		return any( X, collection( values() ), property().eq( identifier( X ) ) );
+	}
+
+	/**
+	 * NOT HAS n.propertyName OR NONE( x IN values WHERE n.propertyName = x)
+	 */
+	@Override
+	public BooleanExpression getNegatedQuery() {
+		return not( has( property() ) ).or( none( X, collection( values() ), property().eq( identifier( X ) ) ) );
+	}
+
+	private Property property() {
+		return identifier( alias ).property( propertyName );
+	}
+
+	private Object[] values() {
+		return values.toArray( new Object[values.size()] );
+	}
+
+}
---
+++ neo4j/src/main/java/org/hibernate/ogm/datastore/neo4j/query/parsing/impl/predicate/impl/Neo4jNegationPredicate.java
@@ -0,0 +1,34 @@
+/*
+ * Hibernate OGM, Domain model persistence for NoSQL datastores
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.ogm.datastore.neo4j.query.parsing.impl.predicate.impl;
+
+import static org.neo4j.cypherdsl.CypherQuery.not;
+
+import org.hibernate.hql.ast.spi.predicate.NegatablePredicate;
+import org.hibernate.hql.ast.spi.predicate.NegationPredicate;
+import org.neo4j.cypherdsl.expression.BooleanExpression;
+
+/**
+ * @author Davide D'Alto <davide@hibernate.org>
+ */
+public class Neo4jNegationPredicate extends NegationPredicate<BooleanExpression> implements NegatablePredicate<BooleanExpression> {
+
+	@Override
+	public BooleanExpression getQuery() {
+		if ( getChild() instanceof NegatablePredicate ) {
+			NegatablePredicate<BooleanExpression> negatable = (NegatablePredicate<BooleanExpression>) getChild();
+			return negatable.getNegatedQuery();
+		}
+		return not( getChild().getQuery() );
+	}
+
+	@Override
+	public BooleanExpression getNegatedQuery() {
+		return getChild().getQuery();
+	}
+
+}
