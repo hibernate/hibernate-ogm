1869e8584be2319f2b9a1b958a031b2cd83787f2
Davide D'Alto <davide@hibernate.org>
2014-08-06 16:51:18 +0100

    OGM-548 Add ArrayHelper#indexOf

--- core/src/main/java/org/hibernate/ogm/util/impl/ArrayHelper.java
+++ core/src/main/java/org/hibernate/ogm/util/impl/ArrayHelper.java
@@ -14,6 +14,7 @@ import java.util.Collection;
 public class ArrayHelper {
 	public static final int[] EMPTY_INT_ARRAY = {};
 	public static final String[] EMPTY_STRING_ARRAY = {};
+	public static final Object[] EMPTY_OBJECT_ARRAY = {};
 
 	public static String[] toStringArray(Collection coll) {
 		return (String[]) coll.toArray( new String[coll.size()] );
@@ -27,5 +28,21 @@ public class ArrayHelper {
 		String[] result = new String[length];
 		System.arraycopy( strings, begin, result, 0, length );
 		return result;
+	}
+
+	/**
+	 * Return the position of an element inside an array
+	 *
+	 * @param array the array where it looks for an element
+	 * @param element the element to find in the array
+	 * @return the position of the element if it's found in the array, -1 otherwise
+	 */
+	public static <T> int indexOf(T[] array, T element) {
+		for ( int i = 0; i < array.length; i++ ) {
+			if (array[i].equals( element )) {
+				return i;
+			}
+		}
+		return -1;
 	}
 }
