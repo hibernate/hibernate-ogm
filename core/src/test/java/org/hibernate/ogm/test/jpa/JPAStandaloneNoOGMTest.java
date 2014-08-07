5442ae3c50ae411fe7f6a4c91466f652894d1e5a
Gunnar Morling <gunnar.morling@googlemail.com>
2014-08-07 10:03:14 +0200

    OGM-369 Re-enabling test

--- core/src/test/java/org/hibernate/ogm/test/jpa/JPAStandaloneNoOGMTest.java
+++ core/src/test/java/org/hibernate/ogm/test/jpa/JPAStandaloneNoOGMTest.java
@@ -10,7 +10,6 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import org.hibernate.ogm.utils.PackagingRule;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -28,8 +27,6 @@ public class JPAStandaloneNoOGMTest {
 	public ExpectedException error = ExpectedException.none();
 
 	@Test
-	@Ignore
-	//Temp. disabled due to HHH-8636; ORM lets plain HibernateExceptions pass through
 	public void testJTAStandaloneNoOgm() throws Exception {
 		// Failure is expected as we didn't configure a JDBC connection nor a Dialect
 		// (and this would fail only if effectively loading Hibernate ORM without OGM superpowers)
