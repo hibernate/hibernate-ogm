ae2c595727691727d9c6dc769693e3a701df4871
Gunnar Morling <gunnar.morling@googlemail.com>
2014-09-10 11:40:57 +0200

    OGM-454 Moving AssociationStorageConverter back to "impl"

--- core/src/main/java/org/hibernate/ogm/datastore/document/options/impl/AssociationStorageConverter.java
+++ core/src/main/java/org/hibernate/ogm/datastore/document/options/impl/AssociationStorageConverter.java
@@ -4,9 +4,10 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.ogm.datastore.document.options.spi;
+package org.hibernate.ogm.datastore.document.options.impl;
 
 import org.hibernate.ogm.datastore.document.options.AssociationStorage;
+import org.hibernate.ogm.datastore.document.options.spi.AssociationStorageOption;
 import org.hibernate.ogm.options.spi.AnnotationConverter;
 import org.hibernate.ogm.options.spi.OptionValuePair;
 
