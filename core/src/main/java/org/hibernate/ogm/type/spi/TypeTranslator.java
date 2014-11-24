bfa60a7e6128286b1f2be0b95ca1761808b1b312
Gunnar Morling <gunnar.morling@googlemail.com>
2014-11-24 09:51:36 +0100

    OGM-649 Logging entity/property name in case we cannot find a GridType

--- core/src/main/java/org/hibernate/ogm/type/spi/TypeTranslator.java
+++ core/src/main/java/org/hibernate/ogm/type/spi/TypeTranslator.java
@@ -6,12 +6,23 @@
  */
 package org.hibernate.ogm.type.spi;
 
+import org.hibernate.HibernateException;
 import org.hibernate.service.Service;
 import org.hibernate.type.Type;
 
 /**
+ * Translates {@link Type}s from Hibernate ORM to corresponding {@link GridType}s from Hibernate OGM.
+ *
  * @author Emmanuel Bernard &lt;emmanuel@hibernate.org&gt;
  */
 public interface TypeTranslator extends Service {
+
+	/**
+	 * Returns the Hibernate OGM type corresponding to the given ORM type.
+	 *
+	 * @param type The type from Hibernate ORM
+	 * @return Hibernate OGM's corresponding type
+	 * @throws HibernateException In case no matching type could be found
+	 */
 	GridType getType(Type type);
 }
