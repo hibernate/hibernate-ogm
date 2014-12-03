2bb727914ea7721b1e032cdd733494b07bf70cdc
Gunnar Morling <gunnar.morling@googlemail.com>
2014-12-03 22:53:16 +0100

    OGM-618 Don't store null values in entity documents for MongoDB

---
+++ mongodb/src/test/java/org/hibernate/ogm/datastore/mongodb/test/embeddable/EmbeddableMappingTest.java
@@ -0,0 +1,126 @@
+/*
+ * Hibernate OGM, Domain model persistence for NoSQL datastores
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.ogm.datastore.mongodb.test.embeddable;
+
+import static org.hibernate.ogm.datastore.mongodb.utils.MongoDBTestHelper.assertDbObject;
+
+import org.hibernate.Transaction;
+import org.hibernate.ogm.OgmSession;
+import org.hibernate.ogm.backendtck.embeddable.Account;
+import org.hibernate.ogm.backendtck.embeddable.Address;
+import org.hibernate.ogm.backendtck.embeddable.AddressType;
+import org.hibernate.ogm.utils.OgmTestCase;
+import org.junit.Test;
+
+/**
+ * Tests for {@code @Embeddable} types and {@code @ElementCollection}s there-of.
+ *
+ * @author Emmanuel Bernard
+ * @author Gunnar Morling
+ */
+public class EmbeddableMappingTest extends OgmTestCase {
+
+	@Test
+	public void testEmbeddable() throws Exception {
+		OgmSession session = openSession();
+
+		Transaction transaction = session.beginTransaction();
+
+		// Given, When
+		Account account = new Account();
+		account.setLogin( "emmanuel" );
+		account.setPassword( "like I would tell ya" );
+		account.setHomeAddress( new Address() );
+		final Address address = account.getHomeAddress();
+		address.setCity( "Paris" );
+		address.setCountry( "France" );
+		address.setStreet1( "1 avenue des Champs Elysees" );
+		address.setZipCode( "75007" );
+		address.setType( new AddressType( "main" ) );
+		session.persist( account );
+		transaction.commit();
+
+		session.clear();
+
+		transaction = session.beginTransaction();
+
+		// Then
+		assertDbObject(
+				session.getSessionFactory(),
+				// collection
+				"Account",
+				// query
+				"{ '_id' : 'emmanuel' }",
+				// expected
+				"{ " +
+					"'_id' : 'emmanuel', " +
+					"'homeAddress' : {" +
+						"'city' : 'Paris', " +
+						"'country' : 'France', " +
+						"'street1' : '1 avenue des Champs Elysees'," +
+						"'type' : {" +
+							"'name' : 'main'" +
+						"}" +
+					"}, " +
+					"'postal_code' : '75007', " +
+					"'password' : 'like I would tell ya' " +
+				"}"
+		);
+
+		transaction.commit();
+		session.clear();
+
+		transaction = session.beginTransaction();
+
+		Account loadedAccount = (Account) session.get( Account.class, account.getLogin() );
+
+		// When
+		loadedAccount.getHomeAddress().setCountry( null );
+		loadedAccount.setPassword( null );
+		session.merge( loadedAccount );
+
+		transaction.commit();
+
+		transaction = session.beginTransaction();
+
+		// Then
+		assertDbObject(
+				session.getSessionFactory(),
+				// collection
+				"Account",
+				// query
+				"{ '_id' : 'emmanuel' }",
+				// expected
+				"{ " +
+					"'_id' : 'emmanuel', " +
+					"'homeAddress' : {" +
+						"'city' : 'Paris', " +
+						"'street1' : '1 avenue des Champs Elysees'," +
+						"'type' : {" +
+							"'name' : 'main'" +
+						"}" +
+					"}, " +
+					"'postal_code' : '75007' " +
+				"}"
+		);
+
+		transaction.commit();
+
+		// Clean-Up
+		transaction = session.beginTransaction();
+		loadedAccount = (Account) session.get( Account.class, account.getLogin() );
+		session.delete( loadedAccount );
+		transaction.commit();
+
+		session.close();
+	}
+
+	@Override
+	protected Class<?>[] getAnnotatedClasses() {
+		return new Class<?>[] { Account.class };
+	}
+}
