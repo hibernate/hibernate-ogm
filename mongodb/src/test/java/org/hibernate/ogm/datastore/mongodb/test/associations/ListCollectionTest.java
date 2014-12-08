57ca578f13dbe345ce0c3a50334ef70abd746cf1
Emmanuel Bernard <emmanuel@hibernate.org>
2014-12-08 15:09:14 +0100

    OGM-605 Add List test for MongoDB collection per assoc and global formats

---
+++ mongodb/src/test/java/org/hibernate/ogm/datastore/mongodb/test/associations/ListCollectionTest.java
@@ -0,0 +1,33 @@
+/*
+ * Hibernate OGM, Domain model persistence for NoSQL datastores
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+
+package org.hibernate.ogm.datastore.mongodb.test.associations;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.ogm.backendtck.associations.collection.types.ListTest;
+import org.hibernate.ogm.datastore.document.cfg.DocumentStoreProperties;
+import org.hibernate.ogm.datastore.document.options.AssociationStorageType;
+import org.hibernate.ogm.datastore.mongodb.MongoDBProperties;
+import org.hibernate.ogm.datastore.mongodb.options.AssociationDocumentType;
+
+/**
+ * @author Emmanuel Bernard &lt;emmanuel@hibernate.org&gt;
+ */
+public class ListCollectionTest extends ListTest {
+	@Override
+	protected void configure(Configuration cfg) {
+		super.configure( cfg );
+		cfg.getProperties().put(
+				DocumentStoreProperties.ASSOCIATIONS_STORE,
+				AssociationStorageType.ASSOCIATION_DOCUMENT
+		);
+		cfg.getProperties().put(
+				MongoDBProperties.ASSOCIATION_DOCUMENT_STORAGE,
+				AssociationDocumentType.COLLECTION_PER_ASSOCIATION
+		);
+	}
+}
---
+++ mongodb/src/test/java/org/hibernate/ogm/datastore/mongodb/test/associations/ListGlobalTest.java
@@ -0,0 +1,27 @@
+/*
+ * Hibernate OGM, Domain model persistence for NoSQL datastores
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+
+package org.hibernate.ogm.datastore.mongodb.test.associations;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.ogm.backendtck.associations.collection.types.ListTest;
+import org.hibernate.ogm.datastore.document.cfg.DocumentStoreProperties;
+import org.hibernate.ogm.datastore.document.options.AssociationStorageType;
+
+/**
+ * @author Emmanuel Bernard &lt;emmanuel@hibernate.org&gt;
+ */
+public class ListGlobalTest extends ListTest {
+	@Override
+	protected void configure(Configuration cfg) {
+		super.configure( cfg );
+		cfg.getProperties().put(
+				DocumentStoreProperties.ASSOCIATIONS_STORE,
+				AssociationStorageType.ASSOCIATION_DOCUMENT
+		);
+	}
+}
