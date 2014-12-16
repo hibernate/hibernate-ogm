24b72f89234bb7d96dfa079f6b39ffa576af16c4
Sanne Grinovero <sanne@hibernate.org>
2014-12-16 18:34:23 +0000

    OGM-537 Move the Infinispan OGM integration in a dedicated module

--- integrationtest/testcase/src/test/java/org/hibernate/ogm/test/integration/jboss/InfinispanModuleMemberRegistrationIT.java
+++ integrationtest/testcase/src/test/java/org/hibernate/ogm/test/integration/jboss/InfinispanModuleMemberRegistrationIT.java
@@ -28,7 +28,7 @@ public class InfinispanModuleMemberRegistrationIT extends ModuleMemberRegistrati
 		return new ModuleMemberRegistrationDeployment
 			.Builder( InfinispanModuleMemberRegistrationIT.class )
 			.persistenceXml( persistenceXml() )
-			.manifestDependencies( "org.hibernate:ogm services, org.hibernate.search.orm:5.0.0.Final-orm-4.3.7.Final services" )
+			.manifestDependencies( "org.hibernate:ogm services, org.hibernate.ogm.infinispan services, org.hibernate.search.orm:5.0.0.Final-orm-4.3.7.Final services" )
 			.createDeployment()
 			.addAsResource( "infinispan.xml", "infinispan.xml" );
 	}
