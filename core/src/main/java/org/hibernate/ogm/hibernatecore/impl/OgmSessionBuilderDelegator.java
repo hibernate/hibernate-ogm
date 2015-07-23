19bdeed9561fc6884cd1cd866c14e09266708457
Gunnar Morling <gunnar.morling@googlemail.com>
2015-07-23 11:25:51 +0200

    OGM-770 Re-establishing OGM exception mapper; Letting it delegate to ORM's default mapper now

--- core/src/main/java/org/hibernate/ogm/hibernatecore/impl/OgmSessionBuilderDelegator.java
+++ core/src/main/java/org/hibernate/ogm/hibernatecore/impl/OgmSessionBuilderDelegator.java
@@ -9,10 +9,15 @@ package org.hibernate.ogm.hibernatecore.impl;
 import org.hibernate.Session;
 import org.hibernate.engine.spi.ForwardingSessionBuilderImplementor;
 import org.hibernate.engine.spi.SessionBuilderImplementor;
+import org.hibernate.engine.spi.SessionOwner;
 import org.hibernate.event.spi.EventSource;
 import org.hibernate.ogm.OgmSession;
 import org.hibernate.ogm.OgmSessionFactory;
 import org.hibernate.ogm.engine.spi.OgmSessionBuilderImplementor;
+import org.hibernate.ogm.jpa.impl.OgmExceptionMapper;
+import org.hibernate.resource.transaction.backend.jta.internal.synchronization.AfterCompletionAction;
+import org.hibernate.resource.transaction.backend.jta.internal.synchronization.ExceptionMapper;
+import org.hibernate.resource.transaction.backend.jta.internal.synchronization.ManagedFlushChecker;
 
 /**
  * @author Emmanuel Bernard &lt;emmanuel@hibernate.org&gt;
@@ -33,5 +38,66 @@ public class OgmSessionBuilderDelegator extends ForwardingSessionBuilderImplemen
 	public OgmSession openSession() {
 		Session session = builder.openSession();
 		return new OgmSessionImpl( factory, (EventSource) session );
+	}
+
+	@Override
+	public OgmSessionBuilderImplementor owner(SessionOwner sessionOwner) {
+		if ( sessionOwner.getExceptionMapper() != null ) {
+			sessionOwner = new OgmExceptionMapperSessionOwner( sessionOwner );
+		}
+
+		builder.owner( sessionOwner );
+		return this;
+	}
+
+	/**
+	 * {@link SessionOwner} which injects OGM's {@link ExceptionMapper}.
+	 *
+	 * @author Gunnar Morling
+	 */
+	private static class OgmExceptionMapperSessionOwner extends ForwardingSessionOwner {
+
+		public OgmExceptionMapperSessionOwner(SessionOwner delegate) {
+			super( delegate );
+		}
+
+		@Override
+		public ExceptionMapper getExceptionMapper() {
+			return new OgmExceptionMapper( super.getExceptionMapper() );
+		}
+	}
+
+	/**
+	 * Delegating {@link SessionOwner}.
+	 *
+	 * @author Gunnar Morling
+	 */
+	private abstract static class ForwardingSessionOwner implements SessionOwner {
+
+		private final SessionOwner delegate;
+
+		public ForwardingSessionOwner(SessionOwner delegate) {
+			this.delegate = delegate;
+		}
+
+		@Override
+		public boolean shouldAutoCloseSession() {
+			return delegate.shouldAutoCloseSession();
+		}
+
+		@Override
+		public ExceptionMapper getExceptionMapper() {
+			return delegate.getExceptionMapper();
+		}
+
+		@Override
+		public AfterCompletionAction getAfterCompletionAction() {
+			return delegate.getAfterCompletionAction();
+		}
+
+		@Override
+		public ManagedFlushChecker getManagedFlushChecker() {
+			return delegate.getManagedFlushChecker();
+		}
 	}
 }
