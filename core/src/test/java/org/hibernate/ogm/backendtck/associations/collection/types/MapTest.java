f9a71f6d503ac791fa7e6971d2f4cd0e9edcc42b
Gunnar Morling <gunnar.morling@googlemail.com>
2015-07-28 10:01:52 +0200

    OGM-555 Splitting up test for maps and element collections

--- core/src/test/java/org/hibernate/ogm/backendtck/associations/collection/types/MapTest.java
+++ core/src/test/java/org/hibernate/ogm/backendtck/associations/collection/types/MapTest.java
@@ -29,9 +29,10 @@ import org.junit.Test;
 public class MapTest extends OgmTestCase {
 
 	@Test
-	public void testMapAndElementCollection() throws Exception {
+	public void testMapOfEntity() throws Exception {
 		Session session = openSession();
 		Transaction tx = session.beginTransaction();
+
 		Address home = new Address();
 		home.setCity( "Paris" );
 		Address work = new Address();
@@ -39,11 +40,49 @@ public class MapTest extends OgmTestCase {
 		User user = new User();
 		user.getAddresses().put( "home", home );
 		user.getAddresses().put( "work", work );
-		user.getNicknames().add( "idrA" );
-		user.getNicknames().add( "day[9]" );
+
 		session.persist( home );
 		session.persist( work );
 		session.persist( user );
+
+		tx.commit();
+		session.clear();
+
+		if ( getCurrentDialectType().isDocumentStore() ) {
+			assertThat( getNumberOfAssociations( sessions, AssociationStorageType.IN_ENTITY ) )
+					.describedAs( "Map contents should be stored within the entity document" )
+					.isEqualTo( 1 );
+			assertThat( getNumberOfAssociations( sessions, AssociationStorageType.ASSOCIATION_DOCUMENT ) )
+					.describedAs( "Map contents should be stored within the entity document" )
+					.isEqualTo( 0 );
+		}
+
+		tx = session.beginTransaction();
+		user = (User) session.get( User.class, user.getId() );
+		// TODO do null value
+		assertThat( user.getAddresses() ).as( "Map should have 2 elements" ).hasSize( 2 );
+		assertThat( user.getAddresses().get( "home" ).getCity() ).as( "home address should be under home" ).isEqualTo(
+				home.getCity() );
+		session.delete( user );
+		session.delete( session.load( Address.class, home.getId() ) );
+		session.delete( session.load( Address.class, work.getId() ) );
+
+		tx.commit();
+		session.close();
+
+		checkCleanCache();
+	}
+
+	@Test
+	public void testSetElementCollectionStorageAndRemoval() throws Exception {
+		Session session = openSession();
+		Transaction tx = session.beginTransaction();
+
+		User user = new User();
+		user.getNicknames().add( "idrA" );
+		user.getNicknames().add( "day[9]" );
+		session.persist( user );
+
 		User user2 = new User();
 		user2.getNicknames().add( "idrA" );
 		user2.getNicknames().add( "day[9]" );
@@ -54,37 +93,30 @@ public class MapTest extends OgmTestCase {
 
 		if ( getCurrentDialectType().isDocumentStore() ) {
 			assertThat( getNumberOfAssociations( sessions, AssociationStorageType.IN_ENTITY ) )
-					.describedAs( "Map contents and element collections should be stored within the entity document" )
-					.isEqualTo( 3 );
+					.describedAs( "Element collections should be stored within the entity document" )
+					.isEqualTo( 2 );
 			assertThat( getNumberOfAssociations( sessions, AssociationStorageType.ASSOCIATION_DOCUMENT ) )
-					.describedAs( "Map contents should be stored within the entity document" )
+					.describedAs( "Element collections should be stored within the entity document" )
 					.isEqualTo( 0 );
 		}
 
 		tx = session.beginTransaction();
-		user = (User) session.get( User.class, user.getId() );
-		assertThat( user.getNicknames() ).as( "Should have 2 nick1" ).hasSize( 2 );
-		assertThat( user.getNicknames() ).as( "Should contain nicks" ).contains( "idrA", "day[9]" );
-		user.getNicknames().remove( "idrA" );
-		tx.commit();
 
+		user = (User) session.get( User.class, user.getId() );
+		assertThat( user.getNicknames() ).containsOnly( "idrA", "day[9]" );
+		user.getNicknames().remove( "idrA" );
+
+		tx.commit();
 		session.clear();
 
 		tx = session.beginTransaction();
+
 		user = (User) session.get( User.class, user.getId() );
-		// TODO do null value
-		assertThat( user.getAddresses() ).as( "Map should have 2 elements" ).hasSize( 2 );
-		assertThat( user.getAddresses().get( "home" ).getCity() ).as( "home address should be under home" ).isEqualTo(
-				home.getCity() );
-		assertThat( user.getNicknames() ).as( "Should have 1 nick1" ).hasSize( 1 );
-		assertThat( user.getNicknames() ).as( "Should contain nick" ).contains( "day[9]" );
+		assertThat( user.getNicknames() ).containsOnly( "day[9]" );
 		session.delete( user );
-		session.delete( session.load( Address.class, home.getId() ) );
-		session.delete( session.load( Address.class, work.getId() ) );
 
 		user2 = (User) session.get( User.class, user2.getId() );
-		assertThat( user2.getNicknames() ).as( "Should have 2 nicks" ).hasSize( 2 );
-		assertThat( user2.getNicknames() ).as( "Should contain nick" ).contains( "idrA", "day[9]" );
+		assertThat( user2.getNicknames() ).containsOnly( "idrA", "day[9]" );
 		session.delete( user2 );
 
 		tx.commit();
