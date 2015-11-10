c160a9d328639a4ac2b6518384ad2fbf8f7ec224
Davide D'Alto <davide@hibernate.org>
2015-11-10 12:38:21 +0000

    OGM-366 Neo4j dialect and Neo4jTypeconverter

--- neo4j/src/main/java/org/hibernate/ogm/datastore/neo4j/Neo4jDialect.java
+++ neo4j/src/main/java/org/hibernate/ogm/datastore/neo4j/Neo4jDialect.java
@@ -6,9 +6,6 @@
  */
 package org.hibernate.ogm.datastore.neo4j;
 
-import static org.hibernate.ogm.datastore.neo4j.query.parsing.cypherdsl.impl.CypherDSL.limit;
-import static org.hibernate.ogm.datastore.neo4j.query.parsing.cypherdsl.impl.CypherDSL.skip;
-import static org.hibernate.ogm.util.impl.EmbeddedHelper.isPartOfEmbedded;
 import static org.hibernate.ogm.util.impl.EmbeddedHelper.split;
 import static org.neo4j.graphdb.DynamicRelationshipType.withName;
 
@@ -18,13 +15,11 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 
 import org.hibernate.AssertionFailure;
 import org.hibernate.engine.spi.QueryParameters;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
-import org.hibernate.engine.spi.TypedValue;
 import org.hibernate.ogm.datastore.impl.EmptyTupleSnapshot;
 import org.hibernate.ogm.datastore.neo4j.dialect.impl.MapsTupleIterator;
 import org.hibernate.ogm.datastore.neo4j.dialect.impl.Neo4jAssociationQueries;
@@ -39,18 +34,11 @@ import org.hibernate.ogm.datastore.neo4j.impl.Neo4jDatastoreProvider;
 import org.hibernate.ogm.datastore.neo4j.logging.impl.GraphLogger;
 import org.hibernate.ogm.datastore.neo4j.logging.impl.Log;
 import org.hibernate.ogm.datastore.neo4j.logging.impl.LoggerFactory;
-import org.hibernate.ogm.datastore.neo4j.query.impl.Neo4jParameterMetadataBuilder;
 import org.hibernate.ogm.dialect.query.spi.BackendQuery;
 import org.hibernate.ogm.dialect.query.spi.ClosableIterator;
-import org.hibernate.ogm.dialect.query.spi.ParameterMetadataBuilder;
-import org.hibernate.ogm.dialect.query.spi.QueryableGridDialect;
 import org.hibernate.ogm.dialect.spi.AssociationContext;
-import org.hibernate.ogm.dialect.spi.AssociationTypeContext;
-import org.hibernate.ogm.dialect.spi.BaseGridDialect;
-import org.hibernate.ogm.dialect.spi.DuplicateInsertPreventionStrategy;
 import org.hibernate.ogm.dialect.spi.ModelConsumer;
 import org.hibernate.ogm.dialect.spi.NextValueRequest;
-import org.hibernate.ogm.dialect.spi.SessionFactoryLifecycleAwareDialect;
 import org.hibernate.ogm.dialect.spi.TupleAlreadyExistsException;
 import org.hibernate.ogm.dialect.spi.TupleContext;
 import org.hibernate.ogm.model.key.spi.AssociatedEntityKeyMetadata;
@@ -65,14 +53,8 @@ import org.hibernate.ogm.model.spi.Tuple;
 import org.hibernate.ogm.model.spi.TupleOperation;
 import org.hibernate.ogm.persister.impl.OgmCollectionPersister;
 import org.hibernate.ogm.persister.impl.OgmEntityPersister;
-import org.hibernate.ogm.type.spi.GridType;
-import org.hibernate.ogm.type.spi.TypeTranslator;
-import org.hibernate.ogm.util.impl.ArrayHelper;
 import org.hibernate.persister.collection.CollectionPersister;
 import org.hibernate.persister.entity.EntityPersister;
-import org.hibernate.service.spi.ServiceRegistryAwareService;
-import org.hibernate.service.spi.ServiceRegistryImplementor;
-import org.hibernate.type.Type;
 import org.neo4j.graphdb.ConstraintViolationException;
 import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
@@ -95,31 +77,22 @@ import org.neo4j.kernel.api.exceptions.schema.UniqueConstraintViolationKernelExc
  *
  * @author Davide D'Alto &lt;davide@hibernate.org&gt;
  */
-public class Neo4jDialect extends BaseGridDialect implements QueryableGridDialect<String>, ServiceRegistryAwareService, SessionFactoryLifecycleAwareDialect {
-
-	public static final String CONSTRAINT_VIOLATION_CODE = "Neo.ClientError.Schema.ConstraintViolation";
+public class Neo4jDialect extends BaseNeo4jDialect<GraphDatabaseService> {
 
 	private static final Log log = LoggerFactory.getLogger();
 
-	private final Neo4jSequenceGenerator neo4jSequenceGenerator;
-
 	private final GraphDatabaseService dataBase;
 
-	private ServiceRegistryImplementor serviceRegistry;
+	private final Neo4jSequenceGenerator sequenceGenerator;
 
 	private Map<EntityKeyMetadata, Neo4jEntityQueries> entityQueries;
 
 	private Map<AssociationKeyMetadata, Neo4jAssociationQueries> associationQueries;
 
-
 	public Neo4jDialect(Neo4jDatastoreProvider provider) {
-		dataBase = provider.getDataBase();
-		this.neo4jSequenceGenerator = provider.getSequenceGenerator();
-	}
-
-	@Override
-	public void injectServices(ServiceRegistryImplementor serviceRegistry) {
-		this.serviceRegistry = serviceRegistry;
+		super( Neo4jTypeConverter.FOR_EMBEDDED );
+		this.dataBase = provider.getDataBase();
+		this.sequenceGenerator = provider.getSequenceGenerator();
 	}
 
 	@Override
@@ -182,11 +155,6 @@ public class Neo4jDialect extends BaseGridDialect implements QueryableGridDialec
 						key.getMetadata()
 				)
 		);
-	}
-
-	@Override
-	public Tuple createTuple(EntityKey key, TupleContext tupleContext) {
-		return new Tuple();
 	}
 
 	@Override
@@ -322,22 +290,6 @@ public class Neo4jDialect extends BaseGridDialect implements QueryableGridDialec
 		}
 	}
 
-	private RowKey convert(AssociationKey associationKey, Neo4jTupleAssociationSnapshot snapshot) {
-		String[] columnNames = associationKey.getMetadata().getRowKeyColumnNames();
-		Object[] values = new Object[columnNames.length];
-
-		for ( int i = 0; i < columnNames.length; i++ ) {
-			values[i] = snapshot.get( columnNames[i] );
-		}
-
-		return new RowKey( columnNames, values );
-	}
-
-	@Override
-	public Association createAssociation(AssociationKey associationKey, AssociationContext associationContext) {
-		return new Association();
-	}
-
 	@Override
 	public void insertOrUpdateAssociation(AssociationKey key, Association association, AssociationContext associationContext) {
 		// If this is the inverse side of a bi-directional association, we don't create a relationship for this; this
@@ -349,26 +301,6 @@ public class Neo4jDialect extends BaseGridDialect implements QueryableGridDialec
 		for ( AssociationOperation action : association.getOperations() ) {
 			applyAssociationOperation( association, key, action, associationContext );
 		}
-	}
-
-	@Override
-	public boolean isStoredInEntityStructure(AssociationKeyMetadata associationKeyMetadata, AssociationTypeContext associationTypeContext) {
-		return false;
-	}
-
-	@Override
-	public Number nextValue(NextValueRequest request) {
-		return neo4jSequenceGenerator.nextValue( request.getKey(), request.getIncrement(), request.getInitialValue() );
-	}
-
-	@Override
-	public boolean supportsSequences() {
-		return true;
-	}
-
-	@Override
-	public GridType overrideType(Type type) {
-		return Neo4jTypeConverter.INSTANCE.convert( type );
 	}
 
 	@Override
@@ -510,23 +442,12 @@ public class Neo4jDialect extends BaseGridDialect implements QueryableGridDialec
 		}
 	}
 
-	/**
-	 * A regular embedded is an element that it is embedded but it is not a key or a collection.
-	 *
-	 * @param keyColumnNames the column names representing the identifier of the entity
-	 * @param column the column we want to check
-	 * @return {@code true} if the column represent an attribute of a regular embedded element, {@code false} otherwise
-	 */
-	public static boolean isPartOfRegularEmbedded(String[] keyColumnNames, String column) {
-		return isPartOfEmbedded( column ) && !ArrayHelper.contains( keyColumnNames, column );
-	}
-
 	private void putProperty(EntityKey entityKey, Node node, TupleOperation operation) {
 		try {
 			node.setProperty( operation.getColumn(), operation.getValue() );
 		}
 		catch (ConstraintViolationException e) {
-			throw log.constraintViolation( entityKey, operation, e );
+			throw log.constraintViolation( entityKey, String.valueOf( operation ), e );
 		}
 	}
 
@@ -569,6 +490,11 @@ public class Neo4jDialect extends BaseGridDialect implements QueryableGridDialec
 	}
 
 	@Override
+	public Number nextValue(NextValueRequest request) {
+		return sequenceGenerator.nextValue( request.getKey(), request.getIncrement(), request.getInitialValue() );
+	}
+
+	@Override
 	public ClosableIterator<Tuple> executeBackendQuery(BackendQuery<String> backendQuery, QueryParameters queryParameters) {
 		Map<String, Object> parameters = getNamedParameterValuesConvertedByGridType( queryParameters );
 		String nativeQuery = buildNativeQuery( backendQuery, queryParameters );
@@ -578,83 +504,5 @@ public class Neo4jDialect extends BaseGridDialect implements QueryableGridDialec
 			return new NodesTupleIterator( result, backendQuery.getSingleEntityKeyMetadataOrNull() );
 		}
 		return new MapsTupleIterator( result );
-	}
-
-	@Override
-	public String parseNativeQuery(String nativeQuery) {
-		// We return given Cypher queries as they are; Currently there is no API for validating Cypher queries without
-		// actually executing them (see https://github.com/neo4j/neo4j/issues/2766)
-		return nativeQuery;
-	}
-
-	private String buildNativeQuery(BackendQuery<String> customQuery, QueryParameters queryParameters) {
-		StringBuilder nativeQuery = new StringBuilder( customQuery.getQuery() );
-		applyFirstRow( queryParameters, nativeQuery );
-		applyMaxRows( queryParameters, nativeQuery );
-		return nativeQuery.toString();
-	}
-
-	private void applyFirstRow(QueryParameters queryParameters, StringBuilder nativeQuery) {
-		Integer firstRow = queryParameters.getRowSelection().getFirstRow();
-		if ( firstRow != null ) {
-			skip( nativeQuery, firstRow );
-		}
-	}
-
-	private void applyMaxRows(QueryParameters queryParameters, StringBuilder nativeQuery) {
-		Integer maxRows = queryParameters.getRowSelection().getMaxRows();
-		if ( maxRows != null ) {
-			limit( nativeQuery, maxRows );
-		}
-	}
-
-	/**
-	 * Returns a map with the named parameter values from the given parameters object, converted by the {@link GridType}
-	 * corresponding to each parameter type.
-	 */
-	private Map<String, Object> getNamedParameterValuesConvertedByGridType(QueryParameters queryParameters) {
-		Map<String, Object> parameterValues = new HashMap<String, Object>( queryParameters.getNamedParameters().size() );
-		Tuple dummy = new Tuple();
-		TypeTranslator typeTranslator = serviceRegistry.getService( TypeTranslator.class );
-
-		for ( Entry<String, TypedValue> parameter : queryParameters.getNamedParameters().entrySet() ) {
-			GridType gridType = typeTranslator.getType( parameter.getValue().getType() );
-			gridType.nullSafeSet( dummy, parameter.getValue().getValue(), new String[]{ parameter.getKey() }, null );
-			parameterValues.put( parameter.getKey(), dummy.get( parameter.getKey() ) );
-		}
-
-		return parameterValues;
-	}
-
-	@Override
-	public ParameterMetadataBuilder getParameterMetadataBuilder() {
-		return new Neo4jParameterMetadataBuilder();
-	}
-
-	/**
-	 * Returns the key of the entity targeted by the represented association, retrieved from the given tuple.
-	 *
-	 * @param tuple the tuple from which to retrieve the referenced entity key
-	 * @return the key of the entity targeted by the represented association
-	 */
-	private EntityKey getEntityKey(Tuple tuple, AssociatedEntityKeyMetadata associatedEntityKeyMetadata) {
-		Object[] columnValues = new Object[ associatedEntityKeyMetadata.getAssociationKeyColumns().length];
-		int i = 0;
-
-		for ( String associationKeyColumn : associatedEntityKeyMetadata.getAssociationKeyColumns() ) {
-			columnValues[i] = tuple.get( associationKeyColumn );
-			i++;
-		}
-
-		return new EntityKey( associatedEntityKeyMetadata.getEntityKeyMetadata(), columnValues );
-	}
-
-	@Override
-	public DuplicateInsertPreventionStrategy getDuplicateInsertPreventionStrategy(EntityKeyMetadata entityKeyMetadata) {
-		// Only for non-composite keys (= one column) Neo4j supports unique key constraints; Hence an explicit look-up
-		// is required to detect duplicate insertions when using composite keys
-		return entityKeyMetadata.getColumnNames().length == 1 ?
-				DuplicateInsertPreventionStrategy.NATIVE :
-				DuplicateInsertPreventionStrategy.LOOK_UP;
 	}
 }
