45518a0628311d3430c5d7a851bc410036d6fe4a
Davide D'Alto <davide@hibernate.org>
2015-11-13 10:31:44 +0000

    OGM-336 Create Neo4jTransactionDriver interface
      Common interface for the Neo4j TransactionDriver embedded and remote.
      It used to get the id of the running transaction.

---
+++ neo4j/src/main/java/org/hibernate/ogm/datastore/neo4j/remote/transaction/impl/Neo4jTransactionDriver.java
@@ -0,0 +1,16 @@
+/*
+ * Hibernate OGM, Domain model persistence for NoSQL datastores
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.ogm.datastore.neo4j.remote.transaction.impl;
+
+
+/**
+ * @author Davide D'Alto
+ */
+public interface Neo4jTransactionDriver {
+
+	Long getTransactionId();
+}
