9878397175f5a92a7f35926961680f5cc5277281
Davide D'Alto <davide@hibernate.org>
2015-11-22 19:27:46 +0000

    OGM-931 Test that the proper exception is thrown

---
+++ core/src/test/java/org/hibernate/ogm/backendtck/associations/manytoone/Employee.java
@@ -0,0 +1,61 @@
+/*
+ * Hibernate OGM, Domain model persistence for NoSQL datastores
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.ogm.backendtck.associations.manytoone;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "Employee")
+public class Employee implements Serializable {
+
+	private static final long serialVersionUID = -8732345803771451030L;
+	private String id;
+	private String name;
+	private Employeer employeer;
+
+	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@ManyToOne
+	@Cascade(value = { CascadeType.MERGE })
+	@JoinColumn(insertable = true, updatable = true, name = "EmployerID")
+	public Employeer getEmployeer() {
+		return employeer;
+	}
+
+	public void setEmployeer(Employeer employeer) {
+		this.employeer = employeer;
+	}
+
+}
