a9f6c634a0383944bc8984774882336ba176c292
Mark Paluch <mpaluch@paluch.biz>
2016-01-12 16:41:57 +0100

    OGM-873 Test bidirectional one-to-one relations using composite primary keys

---
+++ core/src/test/java/org/hibernate/ogm/backendtck/associations/onetoone/Victim.java
@@ -0,0 +1,54 @@
+/*
+ * Hibernate OGM, Domain model persistence for NoSQL datastores
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.ogm.backendtck.associations.onetoone;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.OneToOne;
+
+/**
+ * @author Mark Paluch
+ */
+@Entity
+public class Victim {
+
+	@EmbeddedId
+	private PersonId id;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumns({
+			@JoinColumn(name = "firstName"),
+			@JoinColumn(name = "lastName")
+	})
+	private Offender offender;
+
+	public Victim() {
+	}
+
+	public Victim(PersonId id) {
+		this.id = id;
+	}
+
+	public PersonId getId() {
+		return id;
+	}
+
+	public void setId(PersonId id) {
+		this.id = id;
+	}
+
+	public Offender getOffender() {
+		return offender;
+	}
+
+	public void setOffender(Offender offender) {
+		this.offender = offender;
+	}
+}
