751aebf5436c9400b48fbc7b8a8b698d09ea9a3f
Davide D'Alto <davide@hibernate.org>
2016-01-21 15:10:23 +0000

    OGM-366 Move ByteStringType to core
      ByteStringType store a Byte as a String.
    
      It's currently used by MongoDB only and we want to make it available to
      the other dataastores.

--- core/src/main/java/org/hibernate/ogm/type/impl/ByteStringType.java
+++ core/src/main/java/org/hibernate/ogm/type/impl/ByteStringType.java
@@ -4,12 +4,11 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.ogm.datastore.mongodb.type.impl;
+package org.hibernate.ogm.type.impl;
 
 import org.hibernate.MappingException;
 import org.hibernate.engine.spi.Mapping;
 import org.hibernate.ogm.type.descriptor.impl.StringMappedGridTypeDescriptor;
-import org.hibernate.ogm.type.impl.AbstractGenericBasicType;
 import org.hibernate.type.descriptor.java.ByteTypeDescriptor;
 
 /**
