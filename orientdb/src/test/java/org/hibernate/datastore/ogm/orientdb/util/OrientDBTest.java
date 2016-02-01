733938d4f6a0cf9a55d68310fb810c7dc7f9aa9c
Davide D'Alto <davide@hibernate.org>
2016-02-01 17:38:52 +0000

    OrientDBTest

---
+++ orientdb/src/test/java/org/hibernate/datastore/ogm/orientdb/util/OrientDBTest.java
@@ -0,0 +1,17 @@
+/*
+ * Hibernate OGM, Domain model persistence for NoSQL datastores
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.datastore.ogm.orientdb.util;
+
+import org.hibernate.ogm.backendtck.type.BuiltInTypeTest;
+import org.junit.Ignore;
+
+/**
+ * @author Davide D'Alto
+ */
+public class OrientDBTest extends BuiltInTypeTest {
+
+}
