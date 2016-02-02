75ef325857ab61a611591507f0b965f20026169b
Davide D'Alto <davide@hibernate.org>
2016-02-02 16:10:46 +0000

    OGM-855 Rename package util to utils

--- orientdb/src/test/java/org/hibernate/datastore/ogm/orientdb/utils/OrientDBTest.java
+++ orientdb/src/test/java/org/hibernate/datastore/ogm/orientdb/utils/OrientDBTest.java
@@ -4,14 +4,13 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.datastore.ogm.orientdb.util;
+package org.hibernate.datastore.ogm.orientdb.utils;
 
-import org.hibernate.ogm.backendtck.type.BuiltInTypeTest;
-import org.junit.Ignore;
+import org.hibernate.ogm.backendtck.simpleentity.CRUDTest;
 
 /**
  * @author Davide D'Alto
  */
-public class OrientDBTest extends BuiltInTypeTest {
+public class OrientDBTest extends CRUDTest {
 
 }
