2ef3923b97506c0e034673a4a681b5b542628dfe
Z-z-z-z <vikad111@gmail.com>
2016-04-26 11:50:25 +0300

    checkstyle

--- ignite/src/main/java/org/hibernate/ogm/datastore/ignite/transaction/impl/DelegatingTransactionManager.java
+++ ignite/src/main/java/org/hibernate/ogm/datastore/ignite/transaction/impl/DelegatingTransactionManager.java
@@ -23,111 +23,109 @@ import javax.transaction.TransactionManager;
 import org.hibernate.HibernateException;
 
 public class DelegatingTransactionManager implements TransactionManager {
-    
-    private static final String WAS_TRANSACTION_MANAGER_CLASS_NAME = "com.ibm.tx.jta.impl.TranManagerSet";
-    private static final String WAS_TRANSACTION_MANAGER_METHOD_NAME = "instance";
-    private static final String JBOSS_TRANSACTION_MANAGER_JNDI_NAME = "java:jboss/TransactionManager";
 
-    private static volatile TransactionManager INSTANCE;
+	private static final String WAS_TRANSACTION_MANAGER_CLASS_NAME = "com.ibm.tx.jta.impl.TranManagerSet";
+	private static final String WAS_TRANSACTION_MANAGER_METHOD_NAME = "instance";
+	private static final String JBOSS_TRANSACTION_MANAGER_JNDI_NAME = "java:jboss/TransactionManager";
 
-    private final TransactionManager delegate;
-    private final ApplicationServer selectedApplicationServer;
+	private static volatile TransactionManager INSTANCE;
 
-    public DelegatingTransactionManager(final TransactionManager delegate, final ApplicationServer applicationServer) {
-        this.delegate = delegate;
-        selectedApplicationServer = applicationServer;
-    }
+	private final TransactionManager delegate;
+	private final ApplicationServer selectedApplicationServer;
 
-    public static TransactionManager transactionManager() {
-        if (INSTANCE == null) {
-            synchronized (DelegatingTransactionManager.class) {
-                if (INSTANCE == null) {
-                    try {
-                        TransactionManager delegate = null;
-                        final ApplicationServer applicationServer = ApplicationServer.currentApplicationServer();
-                        switch (applicationServer) {
-	                        case WEBSPHERE:
-	                            delegate = initWebSphereTransactionManager();
-	                            break;
-	                        case JBOSS:
-	                            delegate = initJBossTransactionManager();
-	                            break;
-	                        default:
-	                        	throw new UnsupportedOperationException("Unsupported application server");
-                        }
-                        INSTANCE = new DelegatingTransactionManager(delegate, applicationServer);
-                    } catch (ClassNotFoundException | NoSuchMethodException | SecurityException | IllegalAccessException
-                            | IllegalArgumentException | InvocationTargetException | NamingException e) {
-                        throw new HibernateException("Cannot instantiate TransactionManager", e);
-                    }
-                }
-            }
-        }
-        return INSTANCE;
-    }
+	public DelegatingTransactionManager( final TransactionManager delegate, final ApplicationServer applicationServer ) {
+		this.delegate = delegate;
+		selectedApplicationServer = applicationServer;
+	}
 
-    @Override
-    public void begin() throws NotSupportedException, SystemException {
-        delegate.begin();
-    }
+	public static TransactionManager transactionManager() {
+		if ( INSTANCE == null ) {
+			synchronized ( DelegatingTransactionManager.class ) {
+				if ( INSTANCE == null ) {
+					try {
+						TransactionManager delegate = null;
+						final ApplicationServer applicationServer = ApplicationServer.currentApplicationServer();
+						switch ( applicationServer ) {
+							case WEBSPHERE:
+								delegate = initWebSphereTransactionManager();
+								break;
+							case JBOSS:
+								delegate = initJBossTransactionManager();
+								break;
+							default:
+								throw new UnsupportedOperationException( "Unsupported application server" );
+						}
+						INSTANCE = new DelegatingTransactionManager( delegate, applicationServer );
+					}
+					catch ( ClassNotFoundException | NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NamingException e ) {
+						throw new HibernateException( "Cannot instantiate TransactionManager", e );
+					}
+				}
+			}
+		}
+		return INSTANCE;
+	}
 
-    @Override
-    public void commit() throws RollbackException, HeuristicMixedException, HeuristicRollbackException, SecurityException,
-            IllegalStateException, SystemException {
-        delegate.commit();
-    }
+	@Override
+	public void begin() throws NotSupportedException, SystemException {
+		delegate.begin();
+	}
 
-    @Override
-    public int getStatus() throws SystemException {
-        return delegate.getStatus();
-    }
+	@Override
+	public void commit() throws RollbackException, HeuristicMixedException, HeuristicRollbackException, SecurityException, IllegalStateException, SystemException {
+		delegate.commit();
+	}
 
-    @Override
-    public Transaction getTransaction() throws SystemException {
-        final Transaction transaction = delegate.getTransaction();
-        if (selectedApplicationServer == ApplicationServer.WEBSPHERE) {
-            return transaction != null ? new WebSphereTransaction(transaction) : null;
-        }
-        return transaction;
-    }
+	@Override
+	public int getStatus() throws SystemException {
+		return delegate.getStatus();
+	}
 
-    @Override
-    public void resume(final Transaction tobj) throws InvalidTransactionException, IllegalStateException, SystemException {
-        delegate.resume(tobj);
-    }
+	@Override
+	public Transaction getTransaction() throws SystemException {
+		final Transaction transaction = delegate.getTransaction();
+		if ( selectedApplicationServer == ApplicationServer.WEBSPHERE ) {
+			return transaction != null ? new WebSphereTransaction( transaction ) : null;
+		}
+		return transaction;
+	}
 
-    @Override
-    public void rollback() throws IllegalStateException, SecurityException, SystemException {
-        delegate.rollback();
-    }
+	@Override
+	public void resume( final Transaction tobj ) throws InvalidTransactionException, IllegalStateException, SystemException {
+		delegate.resume( tobj );
+	}
 
-    @Override
-    public void setRollbackOnly() throws IllegalStateException, SystemException {
-        delegate.setRollbackOnly();
-    }
+	@Override
+	public void rollback() throws IllegalStateException, SecurityException, SystemException {
+		delegate.rollback();
+	}
 
-    @Override
-    public void setTransactionTimeout(final int seconds) throws SystemException {
-        delegate.setTransactionTimeout(seconds);
-    }
+	@Override
+	public void setRollbackOnly() throws IllegalStateException, SystemException {
+		delegate.setRollbackOnly();
+	}
 
-    @Override
-    public Transaction suspend() throws SystemException {
-        return delegate.suspend();
-    }
+	@Override
+	public void setTransactionTimeout( final int seconds ) throws SystemException {
+		delegate.setTransactionTimeout( seconds );
+	}
 
-    private static TransactionManager initWebSphereTransactionManager() throws ClassNotFoundException, NoSuchMethodException,
-            SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        final Class<?> clazz = Class.forName(WAS_TRANSACTION_MANAGER_CLASS_NAME);
-        final Method m = clazz.getMethod(WAS_TRANSACTION_MANAGER_METHOD_NAME, (Class[]) null);
-        final TransactionManager transactionManager = (TransactionManager) m.invoke(null, (Object[]) null);
+	@Override
+	public Transaction suspend() throws SystemException {
+		return delegate.suspend();
+	}
 
-        return transactionManager;
-    }
+	private static TransactionManager initWebSphereTransactionManager() throws ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		final Class<?> clazz = Class.forName( WAS_TRANSACTION_MANAGER_CLASS_NAME );
+		final Method m = clazz.getMethod( WAS_TRANSACTION_MANAGER_METHOD_NAME, (Class[]) null );
+		final TransactionManager transactionManager = (TransactionManager) m.invoke( null, (Object[]) null );
 
-    private static TransactionManager initJBossTransactionManager() throws NamingException {
-        final InitialContext initialContext = new InitialContext();
-        final TransactionManager transactionManager = (TransactionManager) initialContext.lookup(JBOSS_TRANSACTION_MANAGER_JNDI_NAME);
-        return transactionManager;
-    }
+		return transactionManager;
+	}
+
+	private static TransactionManager initJBossTransactionManager() throws NamingException {
+		final InitialContext initialContext = new InitialContext();
+		final TransactionManager transactionManager = (TransactionManager) initialContext.lookup( JBOSS_TRANSACTION_MANAGER_JNDI_NAME );
+		return transactionManager;
+	}
 }
--- ignite/src/main/java/org/hibernate/ogm/datastore/ignite/transaction/impl/WebSphereTransaction.java
+++ ignite/src/main/java/org/hibernate/ogm/datastore/ignite/transaction/impl/WebSphereTransaction.java
@@ -17,63 +17,66 @@ import javax.transaction.Transaction;
 import javax.transaction.xa.XAResource;
 
 public class WebSphereTransaction implements Transaction {
-    private final Transaction delegate;
-    private static volatile Class<?> onePhaseXAResourceClass;
-    private static final String ONE_PHASE_XA_RESOURCE_CLASS_NAME = "com.ibm.tx.jta.OnePhaseXAResource";
+	private static final String ONE_PHASE_XA_RESOURCE_CLASS_NAME = "com.ibm.tx.jta.OnePhaseXAResource";
+	private static volatile Class<?> onePhaseXAResourceClass;
+	private final Transaction delegate;
 
-    public WebSphereTransaction(final Transaction delegate) {
-        this.delegate = delegate;
-        if (onePhaseXAResourceClass == null) {
-            synchronized (WebSphereTransaction.class) {
-                if (onePhaseXAResourceClass == null) {
-                    try {
-                        onePhaseXAResourceClass = Class.forName(ONE_PHASE_XA_RESOURCE_CLASS_NAME);
-                    } catch (final ClassNotFoundException e) {
-                        throw new RuntimeException("Cannot instantiate Transaction");
-                    }
-                }
-            }
-        }
-    }
+	public WebSphereTransaction( final Transaction delegate ) {
+		this.delegate = delegate;
+		if ( onePhaseXAResourceClass == null ) {
+			synchronized ( WebSphereTransaction.class ) {
+				if ( onePhaseXAResourceClass == null ) {
+					try {
+						onePhaseXAResourceClass = Class.forName( ONE_PHASE_XA_RESOURCE_CLASS_NAME );
+					}
+					catch ( final ClassNotFoundException e ) {
+						throw new RuntimeException( "Cannot instantiate Transaction" );
+					}
+				}
+			}
+		}
+	}
 
-    @Override
-    public void commit() throws RollbackException, HeuristicMixedException, HeuristicRollbackException, SecurityException,
-            IllegalStateException, SystemException {
-        delegate.commit();
-    }
+	@Override
+	public void commit() throws RollbackException, HeuristicMixedException, HeuristicRollbackException, SecurityException, IllegalStateException, SystemException {
+		delegate.commit();
+	}
 
-    @Override
-    public boolean delistResource(final XAResource xaRes, final int flag) throws IllegalStateException, SystemException {
-        return delegate.delistResource(xaRes, flag);
-    }
+	@Override
+	public boolean delistResource( final XAResource xaRes, final int flag ) throws IllegalStateException, SystemException {
+		return delegate.delistResource( xaRes, flag );
+	}
 
-    @Override
-    public boolean enlistResource(final XAResource xaResource) throws RollbackException, IllegalStateException, SystemException {
-        if (xaResource == null) {
-            return false;
-        }
-        return delegate.enlistResource((XAResource) newProxyInstance(currentThread().getContextClassLoader(),
-                new Class<?>[] { onePhaseXAResourceClass }, new DelegatingInvocationHandler(xaResource)));
-    }
+	@Override
+	public boolean enlistResource( final XAResource xaResource ) throws RollbackException, IllegalStateException, SystemException {
+		if ( xaResource == null ) {
+			return false;
+		}
+		return delegate.enlistResource(
+					(XAResource) newProxyInstance(
+						currentThread().getContextClassLoader(),
+						new Class<?>[] { onePhaseXAResourceClass }, new DelegatingInvocationHandler( xaResource )
+					)
+				);
+	}
 
-    @Override
-    public int getStatus() throws SystemException {
-        return delegate.getStatus();
-    }
+	@Override
+	public int getStatus() throws SystemException {
+		return delegate.getStatus();
+	}
 
-    @Override
-    public void registerSynchronization(final javax.transaction.Synchronization sync) throws RollbackException, IllegalStateException,
-            SystemException {
-        delegate.registerSynchronization(sync);
-    }
+	@Override
+	public void registerSynchronization( final javax.transaction.Synchronization sync ) throws RollbackException, IllegalStateException, SystemException {
+		delegate.registerSynchronization( sync );
+	}
 
-    @Override
-    public void rollback() throws IllegalStateException, SystemException {
-        delegate.rollback();
-    }
+	@Override
+	public void rollback() throws IllegalStateException, SystemException {
+		delegate.rollback();
+	}
 
-    @Override
-    public void setRollbackOnly() throws IllegalStateException, SystemException {
-        delegate.setRollbackOnly();
-    }
+	@Override
+	public void setRollbackOnly() throws IllegalStateException, SystemException {
+		delegate.setRollbackOnly();
+	}
 }
