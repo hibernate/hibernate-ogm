b18fa3fda3d916423a548485fc0443f3a1ae5a46
Sanne Grinovero <sanne@hibernate.org>
2016-05-04 22:33:42 +0100

    OGM-991 Update the Getting Started example: simpler access to Narayana TM

--- documentation/examples/gettingstarted/src/main/java/org/hibernate/ogm/examples/gettingstarted/DogBreedRunner.java
+++ documentation/examples/gettingstarted/src/main/java/org/hibernate/ogm/examples/gettingstarted/DogBreedRunner.java
@@ -15,16 +15,14 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.transaction.TransactionManager;
-import java.lang.reflect.InvocationTargetException;
 
 public class DogBreedRunner {
 
-	private static final String JBOSS_TM_CLASS_NAME = "com.arjuna.ats.jta.TransactionManager";
 	private static final Log logger = LoggerFactory.make();
 
 	public static void main(String[] args) {
 
-		TransactionManager tm = getTransactionManager();
+		TransactionManager tm = com.arjuna.ats.jta.TransactionManager.transactionManager();
 
 		//build the EntityManagerFactory as you would build in in Hibernate Core
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory( "ogm-jpa-tutorial" );
@@ -64,23 +62,4 @@ public class DogBreedRunner {
 
 	}
 
-	public static TransactionManager getTransactionManager() {
-		try {
-			Class<?> tmClass = DogBreedRunner.class.getClassLoader().loadClass( JBOSS_TM_CLASS_NAME );
-			return (TransactionManager) tmClass.getMethod( "transactionManager" ).invoke( null );
-		}
-		catch ( ClassNotFoundException e ) {
-			e.printStackTrace();
-		}
-		catch ( InvocationTargetException e ) {
-			e.printStackTrace();
-		}
-		catch ( NoSuchMethodException e ) {
-			e.printStackTrace();
-		}
-		catch ( IllegalAccessException e ) {
-			e.printStackTrace();
-		}
-		return null;
-	}
 }
