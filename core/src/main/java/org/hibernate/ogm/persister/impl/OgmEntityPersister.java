5e1f58443a113f048fdd74c2c515c7e26a273b62
Guillaume Smet <guillaume.smet@gmail.com>
2016-06-08 18:41:48 +0200

    OGM-1064 Properly update the tuple in OgmEntityEntryState after updates
    Currently, it's only updated after the insert operation while there are
    a lot of operations modifying the tuple.

--- core/src/main/java/org/hibernate/ogm/persister/impl/OgmEntityPersister.java
+++ core/src/main/java/org/hibernate/ogm/persister/impl/OgmEntityPersister.java
@@ -712,6 +712,9 @@ public abstract class OgmEntityPersister extends AbstractEntityPersister impleme
 		checkVersionAndRaiseSOSE( id, currentVersion, session, resultset );
 		gridVersionType.nullSafeSet( resultset, nextVersion, new String[] { getVersionColumnName() }, session );
 		gridDialect.insertOrUpdateTuple( key, resultset, getTupleContext( session ) );
+
+		OgmEntityEntryState.getStateFor( session, nextVersion ).setTuple( resultset );
+
 		return nextVersion;
 	}
 
@@ -1227,6 +1230,8 @@ public abstract class OgmEntityPersister extends AbstractEntityPersister impleme
 				if ( mightRequireInverseAssociationManagement ) {
 					addToInverseAssociations( resultset, j, id, session );
 				}
+
+				OgmEntityEntryState.getStateFor( session, object ).setTuple( resultset );
 			}
 		}
 	}
@@ -1809,6 +1814,8 @@ public abstract class OgmEntityPersister extends AbstractEntityPersister impleme
 				setPropertyValue( entity, propertyIndex, state[propertyIndex] );
 			}
 		}
+
+		OgmEntityEntryState.getStateFor( session, entity ).setTuple( tuple );
 	}
 
 	/**
