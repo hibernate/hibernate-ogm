496171be8df49ffc57ae2d676ad926e9f8632381
Guillaume Smet <guillaume.smet@gmail.com>
2016-07-13 22:04:43 +0200

    OGM-1119 Fix a few minor details in OgmTestCase and associates
    Mostly outdated javadoc.

--- core/src/test/java/org/hibernate/ogm/utils/TestSessionFactoryConfiguration.java
+++ core/src/test/java/org/hibernate/ogm/utils/TestSessionFactoryConfiguration.java
@@ -13,13 +13,13 @@ import java.lang.annotation.Target;
 
 /**
  * Marks a method of a test executed with {@link OgmTestRunner} which should inspect or modify the test's
- * {@link Configuration}.
+ * configuration.
  * <p>
- * The method must have a single parameter of type {@code Configuration}.
+ * The method must have a single parameter of type {@code Map<String, Object>}.
  *
  * @author Gunnar Morling
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface SessionFactoryConfiguration {
+public @interface TestSessionFactoryConfiguration {
 }
