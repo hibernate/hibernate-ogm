54cd83f2d054b04af88e3effacacbd289d575ce8
Guillaume Smet <guillaume.smet@gmail.com>
2016-08-10 17:46:29 +0200

    OGM-1116 Be consistent with Search/Validator in the way modules are built

--- src/main/release-scripts/upload-distribution.sh
+++ src/main/release-scripts/upload-distribution.sh
@@ -21,6 +21,6 @@ scp readme.md frs.sourceforge.net:$DIST_PARENT_DIR/$RELEASE_VERSION
 scp changelog.txt frs.sourceforge.net:$DIST_PARENT_DIR/$RELEASE_VERSION
 scp distribution/target/hibernate-ogm-$RELEASE_VERSION-dist.zip frs.sourceforge.net:$DIST_PARENT_DIR/$RELEASE_VERSION
 scp distribution/target/hibernate-ogm-$RELEASE_VERSION-dist.tar.gz frs.sourceforge.net:$DIST_PARENT_DIR/$RELEASE_VERSION
-scp modules/wildfly/target/hibernate-ogm-modules-wildfly10-$RELEASE_VERSION.zip frs.sourceforge.net:$DIST_PARENT_DIR/$RELEASE_VERSION
+scp modules/target/hibernate-ogm-modules-$RELEASE_VERSION-wildfly-10-dist.zip frs.sourceforge.net:$DIST_PARENT_DIR/$RELEASE_VERSION
 
 echo "Distribution uploaded to SourceForge"
