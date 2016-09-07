f5aa1a3a6055d3e4a7f0bf5a50a0bf9b00f1de14
Guillaume Smet <guillaume.smet@gmail.com>
2016-09-07 17:49:51 +0200

    OGM-1064 processInsertGeneratedProperties should test insert properties

--- core/src/main/java/org/hibernate/ogm/persister/impl/OgmEntityPersister.java
+++ core/src/main/java/org/hibernate/ogm/persister/impl/OgmEntityPersister.java
@@ -1766,7 +1766,7 @@ public abstract class OgmEntityPersister extends AbstractEntityPersister impleme
 
 	@Override
 	public void processInsertGeneratedProperties(Serializable id, Object entity, Object[] state, SessionImplementor session) {
-		if ( !hasUpdateGeneratedProperties() ) {
+		if ( !hasInsertGeneratedProperties() ) {
 			throw new AssertionFailure("no insert-generated properties");
 		}
 		processGeneratedProperties( id, entity, state, session, GenerationTiming.INSERT );
