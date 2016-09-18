c3d9b7f4070c10caed1d6052bfb8e741ff4f9cd8
Davide D'Alto <davide@hibernate.org>
2016-09-19 00:17:01 +0100

    OGM-1089 Rename package of JSON classes used with HTTP Neo4j

--- neo4j/src/main/java/org/hibernate/ogm/datastore/neo4j/remote/http/json/impl/StatementsResponse.java
+++ neo4j/src/main/java/org/hibernate/ogm/datastore/neo4j/remote/http/json/impl/StatementsResponse.java
@@ -4,7 +4,7 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.ogm.datastore.neo4j.remote.json.impl;
+package org.hibernate.ogm.datastore.neo4j.remote.http.json.impl;
 
 import java.util.List;
 
