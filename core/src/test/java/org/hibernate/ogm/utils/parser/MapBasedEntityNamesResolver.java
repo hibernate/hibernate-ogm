a4900244a04f8ff9f7bb509e7bf4a4874f08456a
Davide D'Alto <davide@hibernate.org>
2016-11-24 14:21:12 +0000

    OGM-732 Add MapBasedEntityNameResolver in core for testing

---
+++ core/src/test/java/org/hibernate/ogm/utils/parser/MapBasedEntityNamesResolver.java
@@ -0,0 +1,28 @@
+/*
+ * Hibernate OGM, Domain model persistence for NoSQL datastores
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.ogm.utils.parser;
+
+import java.util.Map;
+
+import org.hibernate.hql.ast.spi.EntityNamesResolver;
+
+/**
+ * @author Sanne Grinovero &lt;sanne@hibernate.org&gt; (C) 2012 Red Hat Inc.
+ */
+public class MapBasedEntityNamesResolver implements EntityNamesResolver {
+
+	private final Map<String, Class<?>> entityNames;
+
+	public MapBasedEntityNamesResolver(Map<String, Class<?>> entityNames) {
+		this.entityNames = entityNames;
+	}
+
+	@Override
+	public Class<?> getClassFromName(String entityName) {
+		return entityNames.get( entityName );
+	}
+}
