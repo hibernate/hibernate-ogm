7bc887ff446e3da2794854a7d3a975bdea148819
Z-z-z-z <vikad111@gmail.com>
2017-01-10 22:33:23 +0300

    merge error

--- core/src/test/java/org/hibernate/ogm/backendtck/queries/QueriesWithEmbeddedTest.java
+++ core/src/test/java/org/hibernate/ogm/backendtck/queries/QueriesWithEmbeddedTest.java
@@ -34,7 +34,7 @@ import org.junit.rules.ExpectedException;
  * @author Davide D'Alto
  */
 @SkipByGridDialect(
-		value = { GridDialectType.CASSANDRA, GridDialectType.IGNITE },
+		value = { GridDialectType.CASSANDRA, GridDialectType.INFINISPAN_REMOTE, GridDialectType.IGNITE },
 		comment = "Collection of embeddeds - bag semantics unsupported (no primary key)"
 )
 public class QueriesWithEmbeddedTest extends OgmTestCase {
