232053c9b2cb562d78e3d4ec9ccfe1f1c4a17e28
Guillaume Smet <guillaume.smet@gmail.com>
2017-02-14 16:10:11 +0100

    OGM-1067 Avoid systematic resizing of collections when we provide a size

--- core/src/main/java/org/hibernate/ogm/util/impl/CollectionHelper.java
+++ core/src/main/java/org/hibernate/ogm/util/impl/CollectionHelper.java
@@ -22,6 +22,7 @@ import org.hibernate.ogm.dialect.query.spi.ClosableIterator;
  * Provides commonly used functionality around collections.
  *
  * @author Gunnar Morling
+ * @author Guillaume Smet
  */
 public class CollectionHelper {
 
@@ -36,6 +37,7 @@ public class CollectionHelper {
 	 * @return an unmodifiable set containing the given elements or {@code null} in case the given element array is
 	 * {@code null}.
 	 */
+	@SafeVarargs
 	public static <T> Set<T> asSet(T... ts) {
 		if ( ts == null ) {
 			return null;
@@ -44,7 +46,7 @@ public class CollectionHelper {
 			return Collections.emptySet();
 		}
 		else {
-			Set<T> set = new HashSet<T>( ts.length );
+			Set<T> set = new HashSet<T>( getInitialCapacityFromExpectedSize( ts.length ) );
 			Collections.addAll( set, ts );
 			return Collections.unmodifiableSet( set );
 		}
@@ -55,7 +57,7 @@ public class CollectionHelper {
 	}
 
 	public static <T> LinkedHashSet<T> newLinkedHashSet(int initialCapacity) {
-		return new LinkedHashSet<T>( initialCapacity );
+		return new LinkedHashSet<T>( getInitialCapacityFromExpectedSize( initialCapacity ) );
 	}
 
 	public static <K, V> HashMap<K, V> newHashMap() {
@@ -63,22 +65,22 @@ public class CollectionHelper {
 	}
 
 	public static <K, V> HashMap<K, V> newHashMap(int initialCapacity) {
-		return new HashMap<K, V>( initialCapacity );
+		return new HashMap<K, V>( getInitialCapacityFromExpectedSize( initialCapacity ) );
 	}
 
 	public static <K, V> HashMap<K, V> newHashMap(Map<? extends K, ? extends V> other) {
 		return new HashMap<K, V>( other );
 	}
 
-	public static <K, V> HashMap<K, V> newLinkedHashMap() {
+	public static <K, V> LinkedHashMap<K, V> newLinkedHashMap() {
 		return new LinkedHashMap<K, V>();
 	}
 
-	public static <K, V> HashMap<K, V> newLinkedHashMap(int initialCapacity) {
-		return new LinkedHashMap<K, V>( initialCapacity );
+	public static <K, V> LinkedHashMap<K, V> newLinkedHashMap(int initialCapacity) {
+		return new LinkedHashMap<K, V>( getInitialCapacityFromExpectedSize( initialCapacity ) );
 	}
 
-	public static <K, V> HashMap<K, V> newLinkedHashMap(Map<? extends K, ? extends V> other) {
+	public static <K, V> LinkedHashMap<K, V> newLinkedHashMap(Map<? extends K, ? extends V> other) {
 		return new LinkedHashMap<K, V>( other );
 	}
 
@@ -105,6 +107,21 @@ public class CollectionHelper {
 			}
 		}
 		return true;
+	}
+
+	/**
+	 * As the default loadFactor is of 0.75, we need to calculate the initial capacity from the expected size to avoid
+	 * resizing the collection when we populate the collection with all the initial elements. We use a calculation
+	 * similar to what is done in {@link HashMap#putAll(Map)}.
+	 *
+	 * @param expectedSize the expected size of the collection
+	 * @return the initial capacity of the collection
+	 */
+	private static int getInitialCapacityFromExpectedSize(int expectedSize) {
+		if ( expectedSize < 3 ) {
+			return expectedSize + 1;
+		}
+		return (int) ( (float) expectedSize / 0.75f + 1.0f );
 	}
 
 	private static class ClosableIteratorWrapper<T> implements ClosableIterator<T> {
