b917002f9d226c9f153f2eb5f14ab420ffe3bfc6
Guillaume Smet <guillaume.smet@gmail.com>
2017-09-18 16:23:31 +0200

    OGM-1229 Initialize the OperationCollector on pulse()
    ORM used to initialize a TransactionImpl when initializing the
    SessionImpl but it now only calls pulse() on the TransactionCoordinator.
    Thus, the OperationCollector initialization must be moved there.

--- core/src/main/java/org/hibernate/ogm/compensation/impl/ErrorHandlerEnabledTransactionCoordinatorDecorator.java
+++ core/src/main/java/org/hibernate/ogm/compensation/impl/ErrorHandlerEnabledTransactionCoordinatorDecorator.java
@@ -15,6 +15,7 @@ import org.hibernate.resource.transaction.spi.TransactionCoordinator;
  * A {@link TransactionCoordinator} invoking the registered {@link ErrorHandler} upon rollbacks.
  *
  * @author Gunnar Morling
+ * @author Guillaume Smet
  */
 public class ErrorHandlerEnabledTransactionCoordinatorDecorator extends ForwardingTransactionCoordinator {
 
@@ -35,14 +36,14 @@ public class ErrorHandlerEnabledTransactionCoordinatorDecorator extends Forwardi
 	}
 
 	@Override
-	public TransactionDriver getTransactionDriverControl() {
+	public void pulse() {
 		// Create the collector upon first usage in a given TX (not done via TransactionObserver#afterBegin() as it is
 		// not invoked in the case of JTA
 		if ( operationCollector == null ) {
 			operationCollector = new OperationCollector( errorHandler );
 		}
 
-		return super.getTransactionDriverControl();
+		super.pulse();
 	}
 
 	/**
