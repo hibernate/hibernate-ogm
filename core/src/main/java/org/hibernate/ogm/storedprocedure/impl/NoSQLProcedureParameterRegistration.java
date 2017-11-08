333d4d75a400bc966be579e45e726a1b4de784c4
Sergey Chernolyas <sergey.chernolyas@gmail.com>
2017-11-08 11:58:29 +0300

    OGM-359 support stored procedures

---
+++ core/src/main/java/org/hibernate/ogm/storedprocedure/impl/NoSQLProcedureParameterRegistration.java
@@ -0,0 +1,154 @@
+/*
+ * Hibernate OGM, Domain model persistence for NoSQL datastores
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.ogm.storedprocedure.impl;
+
+import java.lang.invoke.MethodHandles;
+import javax.persistence.ParameterMode;
+import javax.persistence.TemporalType;
+
+import org.hibernate.ogm.util.impl.Log;
+import org.hibernate.ogm.util.impl.LoggerFactory;
+import org.hibernate.procedure.ParameterBind;
+import org.hibernate.procedure.ParameterMisuseException;
+import org.hibernate.procedure.ParameterRegistration;
+import org.hibernate.type.Type;
+
+/**
+ * @author Sergey Chernolyas &amp;sergey_chernolyas@gmail.com&amp;
+ */
+public class NoSQLProcedureParameterRegistration<T> implements ParameterRegistration<T> {
+	private static final Log log = LoggerFactory.make( MethodHandles.lookup() );
+
+	private final NoSQLProcedureCallImpl procedureCall;
+
+	private final Integer position;
+	private final String name;
+
+	private final ParameterMode mode;
+	private final Class<T> type;
+
+	private ParameterBindImpl bind;
+	private boolean passNulls;
+
+	private int startIndex;
+	private Type hibernateType;
+
+	public NoSQLProcedureParameterRegistration(NoSQLProcedureCallImpl procedureCall,Integer position,
+			String name, ParameterMode mode, Class<T> type) {
+		this.procedureCall = procedureCall;
+		this.position = position;
+		this.name = name;
+		this.mode = mode;
+		this.type = type;
+		this.hibernateType = procedureCall.getSession().getFactory().getTypeResolver().heuristicType( type.getName() );
+	}
+
+	@Override
+	public Integer getPosition() {
+		return position;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public ParameterMode getMode() {
+		return mode;
+	}
+
+	@Override
+	public Class<T> getType() {
+		return type;
+	}
+
+	@Override
+	public ParameterBindImpl getBind() {
+		return bind;
+	}
+
+	public void setBind(ParameterBindImpl bind) {
+		this.bind = bind;
+	}
+
+	public boolean isPassNulls() {
+		return passNulls;
+	}
+
+	public void setPassNulls(boolean passNulls) {
+		this.passNulls = passNulls;
+	}
+
+	public int getStartIndex() {
+		return startIndex;
+	}
+
+	public void setStartIndex(int startIndex) {
+		this.startIndex = startIndex;
+	}
+
+	public Type getHibernateType() {
+		return hibernateType;
+	}
+
+	@Override
+	public void setHibernateType(Type hibernateType) {
+		this.hibernateType = hibernateType;
+	}
+
+	@Override
+	public void enablePassingNulls(boolean enabled) {
+		this.passNulls = enabled;
+	}
+
+	@Override
+	public void bindValue(T value) {
+		validateBindability();
+		this.bind = new ParameterBindImpl<T>( value );
+	}
+
+	@Override
+	public void bindValue(T value, TemporalType explicitTemporalType) {
+		this.bind = new ParameterBindImpl<T>( value, explicitTemporalType );
+	}
+
+	private void validateBindability() {
+		if ( ! canBind() ) {
+			throw new ParameterMisuseException( "Cannot bind value to non-input parameter : " + this );
+		}
+	}
+
+	private boolean canBind() {
+		return mode == ParameterMode.IN || mode == ParameterMode.INOUT;
+	}
+
+	public class ParameterBindImpl<T> implements ParameterBind<T> {
+		private final T value;
+		private final TemporalType explicitTemporalType;
+
+		ParameterBindImpl(T value) {
+			this( value, null );
+		}
+
+		ParameterBindImpl(T value, TemporalType explicitTemporalType) {
+			this.value = value;
+			this.explicitTemporalType = explicitTemporalType;
+		}
+
+		@Override
+		public T getValue() {
+			return value;
+		}
+
+		@Override
+		public TemporalType getExplicitTemporalType() {
+			return explicitTemporalType;
+		}
+	}
+
+}
---
+++ core/src/test/java/org/hibernate/ogm/backendtck/storedprocedures/Car.java
@@ -0,0 +1,106 @@
+/*
+ * Hibernate OGM, Domain model persistence for NoSQL datastores
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.ogm.backendtck.storedprocedures;
+
+import static org.hibernate.ogm.backendtck.storedprocedures.indexed.IndexedStoredProcedureCallTest.TEST_RESULT_SET_STORED_PROC;
+import static org.hibernate.ogm.backendtck.storedprocedures.named.NamedStoredProcedureCallTest.TEST_RESULT_SET_STORED_PROC_ID_PARAM_NAME;
+import static org.hibernate.ogm.backendtck.storedprocedures.named.NamedStoredProcedureCallTest.TEST_RESULT_SET_STORED_PROC_TITLE_PARAM_NAME;
+
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.EntityResult;
+import javax.persistence.Id;
+import javax.persistence.NamedStoredProcedureQueries;
+import javax.persistence.NamedStoredProcedureQuery;
+import javax.persistence.ParameterMode;
+import javax.persistence.SqlResultSetMapping;
+import javax.persistence.StoredProcedureParameter;
+
+/**
+ * @author Sergey Chernolyas &amp;sergey_chernolyas@gmail.com&amp;
+ */
+@Entity
+@NamedStoredProcedureQueries({
+		@NamedStoredProcedureQuery(name = "testproc4_1", procedureName = TEST_RESULT_SET_STORED_PROC, parameters = {
+				@StoredProcedureParameter(mode = ParameterMode.REF_CURSOR, type = Void.class),
+				@StoredProcedureParameter(mode = ParameterMode.IN, type = Integer.class),
+				@StoredProcedureParameter(mode = ParameterMode.IN, type = String.class)
+		}, resultClasses = Car.class),
+		@NamedStoredProcedureQuery(name = "testproc4_2", procedureName = TEST_RESULT_SET_STORED_PROC, parameters = {
+				@StoredProcedureParameter(mode = ParameterMode.REF_CURSOR, type = Void.class),
+				@StoredProcedureParameter(mode = ParameterMode.IN, type = Integer.class),
+				@StoredProcedureParameter(mode = ParameterMode.IN, type = String.class)
+		}, resultSetMappings = "carMapping"),
+		@NamedStoredProcedureQuery(name = "testproc4_3", procedureName = TEST_RESULT_SET_STORED_PROC, parameters = {
+				@StoredProcedureParameter(name = "result", mode = ParameterMode.REF_CURSOR, type = Void.class),
+				@StoredProcedureParameter(name = TEST_RESULT_SET_STORED_PROC_ID_PARAM_NAME, mode = ParameterMode.IN, type = Integer.class),
+				@StoredProcedureParameter(name = TEST_RESULT_SET_STORED_PROC_TITLE_PARAM_NAME, mode = ParameterMode.IN, type = String.class)
+		}, resultClasses = Car.class),
+		@NamedStoredProcedureQuery(name = "testproc4_4", procedureName = TEST_RESULT_SET_STORED_PROC, parameters = {
+				@StoredProcedureParameter(name = "result", mode = ParameterMode.REF_CURSOR, type = Void.class),
+				@StoredProcedureParameter(name = TEST_RESULT_SET_STORED_PROC_ID_PARAM_NAME, mode = ParameterMode.IN, type = Integer.class),
+				@StoredProcedureParameter(name = TEST_RESULT_SET_STORED_PROC_TITLE_PARAM_NAME, mode = ParameterMode.IN, type = String.class)
+		}, resultSetMappings = "carMapping")
+})
+
+@SqlResultSetMapping(name = "carMapping", entities = { @EntityResult(entityClass = Car.class) })
+public class Car {
+
+	@Id
+	private Integer id;
+
+	private String title;
+
+	public Car() {
+	}
+
+	public Car(Integer id, String title) {
+		this.id = id;
+		this.title = title;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if ( this == o ) {
+			return true;
+		}
+		if ( o == null || getClass() != o.getClass() ) {
+			return false;
+		}
+		Car car = (Car) o;
+		return Objects.equals( id, car.id );
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash( id );
+	}
+
+	@Override
+	public String toString() {
+		return "Car{" +
+				"id='" + id + '\'' +
+				", title='" + title + '\'' +
+				'}';
+	}
+}
