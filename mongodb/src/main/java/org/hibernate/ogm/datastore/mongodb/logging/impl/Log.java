a54a942cb1bbf570afe1786e42d38d59d78902d7
Davide D'Alto <davide@hibernate.org>
2017-11-29 17:29:55 +0000

    OGM-359 Remove unecessary Log message

--- mongodb/src/main/java/org/hibernate/ogm/datastore/mongodb/logging/impl/Log.java
+++ mongodb/src/main/java/org/hibernate/ogm/datastore/mongodb/logging/impl/Log.java
@@ -137,6 +137,6 @@ public interface Log extends org.hibernate.ogm.util.impl.Log {
 	@Message(id = 1238, value = "Unable to execute command \"%s\". Error message : %s. Code name: %s")
 	HibernateException unableToExecuteCommand(String command, String errorMessage,String codeMessage, @Cause Exception e);
 
-	@Message(id = 1239, value = "Procedure \"%s\" returns %s but must java.util.List")
-	HibernateException resultSetMustBeRepresentedAsList(String procedureName, String resultClass);
+	@Message(id = 1239, value = "Dialect %s does not support named parameters when calling stored procedures ")
+	HibernateException dialectDoesNotSupportNamedParametersForStoredProcedures( Class<?> dialectClass );
 }
