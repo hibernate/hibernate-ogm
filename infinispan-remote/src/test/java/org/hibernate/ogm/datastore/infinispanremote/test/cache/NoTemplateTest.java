8219d8fef89a1ddc13daebb59fc4fba795932e49
Artem Laushkin <Artem_Laushkin@epam.com>
2017-12-11 14:06:17 +0300

    OGM-1329 fix tests

--- infinispan-remote/src/test/java/org/hibernate/ogm/datastore/infinispanremote/test/cache/NoTemplateTest.java
+++ infinispan-remote/src/test/java/org/hibernate/ogm/datastore/infinispanremote/test/cache/NoTemplateTest.java
@@ -6,9 +6,21 @@
  */
 package org.hibernate.ogm.datastore.infinispanremote.test.cache;
 
-import org.hibernate.HibernateException;
-import org.hibernate.ogm.datastore.infinispanremote.utils.RemoteHotRodServerRule;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.ogm.cfg.OgmProperties;
+import org.hibernate.ogm.datastore.infinispanremote.InfinispanRemoteProperties;
+import org.hibernate.ogm.datastore.infinispanremote.impl.InfinispanRemoteDatastoreProvider;
+import org.hibernate.ogm.datastore.infinispanremote.schema.spi.MapSchemaCapture;
+import org.hibernate.ogm.datastore.infinispanremote.utils.RemoteHotRodServerRule;
+import org.hibernate.ogm.datastore.spi.DatastoreProvider;
+import org.hibernate.ogm.utils.TestHelper;
+
+import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -26,6 +38,29 @@ public class NoTemplateTest {
 	public void noTemplateTest() {
 		thrown.expect( HibernateException.class );
 		thrown.expectMessage( "OGM001709:" );
-		CacheTestHelper.getDatastoreProvider( CacheEntity.class );
+		getDatastoreProvider( CacheEntity.class );
 	}
+
+	public static void getDatastoreProvider(Class<?> entity) {
+		Map<String, Object> settings = settings();
+		MapSchemaCapture schemaCapture = new MapSchemaCapture();
+		settings.put( InfinispanRemoteProperties.SCHEMA_CAPTURE_SERVICE, schemaCapture );
+		try ( SessionFactory sessionFactory = TestHelper.getDefaultTestSessionFactory( settings, entity ) ) {
+			InfinispanRemoteDatastoreProvider provider = (InfinispanRemoteDatastoreProvider) ( (SessionFactoryImplementor) sessionFactory )
+					.getServiceRegistry().getService( DatastoreProvider.class );
+			Assert.assertNotNull( provider.getCache( entity.getSimpleName() ) );
+		}
+	}
+
+	public static Map<String, Object> settings() {
+		Map<String, Object> settings = new HashMap<>();
+		settings.put( OgmProperties.DATASTORE_PROVIDER, "infinispan_remote" );
+		settings.put(
+				InfinispanRemoteProperties.CONFIGURATION_RESOURCE_NAME,
+				"hotrod-client-testingconfiguration.properties"
+		);
+		settings.put( OgmProperties.CREATE_DATABASE, true );
+		return settings;
+	}
+
 }
