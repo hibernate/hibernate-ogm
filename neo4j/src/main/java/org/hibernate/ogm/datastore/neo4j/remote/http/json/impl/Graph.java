664cded2f8d5e0a43709a0816ea17884371ef088
Davide D'Alto <davide@hibernate.org>
2017-12-13 14:18:17 +0000

    OGM-1344 Add equals and hashcode to Graph class for http dialect

--- neo4j/src/main/java/org/hibernate/ogm/datastore/neo4j/remote/http/json/impl/Graph.java
+++ neo4j/src/main/java/org/hibernate/ogm/datastore/neo4j/remote/http/json/impl/Graph.java
@@ -66,6 +66,36 @@ public class Graph {
 			this.properties = properties;
 		}
 
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ( ( id == null ) ? 0 : id.hashCode() );
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if ( this == obj ) {
+				return true;
+			}
+			if ( obj == null ) {
+				return false;
+			}
+			if ( getClass() != obj.getClass() ) {
+				return false;
+			}
+			Node other = (Node) obj;
+			if ( id == null ) {
+				if ( other.id != null ) {
+					return false;
+				}
+			}
+			else if ( !id.equals( other.id ) ) {
+				return false;
+			}
+			return true;
+		}
 	}
 
 	public static class Relationship {
