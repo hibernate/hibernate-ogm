3f12e1341c53b3e28a9ee208bbb619bdaf9e5942
Davide D'Alto <davide@hibernate.org>
2018-02-23 12:28:44 +0000

    OGM-1405 Add javadoc to @CacheTemplate

--- infinispan-remote/src/main/java/org/hibernate/ogm/datastore/infinispanremote/options/cache/CacheTemplate.java
+++ infinispan-remote/src/main/java/org/hibernate/ogm/datastore/infinispanremote/options/cache/CacheTemplate.java
@@ -11,10 +11,21 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.hibernate.ogm.datastore.infinispanremote.InfinispanRemoteProperties;
 import org.hibernate.ogm.datastore.infinispanremote.options.cache.impl.CacheTemplateConverter;
-
+import org.hibernate.ogm.datastore.keyvalue.options.CacheMappingType;
 import org.hibernate.ogm.options.spi.MappingOption;
 
+/**
+ * Define the template to use when the cache associated to the entity is created.
+ * <p>
+ * Overrides the global default template set using {@link InfinispanRemoteProperties#NEW_CACHE_TEMPLATE}
+ * set using for the specific entity cache.
+ * <p>
+ * This annotation is used only when {@link CacheMappingType#CACHE_PER_TABLE} strategy is used.
+ *
+ * @author Davide D'Alto
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @MappingOption( CacheTemplateConverter.class )
