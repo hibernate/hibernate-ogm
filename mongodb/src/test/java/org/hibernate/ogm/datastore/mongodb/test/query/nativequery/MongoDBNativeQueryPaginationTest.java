198932432faa2a5df114cf3eba8735d12829ee4e
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2018-03-02 16:53:49 +0100

    OGM-1411 Ordering enabled, improve to containsExactly result sets

--- mongodb/src/test/java/org/hibernate/ogm/datastore/mongodb/test/query/nativequery/MongoDBNativeQueryPaginationTest.java
+++ mongodb/src/test/java/org/hibernate/ogm/datastore/mongodb/test/query/nativequery/MongoDBNativeQueryPaginationTest.java
@@ -8,6 +8,7 @@ package org.hibernate.ogm.datastore.mongodb.test.query.nativequery;
 
 import static org.fest.assertions.Assertions.assertThat;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.hibernate.Session;
@@ -25,21 +26,21 @@ import org.junit.Test;
  */
 public class MongoDBNativeQueryPaginationTest extends OgmTestCase {
 
-	public static final String QUERY_FIND = "db.WILDE_POEM.find( { 'author' : 'Oscar Wilde' } )";
-	public static final String QUERY_AGGREGATE_MATCH = "db.WILDE_POEM.aggregate( [ { '$match' : { 'author' : 'Oscar Wilde' } } ] )";
+	public static final String QUERY_FIND = "db.WILDE_POEM.find( { '$query' : { 'author' : 'Oscar Wilde' }, '$orderby' : { 'name' : 1 } } )";
+	public static final String QUERY_AGGREGATE_MATCH = "db.WILDE_POEM.aggregate( [ { '$match' : { 'author' : 'Oscar Wilde' } }, { '$sort': {'name': 1 } } ] )";
 
 	private final OscarWildePoem portia = new OscarWildePoem( 1L, "Portia", "Oscar Wilde", 1881 );
 	private final OscarWildePoem athanasia = new OscarWildePoem( 2L, "Athanasia", "Oscar Wilde", 1879 );
 	private final OscarWildePoem imperatrix = new OscarWildePoem( 3L, "Ave Imperatrix", "Oscar Wilde", 1882 );
 	private final OscarWildePoem intellectualis = new OscarWildePoem( 4L, "Amor Intellectualis", "Oscar Wilde", 1881 );
-	private final OscarWildePoem apologia = new OscarWildePoem( 5L, "Apologias ", "Oscar Wilde", 1881 );
+	private final OscarWildePoem apologia = new OscarWildePoem( 5L, "Apologias", "Oscar Wilde", 1881 );
 	private final OscarWildePoem easter = new OscarWildePoem( 6L, "Easter Day", "Oscar Wilde", 1881 );
 	private final OscarWildePoem rome = new OscarWildePoem( 7L, "Rome Unvisited", "Oscar Wilde", 1881 );
 	private final OscarWildePoem miniato = new OscarWildePoem( 8L, "San Miniato", "Oscar Wilde", 1881 );
 	private final OscarWildePoem liberty = new OscarWildePoem( 9L, "Sonnet to Liberty", "Oscar Wilde", 1881 );
 	private final OscarWildePoem vita = new OscarWildePoem( 10L, "Vita Nuova", "Oscar Wilde", 1881 );
 
-	private final OscarWildePoem[] poems = { portia, athanasia, imperatrix, intellectualis, apologia, easter, rome, miniato, liberty, vita };
+	private final OscarWildePoem[] poems = { intellectualis, apologia, athanasia, imperatrix, easter, portia, rome, miniato, liberty, vita };
 
 	@Before
 	public void init() {
@@ -67,102 +68,102 @@ public class MongoDBNativeQueryPaginationTest extends OgmTestCase {
 	}
 
 	@Test
-	public void testFindPage1Size10() {
+	public void testFindPage1Size7FirstPage() {
 		inTransaction( session -> {
-			List result = session.createNativeQuery( QUERY_FIND )
+			List<?> result = session.createNativeQuery( QUERY_FIND )
+				.addEntity( OscarWildePoem.TABLE_NAME, OscarWildePoem.class )
+				.setFirstResult( 0 )
+				.setMaxResults( 7 )
+				.list();
+
+			assertThat( result ).containsExactly( Arrays.copyOfRange( poems, 0, 7 ) );
+		} );
+	}
+
+	@Test
+	public void testAggregationPipelinePage1Size10FullResults() {
+		inTransaction( session -> {
+			List<?> result = session.createNativeQuery( QUERY_AGGREGATE_MATCH )
 				.addEntity( OscarWildePoem.TABLE_NAME, OscarWildePoem.class )
 				.setFirstResult( 0 )
 				.setMaxResults( 10 )
 				.list();
 
-			assertThat( result ).containsOnly( poems );
+			assertThat( result ).containsExactly( poems );
 		} );
 	}
 
 	@Test
-	public void testAggregationPipelinePage1Size10() {
+	public void testFindPage2Size5LastPage() {
 		inTransaction( session -> {
-			List result = session.createNativeQuery( QUERY_AGGREGATE_MATCH )
-				.addEntity( OscarWildePoem.TABLE_NAME, OscarWildePoem.class )
-				.setFirstResult( 0 )
-				.setMaxResults( 10 )
-				.list();
-
-			assertThat( result ).containsOnly( poems );
-		} );
-	}
-
-	@Test
-	public void testFindPage2Size5() {
-		inTransaction( session -> {
-			List result = session.createNativeQuery( QUERY_FIND )
+			List<?> result = session.createNativeQuery( QUERY_FIND )
 				.addEntity( OscarWildePoem.TABLE_NAME, OscarWildePoem.class )
 				.setFirstResult( 5 )
 				.setMaxResults( 5 )
 				.list();
 
-			assertThat( result.size() ).isEqualTo( 5 );
+			assertThat( result ).containsExactly( Arrays.copyOfRange( poems, 5, 10 ) );
 		} );
 	}
 
 	@Test
 	public void testAggregationMatchPage2Size5() {
 		inTransaction( session -> {
-			List result = session.createNativeQuery( QUERY_AGGREGATE_MATCH )
+			List<?> result = session.createNativeQuery( QUERY_AGGREGATE_MATCH )
 				.addEntity( OscarWildePoem.TABLE_NAME, OscarWildePoem.class )
 				.setFirstResult( 5 )
 				.setMaxResults( 5 )
 				.list();
 
-			assertThat( result.size() ).isEqualTo( 5 );
+			assertThat( result ).containsExactly( Arrays.copyOfRange( poems, 5, 10 ) );
 		} );
 	}
 
 	@Test
 	public void testFindPage1Page2Size5() {
 		inTransaction( session -> {
-			List result = session.createNativeQuery( QUERY_FIND )
+			List<?> result = session.createNativeQuery( QUERY_FIND )
 				.addEntity( OscarWildePoem.TABLE_NAME, OscarWildePoem.class )
 				.setFirstResult( 0 )
 				.setMaxResults( 5 )
 				.list();
 
-			assertThat( result.size() ).isEqualTo( 5 );
+			assertThat( result ).containsExactly( Arrays.copyOfRange( poems, 0, 5 ) );
 
-			result.addAll( session.createNativeQuery( QUERY_FIND )
+			result = ( session.createNativeQuery( QUERY_FIND )
 				.addEntity( OscarWildePoem.TABLE_NAME, OscarWildePoem.class )
 				.setFirstResult( 5 )
 				.setMaxResults( 5 )
 				.list() );
 
-			assertThat( result ).containsOnly( poems );
+			assertThat( result ).containsExactly( Arrays.copyOfRange( poems, 5, 10 ) );
 		} );
 	}
 
 	@Test
-	@TestForIssue( jiraKey = "OGM-1411" )
+	@TestForIssue(jiraKey = "OGM-1411")
 	public void testAggregationMatchPage1Page2Size5() {
 		inTransaction( session -> {
-			List result = session.createNativeQuery( QUERY_AGGREGATE_MATCH )
+			List<?> result = session.createNativeQuery( QUERY_AGGREGATE_MATCH )
 				.addEntity( OscarWildePoem.TABLE_NAME, OscarWildePoem.class )
 				.setFirstResult( 0 )
 				.setMaxResults( 5 )
 				.list();
 
-			assertThat( result.size() ).isEqualTo( 5 );
+			assertThat( result ).containsExactly( Arrays.copyOfRange( poems, 0, 5 ) );
 
-			result.addAll(  session.createNativeQuery( QUERY_AGGREGATE_MATCH )
+			result = session.createNativeQuery( QUERY_AGGREGATE_MATCH )
 				.addEntity( OscarWildePoem.TABLE_NAME, OscarWildePoem.class )
 				.setFirstResult( 5 )
 				.setMaxResults( 5 )
-				.list() );
+				.list();
 
-			assertThat( result ).containsOnly( poems );
+			assertThat( result ).containsExactly( Arrays.copyOfRange( poems, 5, 10 ) );
 		} );
 	}
 
 	@Test
-	public void testAggregationMatchPage1Size15() {
+	public void testAggregationMatchPage1Size15LessResultsThenMaxResults() {
 		inTransaction( session -> {
 			List result = session.createNativeQuery( QUERY_AGGREGATE_MATCH )
 				.addEntity( OscarWildePoem.TABLE_NAME, OscarWildePoem.class )
@@ -170,7 +171,75 @@ public class MongoDBNativeQueryPaginationTest extends OgmTestCase {
 				.setMaxResults( 15 )
 				.list();
 
-			assertThat( result ).containsOnly( poems );
+			assertThat( result ).containsExactly( poems );
+		} );
+	}
+
+	@Test
+	public void testFindPage2Size3MiddlePage() {
+		inTransaction( session -> {
+			List result = session.createNativeQuery( QUERY_FIND )
+				.addEntity( OscarWildePoem.TABLE_NAME, OscarWildePoem.class )
+				.setFirstResult( 3 )
+				.setMaxResults( 3 )
+				.list();
+
+			assertThat( result ).containsExactly( Arrays.copyOfRange( poems, 3, 6 ) );
+		} );
+	}
+
+	@Test
+	public void testAggregationPipelinePage2Page3Size3Scrolling() {
+		inTransaction( session -> {
+			List result = session.createNativeQuery( QUERY_AGGREGATE_MATCH )
+				.addEntity( OscarWildePoem.TABLE_NAME, OscarWildePoem.class )
+				.setFirstResult( 3 )
+				.setMaxResults( 3 )
+				.list();
+
+			assertThat( result ).containsExactly( Arrays.copyOfRange( poems, 3, 6 ) );
+
+			result = session.createNativeQuery( QUERY_AGGREGATE_MATCH )
+				.addEntity( OscarWildePoem.TABLE_NAME, OscarWildePoem.class )
+				.setFirstResult( 6 )
+				.setMaxResults( 3 )
+				.list();
+
+			assertThat( result ).containsExactly( Arrays.copyOfRange( poems, 6, 9 ) );
+		} );
+	}
+
+	@Test
+	public void testFindPage2Page1Size3BackScrolling() {
+		inTransaction( session -> {
+			List result = session.createNativeQuery( QUERY_FIND )
+					.addEntity( OscarWildePoem.TABLE_NAME, OscarWildePoem.class )
+					.setFirstResult( 3 )
+					.setMaxResults( 3 )
+					.list();
+
+			assertThat( result ).containsExactly( Arrays.copyOfRange( poems, 3, 6 ) );
+
+			result = session.createNativeQuery( QUERY_FIND )
+					.addEntity( OscarWildePoem.TABLE_NAME, OscarWildePoem.class )
+					.setFirstResult( 0 )
+					.setMaxResults( 3 )
+					.list();
+
+			assertThat( result ).containsExactly( Arrays.copyOfRange( poems, 0, 3 ) );
+		} );
+	}
+
+	@Test
+	public void testFindPage3Size5OutOfRangePage() {
+		inTransaction( session -> {
+			List result = session.createNativeQuery( QUERY_FIND )
+				.addEntity( OscarWildePoem.TABLE_NAME, OscarWildePoem.class )
+				.setFirstResult( 10 )
+				.setMaxResults( 5 )
+				.list();
+
+			assertThat( result ).isEmpty();
 		} );
 	}
 
