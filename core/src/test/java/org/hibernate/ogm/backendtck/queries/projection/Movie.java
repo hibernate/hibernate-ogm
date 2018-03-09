359cd288da9b33f063c1c96b96af114eed94b35c
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2018-03-09 15:21:44 +0100

    OGM-1375 Test avoiding use of partial entities

---
+++ core/src/test/java/org/hibernate/ogm/backendtck/queries/projection/Movie.java
@@ -0,0 +1,83 @@
+/*
+ * Hibernate OGM, Domain model persistence for NoSQL datastores
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.ogm.backendtck.queries.projection;
+
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.search.annotations.Analyze;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Indexed;
+import org.hibernate.search.annotations.SortableField;
+import org.hibernate.search.annotations.Store;
+
+@Entity
+@Table(name = "Movie")
+@Indexed
+public class Movie {
+
+	@Id
+	private Integer id;
+
+	@Field(analyze = Analyze.NO, store = Store.YES)
+	@SortableField
+	private String name;
+
+	@Field(analyze = Analyze.NO, store = Store.YES)
+	@SortableField
+	private String author;
+
+	@Field(analyze = Analyze.NO, store = Store.YES)
+	@SortableField
+	private Integer year;
+
+	public Movie() {
+	}
+
+	public Movie(Integer id, String name, String author, int year) {
+		this.id = id;
+		this.name = name;
+		this.author = author;
+		this.year = year;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Movie{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				", author='" + author + '\'' +
+				", year=" + year +
+				'}';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if ( this == o ) {
+			return true;
+		}
+		if ( o == null || getClass() != o.getClass() ) {
+			return false;
+		}
+		Movie movie = (Movie) o;
+		return Objects.equals( id, movie.id ) &&
+			Objects.equals( name, movie.name ) &&
+			Objects.equals( author, movie.author ) &&
+			Objects.equals( year, movie.year );
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash( id, name, author, year );
+	}
+}
---
+++ core/src/test/java/org/hibernate/ogm/backendtck/queries/projection/NativeQueryProjectionBaseTest.java
@@ -0,0 +1,94 @@
+/*
+ * Hibernate OGM, Domain model persistence for NoSQL datastores
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.ogm.backendtck.queries.projection;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.util.List;
+import javax.persistence.PersistenceException;
+
+import org.hibernate.ogm.utils.TestForIssue;
+
+import org.junit.Test;
+
+/**
+ * Base test for use of a projection:
+ * if a projection is used on root Entity then the result must be an Object array instead of an Entity.
+ *
+ * addEntity native query parameter is not allowed then projection is used on root Entity.
+ *
+ * @author Fabio Massimo Ercoli
+ */
+public abstract class NativeQueryProjectionBaseTest extends SingleMovieBaseTest {
+
+	protected abstract String getNativeQueryWithoutProjection();
+	protected abstract String getNativeQueryWithProjectionIdName();
+	protected abstract String getNativeQueryWithProjectionYearAuthor();
+
+	@Test
+	public void testUniqueResultWithAddEntityWithoutProjection() {
+		inTransaction( session -> {
+			Movie movie = (Movie) session.createNativeQuery( getNativeQueryWithoutProjection() )
+				.addEntity( Movie.class )
+				.uniqueResult();
+
+			assertThat( movie ).isEqualTo( originalMovie );
+		} );
+	}
+
+	@Test
+	@TestForIssue( jiraKey = "OGM-1375" )
+	public void testUniqueResultWithAddEntityWithProjection() {
+		thrown.expect( PersistenceException.class );
+		thrown.expectMessage( PROJECTION_ADD_ENTITY_MESSAGE );
+
+		inTransaction( session -> {
+			session.createNativeQuery( getNativeQueryWithProjectionIdName() )
+				.addEntity( Movie.class )
+				.uniqueResult();
+		} );
+	}
+
+	@Test
+	public void testUniqueResultWithoutAddEntityWithProjection() {
+		inTransaction( session -> {
+			Object[] movie = (Object[]) session.createNativeQuery( getNativeQueryWithProjectionIdName() )
+				.uniqueResult();
+
+			// actually Neo4J remote http return always Integer type
+			// while Neo4J remote bolt return always Long type
+			// regardless the fact that type declared on the Entity is an Integer or a Long
+			assertThat( ( (Number) movie[0] ).intValue() ).isEqualTo( 1 );
+			assertThat( movie[1] ).isEqualTo( "2001: A Space Odyssey" );
+		} );
+	}
+
+	@Test
+	public void testResultListWithAddEntityWithoutProjection() {
+		inTransaction( session -> {
+			List<Movie> movies = session.createNativeQuery( getNativeQueryWithoutProjection() )
+				.addEntity( Movie.class )
+				.getResultList();
+
+			assertThat( movies.get( 0 ) ).isEqualTo( originalMovie );
+		} );
+	}
+
+	@Test
+	@TestForIssue( jiraKey = "OGM-1375" )
+	public void testResultListWithAddEntityWithProjection() {
+		thrown.expect( PersistenceException.class );
+		thrown.expectMessage( PROJECTION_ADD_ENTITY_MESSAGE );
+
+		inTransaction( session -> {
+			session.createNativeQuery( getNativeQueryWithProjectionYearAuthor() )
+				.addEntity( Movie.class )
+				.getResultList();
+		} );
+	}
+
+}
