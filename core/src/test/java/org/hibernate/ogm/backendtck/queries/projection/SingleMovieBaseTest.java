de7c199faac7cdf60dd3700fef039d766da8bb2e
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2018-03-09 15:21:44 +0100

    OGM-1375 Test avoiding use of partial entities

---
+++ core/src/test/java/org/hibernate/ogm/backendtck/queries/projection/SingleMovieBaseTest.java
@@ -0,0 +1,47 @@
+/*
+ * Hibernate OGM, Domain model persistence for NoSQL datastores
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.ogm.backendtck.queries.projection;
+
+import org.hibernate.ogm.utils.OgmTestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
+/**
+ * @author Fabio Massimo Ercoli
+ */
+public abstract class SingleMovieBaseTest extends OgmTestCase {
+
+	protected static final String PROJECTION_ADD_ENTITY_MESSAGE = "OGM000091: Projection and addEntity are not allowed in the same query on <Movie> ";
+
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+	protected final Movie originalMovie = new Movie( 1, "2001: A Space Odyssey", "Stanley Kubrick", 1968 );
+
+	@Before
+	public void init() {
+		inTransaction( session -> session.persist( originalMovie ) );
+	}
+
+	@After
+	public void tearDown() {
+		inTransaction( session -> {
+			Object entity = session.get( Movie.class, originalMovie.getId() );
+			if ( entity != null ) {
+				session.delete( entity );
+			}
+		} );
+	}
+
+	@Override
+	protected Class<?>[] getAnnotatedClasses() {
+		return new Class<?>[] { Movie.class };
+	}
+}
