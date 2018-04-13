94dd844bf3b67ac37dde6530ec973839ceb171f9
Sergey Chernolyas <sergey.chernolyas@gmail.com>
2018-04-13 17:57:11 +0300

    OGM-1047 create database pool
    Main idea is: one instance of Neo4j for one file path

--- neo4j/src/main/java/org/hibernate/ogm/datastore/neo4j/embedded/impl/EmbeddedNeo4jGraphDatabaseFactory.java
+++ neo4j/src/main/java/org/hibernate/ogm/datastore/neo4j/embedded/impl/EmbeddedNeo4jGraphDatabaseFactory.java
@@ -7,16 +7,27 @@
 package org.hibernate.ogm.datastore.neo4j.embedded.impl;
 
 import java.io.File;
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Field;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
 
+import org.hibernate.HibernateException;
 import org.hibernate.ogm.datastore.neo4j.Neo4jProperties;
+import org.hibernate.ogm.datastore.neo4j.logging.impl.Log;
+import org.hibernate.ogm.datastore.neo4j.logging.impl.LoggerFactory;
 import org.hibernate.ogm.datastore.neo4j.spi.GraphDatabaseServiceFactory;
 import org.hibernate.ogm.util.configurationreader.spi.ConfigurationPropertyReader;
+
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.factory.GraphDatabaseBuilder;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.kernel.api.exceptions.TransactionFailureException;
+import org.neo4j.kernel.impl.factory.GraphDatabaseFacade;
+import org.neo4j.kernel.internal.GraphDatabaseAPI;
 
 /**
  * Contains methods to create a {@link GraphDatabaseService} for the embedded Neo4j.
@@ -24,6 +35,10 @@ import org.neo4j.graphdb.factory.GraphDatabaseFactory;
  * @author Davide D'Alto &lt;davide@hibernate.org&gt;
  */
 public class EmbeddedNeo4jGraphDatabaseFactory implements GraphDatabaseServiceFactory {
+	public static final int WAITING_TIME_MS = 100;
+
+	private static final Map<String, FactoryHolder> GRAPH_DATABASE_SERVICE_MAP = new ConcurrentHashMap<>();
+	private static Log LOG = LoggerFactory.make( MethodHandles.lookup() );
 
 	private File dbLocation;
 
@@ -50,6 +65,38 @@ public class EmbeddedNeo4jGraphDatabaseFactory implements GraphDatabaseServiceFa
 
 	@Override
 	public GraphDatabaseService create() {
+		final String dbLocationAbsolutePath = dbLocation.getAbsolutePath();
+		AtomicBoolean isNew = new AtomicBoolean( false );
+		FactoryHolder factoryHolder = GRAPH_DATABASE_SERVICE_MAP.computeIfAbsent( dbLocationAbsolutePath, ( String dbPath ) -> {
+			LOG.infof( " Create new service instance for dbPath  %s", dbLocationAbsolutePath );
+			GraphDatabaseFacade service = buildGraphDatabaseService();
+			FactoryHolder holder = new FactoryHolder();
+			holder.setCounter( 1 );
+			holder.setGraphDatabaseFacade( service );
+			isNew.set( true );
+			return holder;
+		} );
+		if ( !isNew.get() ) {
+			synchronized (GRAPH_DATABASE_SERVICE_MAP) {
+				factoryHolder.getGraphDatabaseFacade();
+				boolean isAvailable = factoryHolder.getGraphDatabaseFacade().isAvailable( WAITING_TIME_MS );
+				if ( isAvailable ) {
+					factoryHolder.setCounter( factoryHolder.getCounter() + 1 );
+				}
+				else {
+					//need recreate db
+					LOG.infof( " Recreate service instance for dbPath  %s", dbLocationAbsolutePath );
+					factoryHolder.setCounter( 1 );
+					factoryHolder.setGraphDatabaseFacade( buildGraphDatabaseService() );
+				}
+				LOG.debugf( " Counter : %s for path %s", factoryHolder.getCounter(), dbLocationAbsolutePath );
+			}
+		}
+		return factoryHolder.getGraphDatabaseFacade();
+	}
+
+	private GraphDatabaseFacade buildGraphDatabaseService() {
+		GraphDatabaseService service = null;
 		GraphDatabaseBuilder builder = new GraphDatabaseFactory().newEmbeddedDatabaseBuilder( dbLocation );
 		setConfigurationFromLocation( builder, configurationLocation );
 		setConfigurationFromProperties( builder, configuration );
@@ -60,11 +107,17 @@ public class EmbeddedNeo4jGraphDatabaseFactory implements GraphDatabaseServiceFa
 			//Neo4J relies on the context classloader to load its own extensions:
 			//Allow it to load even the ones we're not exposing directly to end users.
 			currentThread.setContextClassLoader( neo4JClassLoader );
-			return builder.newGraphDatabase();
+			service =  builder.newGraphDatabase();
+			GraphDatabaseFacade sq = (GraphDatabaseFacade) service;
+			LOG.infof( "Created new storage at path : %s;  store id: %s",sq.getStoreDir(),sq.storeId() );
+		}
+		catch (Exception e) {
+			throw LOG.cannotCreateNewGraphDatabaseServiceException( e );
 		}
 		finally {
 			currentThread.setContextClassLoader( contextClassLoader );
 		}
+		return (GraphDatabaseFacade) service;
 	}
 
 	private void setConfigurationFromProperties(GraphDatabaseBuilder builder, Map<?, ?> properties) {
@@ -86,4 +139,74 @@ public class EmbeddedNeo4jGraphDatabaseFactory implements GraphDatabaseServiceFa
 			builder.loadPropertiesFromURL( cfgLocation );
 		}
 	}
+
+	static void shutdownGraphDatabaseService(GraphDatabaseService neo4jDb) {
+		GraphDatabaseAPI sq = (GraphDatabaseAPI) neo4jDb;
+		String key = sq.getStoreDir().getAbsolutePath();
+		if ( GRAPH_DATABASE_SERVICE_MAP.containsKey( key ) ) {
+			synchronized (GRAPH_DATABASE_SERVICE_MAP) {
+				FactoryHolder factoryHolder = GRAPH_DATABASE_SERVICE_MAP.get( key );
+				factoryHolder.setCounter( factoryHolder.getCounter() - 1 );
+				LOG.debugf( " Counter : %s  for path %s", factoryHolder.getCounter(), key );
+
+				GraphDatabaseFacade graphDatabaseFacade = factoryHolder.getGraphDatabaseFacade();
+				try {
+					GraphDatabaseFacade.SPI spi = null;
+					Object obj = getPrivateField( GraphDatabaseFacade.class.getDeclaredField( "spi" ), graphDatabaseFacade );
+					spi = (GraphDatabaseFacade.SPI) obj;
+					LOG.debugf( " isInOpenTransaction: %s", spi.isInOpenTransaction() );
+					if ( spi.isInOpenTransaction() ) {
+						LOG.warnf( "currentTransaction  %s started at %s", spi.currentTransaction() , spi.currentTransaction().startTime() );
+						spi.currentTransaction().failure();
+						spi.currentTransaction().closeTransaction();
+						throw LOG.existsOpenTransactionsFornDatabasePathException( key );
+					}
+				}
+				catch ( IllegalAccessException | NoSuchFieldException e) {
+					throw new HibernateException( "Can't get value of private variable by reflection!",e );
+				}
+				catch (TransactionFailureException e) {
+					throw new HibernateException( "Can't close transction!",e );
+				}
+
+
+				if ( factoryHolder.getCounter() == 0 ) {
+					LOG.infof( " Shutdown db for path : %s", key );
+					factoryHolder.getGraphDatabaseFacade().shutdown();
+					GRAPH_DATABASE_SERVICE_MAP.remove( key );
+				}
+			}
+		}
+		else {
+			throw LOG.unknownDatabasePathException( key );
+		}
+	}
+
+	private static Object getPrivateField(Field privateField, Object targetObject) throws IllegalAccessException {
+		privateField.setAccessible( true );
+		return privateField.get( targetObject );
+	}
+
+
+	private static class FactoryHolder {
+		private int counter;
+		private GraphDatabaseFacade graphDatabaseFacade;
+
+		int getCounter() {
+			return counter;
+		}
+
+		void setCounter(int counter) {
+			this.counter = counter;
+		}
+
+		public GraphDatabaseFacade getGraphDatabaseFacade() {
+			return graphDatabaseFacade;
+		}
+
+		public void setGraphDatabaseFacade(GraphDatabaseFacade graphDatabaseFacade) {
+			this.graphDatabaseFacade = graphDatabaseFacade;
+		}
+	}
+
 }
