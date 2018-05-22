90a055bbbea48cd1f7718418d07c94f9bcb42b0c
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2018-05-22 18:17:22 +0200

    OGM-1376 testing sequences with local caches

---
+++ infinispan-embedded/src/test/java/org/hibernate/ogm/datastore/infinispan/test/dialect/impl/counter/TransportNotDefinedTableGenerationTest.java
@@ -0,0 +1,27 @@
+/*
+ * Hibernate OGM, Domain model persistence for NoSQL datastores
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.ogm.datastore.infinispan.test.dialect.impl.counter;
+
+import org.hibernate.ogm.backendtck.id.TableNextValueGenerationTest;
+import org.hibernate.ogm.datastore.infinispan.InfinispanProperties;
+import org.hibernate.ogm.utils.TestForIssue;
+import org.hibernate.ogm.utils.jpa.GetterPersistenceUnitInfo;
+
+/**
+ * Repeat {@link TableNextValueGenerationTest} tests
+ * with a configuration does not defining transport
+ *
+ * @author Fabio Massimo Ercoli
+ */
+@TestForIssue(jiraKey = "OGM-1376")
+public class TransportNotDefinedTableGenerationTest extends TableNextValueGenerationTest {
+
+	protected void configure(GetterPersistenceUnitInfo info) {
+		super.configure( info );
+		info.getProperties().setProperty( InfinispanProperties.CONFIGURATION_RESOURCE_NAME, "infinispan-local.xml" );
+	}
+}
