c145dede6e0dfceb4190ff284b628d16c7ed72e5
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2018-05-22 17:40:46 +0200

    OGM-1376 allowing counters for sequences also in local config

--- infinispan-embedded/src/main/java/org/hibernate/ogm/datastore/infinispan/logging/impl/Log.java
+++ infinispan-embedded/src/main/java/org/hibernate/ogm/datastore/infinispan/logging/impl/Log.java
@@ -53,9 +53,6 @@ public interface Log extends org.hibernate.ogm.util.impl.Log {
 			+ " Enable this property explicitly in your configuration to resolve this warning.")
 	void clusteringHashGroupsMustBeEnabled(String cacheName);
 
-	@Message(id = 1108, value = "Sequences or di generation is not supported for local caches")
-	HibernateException counterCannotBeCreatedForLocalCaches();
-
 	@Message(id = 1109, value = "Counter is not defined and cannot be created. Global persistent-location is missing in the Infinispan configuration")
 	HibernateException counterCannotBeCreatedWithoutGlobalConfiguration();
 
