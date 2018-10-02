2d7d7ab1efc3743eddde156b5cc882b59aaead42
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2018-10-02 14:14:11 +0200

    OGM-1425 Move single table test to new package

--- mongodb/src/test/java/org/hibernate/ogm/datastore/mongodb/test/inheritance/singletable/SimpleNode.java
+++ mongodb/src/test/java/org/hibernate/ogm/datastore/mongodb/test/inheritance/singletable/SimpleNode.java
@@ -4,23 +4,13 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.ogm.datastore.mongodb.test.inheritance;
+package org.hibernate.ogm.datastore.mongodb.test.inheritance.singletable;
 
 import javax.persistence.Entity;
 
 @Entity
-public class TextNode extends Node {
+public class SimpleNode extends Node {
 
-	private String text;
-
-	public TextNode() {
-	}
-
-	public String getText() {
-		return text;
-	}
-
-	public void setText(String text) {
-		this.text = text;
+	public SimpleNode() {
 	}
 }
--- mongodb/src/test/java/org/hibernate/ogm/datastore/mongodb/test/inheritance/singletable/TextNode.java
+++ mongodb/src/test/java/org/hibernate/ogm/datastore/mongodb/test/inheritance/singletable/TextNode.java
@@ -4,13 +4,23 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.ogm.datastore.mongodb.test.inheritance;
+package org.hibernate.ogm.datastore.mongodb.test.inheritance.singletable;
 
 import javax.persistence.Entity;
 
 @Entity
-public class SimpleNode extends Node {
+public class TextNode extends Node {
 
-	public SimpleNode() {
+	private String text;
+
+	public TextNode() {
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
 	}
 }
